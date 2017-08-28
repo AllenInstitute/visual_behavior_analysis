dc4a9ccbc2555fd703692c32a2d60e6a982c8e83
Justin Kiggins <justink@alleninstitute.org>
2017-08-28 16:44:07 -0700

    HOTFIX linux path

--- visual_behavior/__init__.py
+++ visual_behavior/__init__.py
@@ -2,9 +2,7 @@ import platform
 import os
 
 if platform.system() == 'Linux':
-    basepath = "/allen/programs/neuralcoding/Behavior/Data"
-    if os.path.isdir(basepath)==False:
-        basepath = "/data/neuralcoding/Behavior/Data"
+    basepath = "/allen/programs/braintv/workgroups/neuralcoding/Behavior/Data"
 else:
     basepath = r"\\allen\programs\braintv\workgroups\neuralcoding\Behavior\Data"
 
