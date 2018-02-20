2508d2e4e0cea92cc2d5bf8a0d057fb24fe75d5e
mochic808 <chrism@alleninstitute.org>
2018-02-20 15:34:52 -0800

    implement Foraging2 version of what was previously known as io.py

---
+++ tests/test_processing.py
@@ -0,0 +1,160 @@
+import pytest
+from six import b
+import numpy as np
+import pandas as pd
+
+from visual_behavior import processing
+
+
+@pytest.mark.parametrize("data, expected", [
+    ({"items": {"behavior": {"wut": "wew", }, }, }, {"wut": "wew", }, ),
+    ({"wut": "wew", }, {"wut": "wew", }, ),
+])
+def test_behavior_items_or_top_level(data, expected):
+    assert processing.behavior_items_or_top_level(data) == expected
+
+
+def test_get_params(pizza_data_fixture):
+    assert processing.get_params(pizza_data_fixture) == {
+        'auto_reward_volume': 0.007,
+        'catch_freq': 0.125,
+        'failure_repeats': 5,
+        'max_task_duration_min': 60.0,
+        'mouse_id': 'M347745',
+        'nidevice': b'Dev1',
+        'periodic_flash': (0.25, 0.5),
+        'pre_change_time': 2.25,
+        'response_window': (0.15, 0.75),
+        'reward_volume': 0.007,
+        'script': 'c:\\users\\svc_nc~1\\appdata\\local\\temp\\tmpulwuhh\\M347745 - spmuoq.py',
+        'stimulus_window': 6.0,
+        'task_id': b'DoC',
+        'trial_translator': True,
+        'user_id': 'kylam',
+        'volume_limit': 5,
+        'warm_up_trials': 5,
+    }
+
+
+def test_get_trials(pizza_data_fixture):
+    expected = [
+        {
+            'index': 0,
+            'cumulative_rewards': 0,
+            'cumulative_volume': 0.0,
+            'stimulus_changes': [],
+            'success': False,
+            'licks': [(7.934872470921013, 25)],
+            'trial_params': {
+                'catch': False,
+                'auto_reward': True,
+                'change_time': 0.7802031782835195
+            },
+            'rewards': [],
+            'events': [
+                ['initial_blank', 'enter', 7.513004185308317, 0],
+                ['initial_blank', 'exit', 7.5131104567819404, 0],
+                ['pre_change', 'enter', 7.513202823576774, 0],
+                ['early_response', '', 7.935050252171282, 25],
+                ['abort', '', 7.935161158537837, 25],
+                ['timeout', 'enter', 7.935301198517099, 25],
+                ['timeout', 'exit', 7.935412104883654, 25]
+            ]
+        }
+    ]
+    assert processing.get_trials(pizza_data_fixture)[:1] == expected
+
+
+def test_get_time(pizza_data_fixture):
+    expected = np.array([
+         0. , 16.7038231, 33.3285219, 50.0144676, 66.6954472, 83.341334,
+         100.0696558, 116.7347444, 133.4120824, 150.1053114,
+    ])
+
+    np.testing.assert_almost_equal(
+        processing.get_time(pizza_data_fixture)[:10],
+        expected
+    )
+
+
+def test_get_rewards(pizza_data_fixture):
+    """numpy array of [<time>, <frame number>]
+    """
+    expected = np.array([
+        [11.33841538, 230. ],
+        [21.34682871, 828. ],
+        [32.12227642, 1472. ],
+        [39.82892842, 1932. ],
+        [51.37191175, 2622. ]
+    ])
+
+    np.testing.assert_almost_equal(
+        processing.get_rewards(pizza_data_fixture)[:5],
+        expected
+    )
+
+
+def test_get_vsyncs(pizza_data_fixture):
+    expected = np.array([
+        16.7038231, 16.6246988, 16.6859457, 16.6809796, 16.6458868, 16.7283218,
+        16.6650886, 16.677338 , 16.693229 , 16.688263
+    ])
+
+    np.testing.assert_almost_equal(
+        processing.get_vsyncs(pizza_data_fixture)[:10],
+        expected
+    )
+
+
+def test_get_dx(pizza_data_fixture):
+    expected = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    np.testing.assert_almost_equal(
+        processing.get_dx(pizza_data_fixture)[:10],
+        expected
+    )
+
+
+def test_get_licks(pizza_data_fixture):
+    expected = pd.DataFrame(data={
+        "frame": np.array([25, 844, 1074, 1257, 1488, ]),
+        "time": np.array([416.96787951514125, 14128.407060168684, 17981.629601912573, 21034.25351413898, 24904.09280906897])
+    })
+
+    pd.testing.assert_frame_equal(
+        processing.get_licks(pizza_data_fixture).iloc[:5],
+        expected,
+        check_column_type=False,
+        check_index_type=False,
+        check_dtype=False,
+        check_like=True
+    )
+
+
+@pytest.mark.parametrize("kwargs, expected", [
+    (
+        {},
+        pd.DataFrame(data={
+            "time": np.array([0.0, 16.70382311567664, 33.328521880321205, 50.01446756068617, 66.69544719625264]),
+            "speed (cm/s)": np.array([0, 0, 0, 0, 0]),
+            "acceleration (cm/s^2)": np.array([0, 0, 0, 0, 0]),
+            "jerk (cm/s^3)": np.array([0, 0, 0, 0, 0]),
+        }),
+    ),
+])
+def test_get_running_speed(pizza_data_fixture, kwargs, expected):
+    pd.testing.assert_frame_equal(
+        processing.get_running_speed(pizza_data_fixture, **kwargs).iloc[:5],
+        expected,
+        check_column_type=False,
+        check_index_type=False,
+        check_dtype=False,
+        check_like=True
+    )
+
+
+def test_get_stimulus_log(pizza_data_fixture):
+    np.testing.assert_equal(
+        processing.get_stimulus_log(pizza_data_fixture)[:10],
+        np.array([True, True, True, True, True, True, True, True, True, True, ])
+    )
---
+++ visual_behavior/processing.py
@@ -0,0 +1,276 @@
+import logging
+import numpy as np
+import pandas as pd
+
+from scipy.signal import medfilt
+from .analyze import calc_deriv, rad_to_dist
+
+
+logger = logging.getLogger(__name__)
+
+
+"""Most of these are from Derric's translator object in camstim
+
+New idea...no more time or frames sometimes, all output in frames, the
+downstream can handle time pairing
+
+or maybe just use time...this was more complex than i realized? frame to time
+relationship might not be the same for
+"""
+
+
+def behavior_items_or_top_level(exp_data):
+    """Some kind of weird thing related to derrics output...TODO ask him to stop
+    """
+    try:
+        return exp_data["items"]["behavior"]
+    except KeyError:
+        logger.warn("got an unexepcted shape", exc_info=True)
+        return exp_data
+
+
+def get_dx(exp_data):
+    """Get wheel rotation for each frame
+
+    Parameters
+    ----------
+    exp_data : Mapping
+        experiment data
+
+    Returns
+    -------
+    numpy.ndarray
+        log of whether or not a stimulus was presented, 1 dimensional with index
+        representing frame number and value position of the frame
+
+    Notes
+    -----
+    - asssumes data is in the Foraging2 format
+    - no idea what the units are
+    """
+    return behavior_items_or_top_level(exp_data)["encoders"][0]["dx"]
+
+
+def get_licks(exp_data, time=None):
+    """Returns each lick in an experiment.
+
+    Parameters
+    ----------
+    exp_data : Mapping
+        experiment data
+    time : np.array, optional
+        array of times for each stimulus frame
+
+    Returns
+    -------
+    pandas.DataFrame
+        dataframe with `frame` and `time` associated with each lick
+
+    Notes
+    -----
+    - TODO remove the time thing, standardize where time evaluation occurs
+    """
+    lick_frames = [
+        trial["licks"][0][1]
+        for trial in get_trials(exp_data)
+        if trial["licks"]
+    ]
+
+    time = time or get_time(exp_data)  # that coverage syntax tho...
+
+    lick_times = [time[frame] for frame in lick_frames]
+
+    return pd.DataFrame(data={"frame": lick_frames, "time": lick_times, })
+
+
+def get_params(exp_data):
+    """Get params
+
+    Parameters
+    ---------
+    exp_data : Mapping
+        exactly what you think it is...
+
+    Returns
+    -------
+    Mapping:
+        params
+
+    Notes
+    -----
+    - asssumes data is in the Foraging2 format
+    """
+    return exp_data.get("params", {})
+
+
+def get_rewards(exp_data):
+    """Get rewards
+
+    Parameters
+    ----------
+    exp_data: Mapping
+        experiment data
+
+    Returns
+    -------
+    pandas.DataFrame
+        dataframe with `frame` and `time` associated with each reward
+
+    Notes
+    -----
+    - asssumes data is in the Foraging2 format
+    - no idea what the units are
+    """
+    return pd.DataFrame(
+        data=np.array([
+            trial["rewards"][0]
+            for trial in get_trials(exp_data)
+            if trial["rewards"]
+        ], dtype=np.float),
+        columns=["time", "frame", ]
+    )
+
+
+def get_running_speed(exp_data, smooth=False, time=None):
+    """Get running speed
+
+    Parameters
+    ----------
+    exp_data: Mapping
+        experiment data
+    smooth: boolean, default=False
+        not implemented
+    time: numpy.array, default=None
+        array of times for each stimulus frame
+
+    Returns
+    -------
+    pandas.DataFrame
+        dataframe with 'time', 'speed (cm/s)', 'acceleration (cm/s^2)',
+        'jerk (cm/s^3)'
+
+    Notes
+    -----
+    - asssumes data is in the Foraging2 format
+    """
+    if time is None:
+        time = get_time(exp_data)
+        logger.info("`time` not passed. using intervalms as `time`")
+
+    dx = get_dx(exp_data)
+    dx = medfilt(dx, kernel_size=5)  # remove big, single frame spikes in encoder values
+    dx = np.cumsum(dx)  # wheel rotations
+
+    if len(time) != len(dx):
+        raise ValueError("dx and time must be the same length")
+
+    speed = calc_deriv(dx, time)
+    speed = rad_to_dist(speed)
+
+    if smooth:
+        # running_speed_cm_per_sec = pd.rolling_mean(running_speed_cm_per_sec, window=6)
+        raise NotImplementedError
+
+    accel = calc_deriv(speed, time)
+    jerk = calc_deriv(accel, time)
+
+    running_speed = pd.DataFrame({
+        'time': time,
+        'speed (cm/s)': speed,
+        'acceleration (cm/s^2)': accel,
+        'jerk (cm/s^3)': jerk,
+    })
+    return running_speed
+
+
+def get_stimulus_log(exp_data):
+    """Get whether or not a stimulus was presented on a frame by frame basis
+
+    Parameters
+    ----------
+    exp_data: Mapping
+        experiment data
+
+    Returns
+    -------
+    numpy.ndarray
+        log of whether or not a stimulus was presented, 1 dimensional with index
+        representing frame number and value as a boolean whether or not a
+        stimulus was shown
+
+    Notes
+    -----
+    - asssumes data is in the Foraging2 format
+    """
+    return np.array(
+        behavior_items_or_top_level(exp_data)["stimuli"]["natual_scenes"]["draw_log"],
+        dtype=np.bool
+    )  # will likely break in the future, explicit is better here tho...
+
+
+def get_time(exp_data):
+    """Get cumulative time for each vsync frame
+
+    Parameters
+    ----------
+    exp_data: Mapping
+        experiment data
+
+    Returns
+    -------
+    numpy.ndarray
+        cumulative time for each frame in the experiment, 1 dimensional with
+        index representing frame number and value as a float representing time
+        since the start of the experiment
+
+    Notes
+    -----
+    - asssumes data is in the Foraging2 format
+    """
+    return np.hstack((0, get_vsyncs(exp_data))).cumsum()  # cumulative time
+
+
+def get_trials(exp_data):
+    """Get trials
+
+    Parameters
+    ----------
+    exp_data: Mapping
+        experiment data
+
+    Returns
+    -------
+    list of dictionaries
+        each dictionary represents a trial in an experiment session, they are
+        ordered in ascending trial number
+
+    Notes
+    -----
+    - asssumes data is in the Foraging2 format
+    """
+    return behavior_items_or_top_level(exp_data).get("trial_log", [])
+
+
+def get_vsyncs(exp_data):
+    """Get vsync times
+
+    Parameters
+    ----------
+    exp_data: Mapping
+        experiment data
+
+    Returns
+    -------
+    numpy.ndarray
+        cumulative time for each vsync, 1 dimensional with index representing
+        vsync number and value as a float representing time since the start of
+        the experiment
+
+    Notes
+    -----
+    - asssumes data is in the Foraging2 format
+    """
+    return behavior_items_or_top_level(exp_data).get(
+        "intervalsms",
+        [16.0] * behavior_items_or_top_level(exp_data)["update_count"]
+    )  # less stable but that coverage tho...
