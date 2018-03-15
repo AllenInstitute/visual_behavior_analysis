750172fb78a6e32164dec16f3a7d946f2e6cbdbe
Justin Kiggins <justink@alleninstitute.org>
2018-03-14 21:01:29 -0700

    add trialschema

--- visual_behavior/validation.py
+++ visual_behavior/validation.py
@@ -5,7 +5,7 @@ class TimeSeriesSchema(Schema):
     frame = fields.Int(required=True)
     time = fields.Float(required=True)
 
-
+    
 class RewardSchema(TimeSeriesSchema):
     pass
 
@@ -16,8 +16,38 @@ class LickSchema(TimeSeriesSchema):
 
 class RunningSchema(TimeSeriesSchema):
     speed = fields.Float(required=True)
-    acceleration = fields.Float(required=True)
-    jerk = fields.Float(required=True)
+
+
+class ChangeDetectionTrialSchema(Schema):
+    auto_rewarded = fields.Bool(required=True, allow_none=True)
+    change_contrast = fields.Int(required=True)
+    change_frame = fields.Float(required=True)
+    change_image_category = fields.String(required=True, allow_none=True)
+    change_image_name = fields.String(required=True, allow_none=True)
+    change_ori = fields.Float(required=True)
+    change_time = fields.Float(required=True)
+    cumulative_reward_number = fields.Int(required=True)
+    cumulative_volume = fields.Float(required=True)
+    delta_ori = fields.Float(required=True)
+    index = fields.Int(required=True)
+    initial_contrast = fields.Float(required=True)
+    initial_image_category = fields.String(required=True)
+    initial_image_name = fields.String(required=True)
+    initial_ori = fields.Float(required=True)
+    lick_times = fields.List(fields.Float, required=True)
+    optogenetics = fields.Bool(required=True)
+    publish_time = fields.Str(required=True)
+    response_latency = fields.Float(required=True)
+    response_time = fields.List(fields.Float, required=True)
+    response_type = fields.List(fields.Bool, required=True)
+    reward_frames = fields.List(fields.Int, required=True)
+    reward_times = fields.List(fields.Float, required=True)
+    reward_volume = fields.Float(required=True)
+    rewarded = fields.Bool(required=True)
+    scheduled_change_time = fields.Float(required=True)
+    startframe = fields.Int(required=True)
+    starttime = fields.Float(required=True)
+    stim_on_frames = fields.List(fields.Bool, required=True)
 
 
 def dataframe_validator(row,schema=None):
