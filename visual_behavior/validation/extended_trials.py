1fdf97fa83399ae4e622b7138893c66b1fb472de
Doug Ollerenshaw <dougo@alleninstute.org>
2018-05-04 23:27:01 -0700

    QC: Validate that frame intervals are logged

--- visual_behavior/validation/extended_trials.py
+++ visual_behavior/validation/extended_trials.py
@@ -715,3 +715,10 @@ def validate_licks(core_data):
     validate that licks exist
     '''
     return len(core_data['licks'])>0
+
+def validate_frame_intervals_exists(data):
+    '''
+    ensure that frame intervals exist in PKL file
+    takes full pickled data object as input
+    '''
+    return len(data['items']['behavior']['intervalsms'])>0
