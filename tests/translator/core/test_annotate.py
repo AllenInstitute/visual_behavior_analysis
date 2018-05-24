038e5d67af30be75f5b45a340274e3a6ba640a71
Justin Kiggins <justink@alleninstitute.org>
2018-05-24 16:31:37 -0700

    adds test & fixes bug

--- tests/translator/core/test_annotate.py
+++ tests/translator/core/test_annotate.py
@@ -1,6 +1,8 @@
+import numpy as np
 import pandas as pd
-from pandas.testing import assert_series_equal
-from visual_behavior.translator.core.annotate import annotate_startdatetime, make_deterministic_session_uuid
+from pandas.testing import assert_series_equal, assert_frame_equal
+from visual_behavior.translator.core.annotate import annotate_startdatetime, \
+    make_deterministic_session_uuid, make_trials_contiguous
 
 def test_annotate_startdatetime():
 
@@ -31,3 +33,26 @@ def test_make_deterministic_session_uuid():
     behavior_session_uuid = make_deterministic_session_uuid(metadata)
 
     assert str(behavior_session_uuid) == EXPECTED_UUID
+
+def test_make_trials_contiguous():
+
+    EXPECTED_TRIALS = pd.DataFrame(dict(
+        starttime=[0, 0.1, 0.2],
+        endtime=[0.1, 0.2, 0.99],
+        startframe=[0, 10, 20],
+        endframe=[10, 20, 99],
+        trial_length=[0.1, 0.1, 0.79],
+    ))
+
+    input_trials = pd.DataFrame(dict(
+        starttime=[0, 0.1, 0.2],
+        endtime=[0.05, 0.15, 0.25],
+        startframe=[0, 10, 20],
+        endframe=[5, 15, 25],
+    ))
+
+    time = np.arange(0,1,0.01)
+
+    output_trials = make_trials_contiguous(input_trials,time)
+
+    assert_frame_equal(output_trials, EXPECTED_TRIALS)
