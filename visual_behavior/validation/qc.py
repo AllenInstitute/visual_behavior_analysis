995414e519daf7fa7918bae38824af208df935ac
nicholasc <nicain.seattle@gmail.com>
2018-06-01 13:04:11 -0700

    correcting location of periodic_flash in core_data

--- visual_behavior/validation/qc.py
+++ visual_behavior/validation/qc.py
@@ -44,7 +44,7 @@ def define_validation_functions(core_data):
     STIMULUS_WINDOW = core_data['metadata']['stimulus_window']
     VOLUME_LIMIT = core_data['metadata']['volume_limit']
 
-    PERIODIC_FLASH = et.fix_periodic_flash(core_data['metadata']['periodic_flash'])
+    PERIODIC_FLASH = et.fix_periodic_flash(core_data['metadata']['params']['periodic_flash'])
 
     validation_functions = {
         # et.validate_schema
