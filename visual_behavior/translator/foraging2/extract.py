aeb3e4663902d51fc995fac4de7f4cbe3e573964
Justin Kiggins <justink@alleninstitute.org>
2018-07-09 17:37:03 -0700

    fixes style

--- visual_behavior/translator/foraging2/extract.py
+++ visual_behavior/translator/foraging2/extract.py
@@ -1355,6 +1355,7 @@ def get_catch_frequency(data):
     """
     return data["items"]["behavior"]["config"]["DoC"]["catch_freq"]
 
+
 def get_auto_reward_delay(data):
     """Get the auto reward delay set for a foraging2 behavior session
 
@@ -1367,7 +1368,7 @@ def get_auto_reward_delay(data):
     -------
     float
     """
-    return data["items"]["behavior"]["config"]["DoC"].get("auto_reward_delay",0.0)
+    return data["items"]["behavior"]["config"]["DoC"].get("auto_reward_delay", 0.0)
 
 
 def get_free_reward_trials(data):
