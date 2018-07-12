cf40b41dc8dda705b5530eac8700da9aaec04e42
Justin Kiggins <justin.kiggins@gmail.com>
2018-07-11 21:28:53 -0700

    updates tests

--- tests/test_devices.py
+++ tests/test_devices.py
@@ -5,7 +5,8 @@ from visual_behavior import devices
 
 @pytest.mark.parametrize("kwargs, expected", [
     ({"in_val": "W7DTMJ19R2F", "input_type": "computer_name", }, "A1", ),
-    ({"in_val": "A2", "input_type": "rig_id", }, "W7DTMJ35Y0T", ),
+    ({"in_val": "w7dtmj19r2f", "input_type": "computer_name", }, "A1", ),
+    ({"in_val": "A2", "input_type": "rig_id", }, "w7dtmj35y0t", ),
     ({"in_val": "NOT_A_NAME", "input_type": "computer_name", }, "unknown", ),
 ])
 def test_get_rid_id(kwargs, expected):
