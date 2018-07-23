39fda6b24e7b17d1c26e4c81f9627df3fcd2629c
nicholasc <nicain.seattle@gmail.com>
2018-07-23 12:02:34 -0700

    updating test work

--- tests/test_ophys.py
+++ tests/test_ophys.py
@@ -4,8 +4,11 @@ import py
 import os
 
 from visual_behavior.ophys.io.convert_level_1_to_level_2 import convert_level_1_to_level_2
+from visual_behavior.ophys.dataset.visual_behavior_ophys_dataset import VisualBehaviorOphysDataset
+from visual_behavior.ophys.response_analysis.response_analysis import ResponseAnalysis
 
-@pytest.fixture(scope='function')
+
+@pytest.fixture(scope='module')
 def tmpdir(request):
     """
     Get a temp dir to build in
@@ -17,7 +20,22 @@ def tmpdir(request):
     return dir
 
 @pytest.mark.skipif(not os.path.exists('/allen/programs/braintv/production/neuralcoding/'), reason='Source files not available')
-def test_generate_level_1(tmpdir):
+def test_generate_level_2(tmpdir):
     
     cache_dir = os.path.join(str(tmpdir))
     convert_level_1_to_level_2(702134928, cache_dir=cache_dir)
+
+
+@pytest.mark.skipif(not os.path.exists('/allen/programs/braintv/production/neuralcoding/'), reason='Source files not available')
+def test_read_level_2_write_level_3(tmpdir):
+    
+    experiment_id = 702134928
+    cache_dir = os.path.join(str(tmpdir))
+    assert os.path.exists(cache_dir)
+    dataset = VisualBehaviorOphysDataset(experiment_id, cache_dir=cache_dir)
+    assert dataset.cache_dir == cache_dir
+
+    ResponseAnalysis(dataset)
+
+
+    
