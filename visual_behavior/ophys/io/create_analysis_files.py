b2e3c1ce802758f2ce8945ff6537e031453ad07b
matchings <marinag@alleninstitute.org>
2018-08-02 13:43:44 -0700

    fix directory formatting for cluster jobs

--- visual_behavior/ophys/io/create_analysis_files.py
+++ visual_behavior/ophys/io/create_analysis_files.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
     import sys
 
     experiment_id = sys.argv[1]
-    cache_dir = r'\\allen\programs\braintv\workgroups\nc-ophys\visual_behavior\visual_behavior_pilot_analysis'
-    create_analysis_files(experiment_id, cache_dir, overwrite_analysis_files=True)
+    cache_dir = r'/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/visual_behavior_pilot_analysis'
+    create_analysis_files(experiment_id, overwrite_analysis_files=True)
 
     # import pandas as pd
     # manifest = r'\\allen\aibs\informatics\swdb2018\visual_behavior\visual_behavior_data_manifest.csv'
