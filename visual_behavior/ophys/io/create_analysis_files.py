470852d80bb457ced64feeb56c2aa669a37c1187
matchings <marinag@alleninstitute.org>
2018-08-02 14:35:31 -0700

    fix create_analysis_files inputs

--- visual_behavior/ophys/io/create_analysis_files.py
+++ visual_behavior/ophys/io/create_analysis_files.py
@@ -2,8 +2,8 @@ from visual_behavior.ophys.dataset.visual_behavior_ophys_dataset import VisualBe
 from visual_behavior.ophys.response_analysis.response_analysis import ResponseAnalysis
 
 
-def create_analysis_files(experiment_id, overwrite_analysis_files=False):
-    dataset = VisualBehaviorOphysDataset(experiment_id)
+def create_analysis_files(experiment_id, cache_dir, overwrite_analysis_files=False):
+    dataset = VisualBehaviorOphysDataset(experiment_id, cache_dir)
     analysis = ResponseAnalysis(dataset, overwrite_analysis_files)
 
     print('plotting cell responses')
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     experiment_id = sys.argv[1]
     cache_dir = r'/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/visual_behavior_pilot_analysis'
-    create_analysis_files(experiment_id, overwrite_analysis_files=True)
+    create_analysis_files(experiment_id, cache_dir, overwrite_analysis_files=True)
 
     # import pandas as pd
     # manifest = r'\\allen\aibs\informatics\swdb2018\visual_behavior\visual_behavior_data_manifest.csv'
