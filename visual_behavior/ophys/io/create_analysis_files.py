df5fc76447998f9beb78e83cfaa27548cc0c24c1
matchings <marinag@alleninstitute.org>
2018-08-14 11:38:07 -0700

    cluster jobs for informatics directory

--- visual_behavior/ophys/io/create_analysis_files.py
+++ visual_behavior/ophys/io/create_analysis_files.py
@@ -22,8 +22,8 @@ if __name__ == '__main__':
     import sys
 
     experiment_id = sys.argv[1]
-    cache_dir = r'/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/visual_behavior_pilot_analysis'
-    # cache_dir = r'/allen/aibs/informatics/swdb2018/visual_behavior'
+    # cache_dir = r'/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/visual_behavior_pilot_analysis'
+    cache_dir = r'/allen/aibs/informatics/swdb2018/visual_behavior'
     create_analysis_files(experiment_id, cache_dir, overwrite_analysis_files=True)
 
     # import pandas as pd
