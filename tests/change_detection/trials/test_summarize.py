e148a26fc7cda1c7315d8f27d9c492c61939dd6a
Justin Kiggins <justin.kiggins@gmail.com>
2018-09-19 12:38:03 -0700

    check like

--- tests/change_detection/trials/test_summarize.py
+++ tests/change_detection/trials/test_summarize.py
@@ -6,11 +6,21 @@ def test_session_level_summary(mock_trials_fixture, session_summary):
     summary = summarize.session_level_summary(mock_trials_fixture)
     print(summary)
     print(session_summary)
-    assert_frame_equal(summary, session_summary, check_names=False)
+    assert_frame_equal(
+        summary,
+        session_summary,
+        check_names=False,
+        check_like=True,
+    )
 
 
 def test_epoch_level_summary(mock_trials_fixture,epoch_summary):
     summary = summarize.epoch_level_summary(mock_trials_fixture)
     print(summary)
     print(epoch_summary)
-    assert_frame_equal(summary, epoch_summary, check_names=False)
+    assert_frame_equal(
+        summary,
+        epoch_summary,
+        check_names=False,
+        check_like=True,
+    )
