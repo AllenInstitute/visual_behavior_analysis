2456fbabb943b5d533440f112d56b53143027d99
matchings <marinag@alleninstitute.org>
2018-09-24 18:38:42 -0700

    make create_analysis_files cluster compatible

--- visual_behavior/ophys/io/create_analysis_files.py
+++ visual_behavior/ophys/io/create_analysis_files.py
@@ -55,12 +55,12 @@ def create_analysis_files(experiment_id, cache_dir, overwrite_analysis_files=Tru
 
 
 if __name__ == '__main__':
-    # import sys
-    #
-    # experiment_id = sys.argv[1]
-    # cache_dir = r'/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/visual_behavior_pilot_analysis'
-    # create_analysis_files(experiment_id, cache_dir, overwrite_analysis_files=True)
+    import sys
 
-    experiment_id = 747248249
-    cache_dir = r'\\allen\programs\braintv\workgroups\nc-ophys\visual_behavior\visual_behavior_pilot_analysis'
+    experiment_id = sys.argv[1]
+    cache_dir = r'/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/visual_behavior_pilot_analysis'
     create_analysis_files(experiment_id, cache_dir, overwrite_analysis_files=True)
+
+    # experiment_id = 747248249
+    # cache_dir = r'\\allen\programs\braintv\workgroups\nc-ophys\visual_behavior\visual_behavior_pilot_analysis'
+    # create_analysis_files(experiment_id, cache_dir, overwrite_analysis_files=True)
