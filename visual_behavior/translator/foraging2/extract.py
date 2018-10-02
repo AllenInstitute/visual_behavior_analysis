1b87108539b5d58f765dac01b4803a50ed0850a0
Jed Perkins <perkins.jed@gmail.com>
2018-10-01 19:18:46 -0700

    Seperate running speed computation from data source and dataframe generation

--- visual_behavior/translator/foraging2/extract.py
+++ visual_behavior/translator/foraging2/extract.py
@@ -5,7 +5,7 @@ from copy import deepcopy
 from six import iteritems
 
 from scipy.signal import medfilt
-from ...analyze import calc_deriv, rad_to_dist
+from ...analyze import calc_deriv, compute_running_speed
 
 
 logger = logging.getLogger(__name__)
@@ -763,26 +763,16 @@ def get_running_speed(exp_data, smooth=False, time=None):
         time = get_time(exp_data)
         logger.info("`time` not passed. using intervalms as `time`")
 
-    dx_raw = get_dx(exp_data)
-    dx = medfilt(dx_raw, kernel_size=5)  # remove big, single frame spikes in encoder values
-    dx = np.cumsum(dx)  # wheel rotations
-
     v_sig = get_vsig(exp_data)
     v_in = get_vin(exp_data)
 
-    if len(time) < len(dx):
+    if len(time) < len(dx_raw):
         logger.error('intervalsms record appears to be missing entries')
-        dx = dx[:len(time)]
         dx_raw = dx_raw[:len(time)]
         v_sig = v_sig[:len(time)]
         v_in = v_in[:len(time)]
 
-    speed = calc_deriv(dx, time)
-    speed = rad_to_dist(speed)
-
-    if smooth:
-        # running_speed_cm_per_sec = pd.rolling_mean(running_speed_cm_per_sec, window=6)
-        raise NotImplementedError
+    speed = compute_running_speed(dx_raw, time, v_sig, v_in)
 
     # accel = calc_deriv(speed, time)
     # jerk = calc_deriv(accel, time)
