dfb5644eb73487734b34521afa5d2fe1ceb152bd
matchings <marinag@alleninstitute.org>
2018-10-07 19:23:30 -0700

    conversion on cluster for integration test data

--- scripts/convert_level_1_to_level_2.py
+++ scripts/convert_level_1_to_level_2.py
@@ -3,11 +3,12 @@ from visual_behavior.ophys.io.convert_level_1_to_level_2 import convert_level_1_
 
 
 if __name__ == '__main__':
-
+    import sys
     import matplotlib
     matplotlib.use('Agg')
 
-    cache_dir = r'\\allen\programs\braintv\workgroups\nc-ophys\visual_behavior\visual_behavior_pilot_analysis'
-    # cache_dir = r'\\allen\programs\braintv\workgroups\ophysdev\OPhysCore\Analysis\2018-08 - Behavior Integration test'
-    experiment_id = 736490031
+    experiment_id = sys.argv[1]
+    # experiment_id = 736490031
+    # cache_dir = r'\\allen\programs\braintv\workgroups\nc-ophys\visual_behavior\visual_behavior_pilot_analysis'
+    cache_dir = r'/allen/programs/braintv/workgroups/ophysdev/OPhysCore/Analysis/2018-08 - Behavior Integration test'
     ophys_data = convert_level_1_to_level_2(experiment_id, cache_dir)
