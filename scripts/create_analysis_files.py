cd901e3bda5516f6ba4c4c173701f59a570003d6
matchings <marinag@alleninstitute.org>
2018-10-29 19:48:50 -0700

    change cache_dir

--- scripts/create_analysis_files.py
+++ scripts/create_analysis_files.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     import sys
 
     experiment_id = sys.argv[1]
-    # cache_dir = r'/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/visual_behavior_pilot_analysis'
-    cache_dir = r'/allen/programs/braintv/workgroups/ophysdev/OPhysCore/Analysis/2018-08 - Behavior Integration test'
+    cache_dir = r'/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/visual_behavior_pilot_analysis'
+    # cache_dir = r'/allen/programs/braintv/workgroups/ophysdev/OPhysCore/Analysis/2018-08 - Behavior Integration test'
     create_analysis_files(experiment_id, cache_dir, overwrite_analysis_files=True)
