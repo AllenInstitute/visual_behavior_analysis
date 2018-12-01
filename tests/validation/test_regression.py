e61fb8b3073b7e83d5e6eb056b445a499e8ac9d3
Doug Ollerenshaw <dougo@alleninstute.org>
2018-11-30 16:43:56 -0800

    remove one test fixture

--- tests/validation/test_regression.py
+++ tests/validation/test_regression.py
@@ -11,7 +11,6 @@ from visual_behavior.validation.qc import generate_qc_report
 sessions = {
     'TRAINING_1_gratings_412629':'//allen/programs/braintv/workgroups/nc-ophys/visual_behavior/test_fixtures/181119092559_412629_a3775e3e-e1ca-474a-b413-91cccd6d886f.pkl',
     'TRAINING_1_gratings_421137': '//allen/programs/braintv/workgroups/nc-ophys/visual_behavior/test_fixtures/181119102010_421137_c108dc71-ef5e-46ad-8d85-8da0fdaf7d3d.pkl',
-    'TRAINING_1_gratings_412619': '//allen/programs/braintv/workgroups/nc-ophys/visual_behavior/test_fixtures/181119092559_412629_a3775e3e-e1ca-474a-b413-91cccd6d886f.pkl',
     'TRAINING_2_gratings_flashed_416656': '//allen/programs/braintv/workgroups/nc-ophys/visual_behavior/test_fixtures/181119150503_416656_2b0893fe-843d-495e-bceb-83b13f2b02dc.pkl',
     'TRAINING_3_images_A_10uL_reward_424460': '//allen/programs/braintv/workgroups/nc-ophys/visual_behavior/test_fixtures/181119135416_424460_b6daf247-2caf-4f38-9eb1-ab97825923cd.pkl',
     'TRAINING_4_images_A_handoff_ready_402329': '//allen/programs/braintv/workgroups/nc-ophys/visual_behavior/test_fixtures/181119134201_402329_b75a87d0-8178-4171-a3b2-7cea3ae8e118.pkl',
