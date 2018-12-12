1cee58f1db7479c9dbc52f2f228269ce0ccc202d
matchings <marinag@alleninstitute.org>
2018-12-12 15:32:14 -0800

    update event detection script location

--- scripts/run_event_detection.py
+++ scripts/run_event_detection.py
@@ -1,7 +1,7 @@
 import sys
 import platform
-# if platform.system() == 'Linux':
-sys.path.append('/allen/programs/braintv/workgroups/nc-ophys/Doug/pbstools')
+if platform.system() == 'Linux':
+    sys.path.append('/allen/programs/braintv/workgroups/nc-ophys/Doug/pbstools')
 from pbstools import PythonJob # flake8: noqa: E999
 
 #VisualBehaviorDevelopment - complete dataset as of 11/15/18
@@ -33,7 +33,7 @@ lims_ids = [754566180,756565411,
        773816712, 773843260, 774370025, 774379465, 775011398, 775429615,
        776042634]
 
-python_file = r"/home/marinag/visual_behavior_analysis/visual_behavior/ophys/io/event_detection.py"
+python_file = r"/home/marinag/visual_behavior_analysis/scripts/event_detection.py"
 
 jobdir = '/allen/programs/braintv/workgroups/nc-ophys/Marina/ClusterJobs/JobRecords2'
 
