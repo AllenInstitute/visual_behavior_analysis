9d2b45321662438a2b921945affc08790f0512c6
matchings <marinag@alleninstitute.org>
2018-12-14 12:02:37 -0800

    add try except for creating multi session df

--- visual_behavior/ophys/io/create_multi_session_mean_df.py
+++ visual_behavior/ophys/io/create_multi_session_mean_df.py
@@ -17,31 +17,34 @@ def get_multi_session_mean_df(experiment_ids, cache_dir,
         print(experiment_id)
         dataset = VisualBehaviorOphysDataset(experiment_id, cache_dir=cache_dir)
         analysis = ResponseAnalysis(dataset, use_events=use_events)
-        if flashes:
-            if analysis.get_flash_response_df_path().split('/')[-1] in os.listdir(dataset.analysis_dir):
-                if 'repeat' in conditions:
-                    flash_response_df = analysis.flash_response_df.copy()
-                    flash_response_df = flash_response_df[flash_response_df.repeat.isin([1, 5, 10, 15])]
+        try:
+            if flashes:
+                if analysis.get_flash_response_df_path().split('/')[-1] in os.listdir(dataset.analysis_dir):
+                    if 'repeat' in conditions:
+                        flash_response_df = analysis.flash_response_df.copy()
+                        flash_response_df = flash_response_df[flash_response_df.repeat.isin([1, 5, 10, 15])]
+                    else:
+                        flash_response_df = analysis.flash_response_df.copy()
+                    flash_response_df['engaged'] = [True if reward_rate > 2 else False for reward_rate in
+                                                    flash_response_df.reward_rate.values]
+                    mdf = ut.get_mean_df(flash_response_df,
+                                         conditions=conditions, flashes=True)
+                    mdf['experiment_id'] = dataset.experiment_id
+                    mdf = ut.add_metadata_to_mean_df(mdf, dataset.metadata)
+                    mega_mdf = pd.concat([mega_mdf, mdf])
                 else:
-                    flash_response_df = analysis.flash_response_df.copy()
-                flash_response_df['engaged'] = [True if reward_rate > 2 else False for reward_rate in
-                                                flash_response_df.reward_rate.values]
-                mdf = ut.get_mean_df(flash_response_df,
-                                     conditions=conditions, flashes=True)
-                mdf['experiment_id'] = dataset.experiment_id
-                mdf = ut.add_metadata_to_mean_df(mdf, dataset.metadata)
-                mega_mdf = pd.concat([mega_mdf, mdf])
+                    print('problem with',analysis.get_flash_response_df_path().split('/')[-1],'for',experiment_id)
             else:
-                print('problem with',analysis.get_flash_response_df_path().split('/')[-1],'for',experiment_id)
-        else:
-            if analysis.get_trial_response_df_path().split('/')[-1] in os.listdir(dataset.analysis_dir):
-                mdf = ut.get_mean_df(analysis.trial_response_df,
-                                     conditions=conditions)
-                mdf['experiment_id'] = dataset.experiment_id
-                mdf = ut.add_metadata_to_mean_df(mdf, dataset.metadata)
-                mega_mdf = pd.concat([mega_mdf, mdf])
-            else:
-                print('problem with',analysis.get_trial_response_df_path().split('/')[-1],'for',experiment_id)
+                if analysis.get_trial_response_df_path().split('/')[-1] in os.listdir(dataset.analysis_dir):
+                    mdf = ut.get_mean_df(analysis.trial_response_df,
+                                         conditions=conditions)
+                    mdf['experiment_id'] = dataset.experiment_id
+                    mdf = ut.add_metadata_to_mean_df(mdf, dataset.metadata)
+                    mega_mdf = pd.concat([mega_mdf, mdf])
+                else:
+                    print('problem with',analysis.get_trial_response_df_path().split('/')[-1],'for',experiment_id)
+        except:
+            print('problem for',experiment_id)
     if flashes:
         type = '_flashes_'
     else:
