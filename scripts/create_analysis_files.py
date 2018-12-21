431a43e43ebf050ca66234b73f15e3373c59768b
matchings <marinag@alleninstitute.org>
2018-12-20 23:29:06 -0800

    dont overwrite analysis files

--- scripts/create_analysis_files.py
+++ scripts/create_analysis_files.py
@@ -13,4 +13,4 @@ if __name__ == '__main__':
     experiment_id = sys.argv[1]
     cache_dir = r'/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/visual_behavior_pilot_analysis'
     # cache_dir = r'/allen/programs/braintv/workgroups/ophysdev/OPhysCore/Analysis/2018-08 - Behavior Integration test'
-    create_analysis_files(experiment_id, cache_dir, overwrite_analysis_files=True)
+    create_analysis_files(experiment_id, cache_dir, overwrite_analysis_files=False)
