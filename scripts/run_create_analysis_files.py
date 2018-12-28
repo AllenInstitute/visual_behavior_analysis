4e093a857399032f1a9bd3b65b709f9d0694b386
matchings <marinag@alleninstitute.org>
2018-12-27 17:21:26 -0800

    try with less wall time

--- scripts/run_create_analysis_files.py
+++ scripts/run_create_analysis_files.py
@@ -62,8 +62,8 @@ python_file = r"/home/marinag/visual_behavior_analysis/scripts/create_analysis_f
 jobdir = '/allen/programs/braintv/workgroups/nc-ophys/Marina/ClusterJobs/JobRecords2'
 
 job_settings = {'queue': 'braintv',
-                'mem': '60g',
-                'walltime': '32:00:00',
+                'mem': '30g',
+                'walltime': '1:00:00',
                 'ppn': 1,
                 'jobdir': jobdir,
                 }
