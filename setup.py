8933f1faef45cf33886f05455857c4c6c5c7900e
nicholasc <nicain.seattle@gmail.com>
2019-01-23 09:51:53 -0800

    Bump version: 0.5.0.a4 → 0.6.0.dev0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="visual-behavior",
-    version="0.5.0.a4",
+    version="0.6.0.dev0",
     author="Justin Kiggins",
     author_email="justink@alleninstitute.org",
     description="analysis package for visual behavior",
