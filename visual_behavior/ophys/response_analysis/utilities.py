5e606c169ae4fd7dd60295e1a98b7e1f85bba238
matchings <marinag@alleninstitute.org>
2019-02-15 23:25:02 -0800

    reliability for trials only

--- visual_behavior/ophys/response_analysis/utilities.py
+++ visual_behavior/ophys/response_analysis/utilities.py
@@ -175,9 +175,10 @@ def get_mean_df(response_df, analysis=None, conditions=['cell', 'change_image_na
     fraction_nonzero_trials = fraction_nonzero_trials.reset_index()
     mdf['fraction_nonzero_trials'] = fraction_nonzero_trials.fraction_nonzero_trials
 
-    # reliability = rdf.groupby(conditions).apply(get_reliability)
-    # reliability = reliability.reset_index()
-    # mdf['reliability'] = reliability.reliability
+    if not flashes: 
+        reliability = rdf.groupby(conditions).apply(get_reliability)
+        reliability = reliability.reset_index()
+        mdf['reliability'] = reliability.reliability
 
     return mdf
 
