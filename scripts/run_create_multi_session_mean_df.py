5d617bd441d451f52cc22d6957a9c57bee3bd70b
matchings <marinag@alleninstitute.org>
2019-02-18 18:35:04 -0800

    generate multi session dfs with matched cell IDs

--- scripts/run_create_multi_session_mean_df.py
+++ scripts/run_create_multi_session_mean_df.py
@@ -9,7 +9,7 @@ python_file = r"/home/marinag/visual_behavior_analysis/scripts/create_multi_sess
 jobdir = '/allen/programs/braintv/workgroups/nc-ophys/Marina/ClusterJobs/JobRecords2'
 
 job_settings = {'queue': 'braintv',
-                'mem': '60g',
+                'mem': '100g',
                 'walltime': '8:00:00',
                 'ppn': 1,
                 'jobdir': jobdir,
