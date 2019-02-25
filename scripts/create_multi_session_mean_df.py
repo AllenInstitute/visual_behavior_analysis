e1719e4e043e980fcd8fa65550f466d0e4796e46
matchings <marinag@alleninstitute.org>
2019-02-24 17:28:26 -0800

    generate multi session df

--- scripts/create_multi_session_mean_df.py
+++ scripts/create_multi_session_mean_df.py
@@ -37,14 +37,14 @@ if __name__ == '__main__':
     #                           conditions=['cell_specimen_id', 'image_name'], flashes=True, omitted=True, get_reliability=False)
     # get_multi_session_mean_df(experiment_ids, cache_dir,
     #                           conditions=['cell_specimen_id', 'image_name'], flashes=True, get_reliability=False)
+    get_multi_session_mean_df(experiment_ids, cache_dir,
+                              conditions=['cell_specimen_id', 'image_name', 'repeat'], flashes=True, get_reliability=True)
     # get_multi_session_mean_df(experiment_ids, cache_dir,
-    #                           conditions=['cell_specimen_id', 'image_name', 'repeat'], flashes=True, get_reliability=True)
-    get_multi_session_mean_df(experiment_ids, cache_dir,
-                              conditions=['cell_specimen_id', 'image_name', 'engaged', 'repeat'], flashes=True,
-                              get_reliability=False)
-    get_multi_session_mean_df(experiment_ids, cache_dir,
-                              conditions=['cell_specimen_id', 'image_name', 'engaged', 'repeat'], flashes=True,
-                              get_reliability=True)
+    #                           conditions=['cell_specimen_id', 'image_name', 'engaged', 'repeat'], flashes=True,
+    #                           get_reliability=False)
+    # get_multi_session_mean_df(experiment_ids, cache_dir,
+    #                           conditions=['cell_specimen_id', 'image_name', 'engaged', 'repeat'], flashes=True,
+    #                           get_reliability=True)
     # get_multi_session_mean_df(experiment_ids, cache_dir,
     #                           conditions=['cell_specimen_id', 'change_image_name', 'trial_type'], get_reliability=False)
     # get_multi_session_mean_df(experiment_ids, cache_dir,
