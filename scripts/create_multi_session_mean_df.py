12d812156595e9462ae7523c21d11c1ab1de7612
matchings <marinag@alleninstitute.org>
2019-03-03 15:32:16 -0800

    cluster run

--- scripts/create_multi_session_mean_df.py
+++ scripts/create_multi_session_mean_df.py
@@ -34,9 +34,9 @@ if __name__ == '__main__':
 
 
     get_multi_session_mean_df(experiment_ids, cache_dir,
-                              conditions=['cell_specimen_id', 'image_name'], flashes=True, omitted=True, get_reliability=False)
+                              conditions=['cell_specimen_id', 'image_name', 'engaged'], flashes=True, omitted=True, get_reliability=False)
     get_multi_session_mean_df(experiment_ids, cache_dir,
-                              conditions=['cell_specimen_id', 'image_name'], flashes=True, get_reliability=False)
+                              conditions=['cell_specimen_id', 'image_name', 'engaged'], flashes=True, get_reliability=False)
     # get_multi_session_mean_df(experiment_ids, cache_dir,
     #                           conditions=['cell_specimen_id', 'image_name', 'repeat'], flashes=True, get_reliability=True)
     # get_multi_session_mean_df(experiment_ids, cache_dir,
