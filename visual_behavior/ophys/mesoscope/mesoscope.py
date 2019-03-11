e6d77338aa77952bff14a715f65c43ba4bb9a971
Natalia Orlova <nataliao@allenisntitute.org>
2019-03-10 22:49:54 -0700

    fix full field offfine path

--- visual_behavior/ophys/mesoscope/mesoscope.py
+++ visual_behavior/ophys/mesoscope/mesoscope.py
@@ -4,6 +4,9 @@ import psycopg2.extras
 import pandas as pd
 import logging
 import json
+import tifffile
+import numpy as np
+import os
 
 
 logger = logging.getLogger(__name__)
@@ -11,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 class MesoscopeDataset(object):
 
-    def __init__(self, session_id, experiment_id):
+    def __init__(self, session_id='', experiment_id=''):
 
         self.session_id = session_id
         self.experiment_id = experiment_id
@@ -22,8 +25,6 @@ class MesoscopeDataset(object):
         self._port = 5432
         self._username = 'limsreader'
         self._password = 'limsro'
-
-        # initialize other attributes here
 
     def psycopg2_select(self, query):
 
@@ -42,99 +43,106 @@ class MesoscopeDataset(object):
 
         return response
 
-    def get_mesoscope_session_data(self, session_id):
+    def get_mesoscope_session_data(self, session_id=''):
+        if session_id != '':
+            lims_data = []
+            self.session_id = session_id
+            try:
+                query = ' '.join((
+                    "SELECT oe.id as experiment_id, os.id as session_id",
+                    ", os.storage_directory as session_folder, oe.storage_directory as experiment_folder",
+                    ", sp.name as specimen",
+                    ", os.date_of_acquisition as date",
+                    ", imaging_depths.depth as depth",
+                    ", st.acronym as structure",
+                    ", os.parent_session_id as parent_id",
+                    ", oe.workflow_state",
+                    ", os.stimulus_name as stimulus",
+                    " FROM ophys_experiments oe",
+                    "join ophys_sessions os on os.id = oe.ophys_session_id "
+                    "join specimens sp on sp.id = os.specimen_id "
+                    "join projects p on p.id = os.project_id "
+                    "join imaging_depths on imaging_depths.id = oe.imaging_depth_id "
+                    "join structures st on st.id = oe.targeted_structure_id "
+                    "where p.code = 'MesoscopeDevelopment' and (oe.workflow_state = 'processing' or oe.workflow_state = 'qc') and os.workflow_state ='uploaded' "
+                    " and os.id='{}'  ",
+                ))
 
-        try:
-
-            query = ' '.join((
-                "SELECT oe.id as experiment_id, os.id as session_id",
-                ", os.storage_directory as session_folder, oe.storage_directory as experiment_folder",
-                ", sp.name as specimen",
-                ", os.date_of_acquisition as date",
-                ", imaging_depths.depth as depth",
-                ", st.acronym as structure",
-                ", os.parent_session_id as parent_id",
-                ", oe.workflow_state",
-                ", os.stimulus_name as stimulus",
-                " FROM ophys_experiments oe",
-                "join ophys_sessions os on os.id = oe.ophys_session_id "
-                "join specimens sp on sp.id = os.specimen_id "
-                "join projects p on p.id = os.project_id "
-                "join imaging_depths on imaging_depths.id = oe.imaging_depth_id "
-                "join structures st on st.id = oe.targeted_structure_id "
-                "where p.code = 'MesoscopeDevelopment' and (oe.workflow_state = 'processing' or oe.workflow_state = 'qc') and os.workflow_state ='uploaded' "
-                " and os.id='{}'  ",
-            ))
-
-            lims_data = self.psycopg2_select(query.format(session_id))
-
-            if lims_data == []:
+                lims_data = self.psycopg2_select(query.format(session_id))
+                if lims_data == []:
+                    self.data_present = False
+                else:
+                    self.data_pointer = lims_data
+                    self.data_present = True
+            except Exception as e:
+                logger.error("Unable to query LIMS database: {}".format(e))
                 self.data_present = False
-            else:
-                self.data_pointer = lims_data
-                self.data_present = True
-
-        except Exception as e:
-            logger.error("Unable to query LIMS database: {}".format(e))
-            self.data_present = False
+        else:
+            lims_data = []
+            logger.error("Provide Session ID")
 
         return lims_data
 
-    def get_mesoscope_experiment_data(self, experiment_id):
+    def get_mesoscope_experiment_data(self, experiment_id=''):
 
-        try:
+        if experiment_id != '':
+            lims_data = []
+            self.experiment_id = experiment_id
+            try:
 
-            query = ' '.join((
-                "SELECT oe.id as experiment_id, os.id as session_id",
-                ", os.storage_directory as session_folder, oe.storage_directory as experiment_folder",
-                ", sp.name as specimen",
-                ", os.date_of_acquisition as date",
-                ", imaging_depths.depth as depth",
-                ", st.acronym as structure",
-                ", os.parent_session_id as parent_id",
-                ", oe.workflow_state",
-                ", os.stimulus_name as stimulus",
-                " FROM ophys_experiments oe",
-                "join ophys_sessions os on os.id = oe.ophys_session_id "
-                "join specimens sp on sp.id = os.specimen_id "
-                "join projects p on p.id = os.project_id "
-                "join imaging_depths on imaging_depths.id = oe.imaging_depth_id "
-                "join structures st on st.id = oe.targeted_structure_id "
-                "where p.code = 'MesoscopeDevelopment' and (oe.workflow_state = 'processing' or oe.workflow_state = 'qc') and os.workflow_state ='uploaded' "
-                " and oe.id='{}'  ",
-            ))
+                query = ' '.join((
+                    "SELECT oe.id as experiment_id, os.id as session_id",
+                    ", os.storage_directory as session_folder, oe.storage_directory as experiment_folder",
+                    ", sp.name as specimen",
+                    ", os.date_of_acquisition as date",
+                    ", imaging_depths.depth as depth",
+                    ", st.acronym as structure",
+                    ", os.parent_session_id as parent_id",
+                    ", oe.workflow_state",
+                    ", os.stimulus_name as stimulus",
+                    " FROM ophys_experiments oe",
+                    "join ophys_sessions os on os.id = oe.ophys_session_id "
+                    "join specimens sp on sp.id = os.specimen_id "
+                    "join projects p on p.id = os.project_id "
+                    "join imaging_depths on imaging_depths.id = oe.imaging_depth_id "
+                    "join structures st on st.id = oe.targeted_structure_id "
+                    "where p.code = 'MesoscopeDevelopment' and (oe.workflow_state = 'processing' or oe.workflow_state = 'qc') and os.workflow_state ='uploaded' "
+                    " and oe.id='{}'  ",
+                ))
 
-            lims_data = self.psycopg2_select(query.format(experiment_id))
+                lims_data = self.psycopg2_select(query.format(experiment_id))
 
-            if lims_data == []:
+                if lims_data == []:
+                    self.data_present = False
+                else:
+                    self.data_pointer = lims_data
+                    self.data_present = True
+
+            except Exception as e:
+                logger.error("Unable to query LIMS database: {}".format(e))
                 self.data_present = False
-            else:
-                self.data_pointer = lims_data
-                self.data_present = True
-
-        except Exception as e:
-            logger.error("Unable to query LIMS database: {}".format(e))
-            self.data_present = False
+        else:
+            lims_data = []
+            logger.error("Provide experiment ID")
 
         return lims_data
 
     def get_session_folder(self):
 
-        _session = pd.DataFrame(self.get_mesoscope_experiment_data())
+        _session = pd.DataFrame(self.get_mesoscope_session_data(self.session_id))
+        self.session_folder = _session['session_folder'].values[0]
 
-        session_folder = _session['session_folder']
-
-        return session_folder.values[0]
+        return self.session_folder
 
     def get_splitting_json(self):
 
-        session_folder = self.get_session_folder()
-        splitting_json = os.path.join(session_folder, f"MESOSCOPE_FILE_SPLITTING_QUEUE_{session_id}_input.json")
+        splitting_json = os.path.join(self.session_folder,
+                                      f"MESOSCOPE_FILE_SPLITTING_QUEUE_{self.session_id}_input.json")
 
         if os.path.isfile(splitting_json):
             self.splitting_json_present = True
         else:
-            logger.error("Unable to find splitting json: {}".format(e))
+            logger.error("Unable to find splitting json")
             self.splitting_json_present = False
 
         return splitting_json
@@ -152,9 +160,9 @@ class MesoscopeDataset(object):
 
         return pairs
 
-    def get_exp_by_structure (self, structure) :
+    def get_exp_by_structure(self, structure):
 
-        experiments = self.get_mesoscope_experiment_data()
+        experiment = pd.DataFrame(self.get_mesoscope_session_data(self.session_id))
 
         return experiment.loc[experiment.structure == structure]
 
@@ -162,32 +170,64 @@ class MesoscopeDataset(object):
 
         if full_field_path_offline != '':
             # use full field path to the tiff:
-            full_field_tiff = os.path.join(full_field_path_offline, f"{session_id}_fullfield.tif")
+            full_field_path = os.path.join(full_field_path_offline, f"{self.session_id}_fullfield.tif")
+            self.full_field_path = full_field_path
 
-            if os.path.isfile(full_field_tiff):
+            if os.path.isfile(full_field_path):
                 self.full_field_present = True
             else:
-                full_field_tiff = ''
+                full_field_path = ''
                 logger.error("Can't find full field tiff at offline path, check if file exists")
                 self.full_field_present = False
 
         else:
             # see if file exists in lims:
             session_folder = self.get_session_folder()
-            full_field_tiff = os.path.join(session_folder, f"{session_id}_fullfield.tif")
+            full_field_path = os.path.join(session_folder, f"{self.session_id}_fullfield.tif")
 
-            if os.path.isfile(full_field_tiff):
-                self.full_field_tiff = full_field_tiff
+            if os.path.isfile(full_field_path):
+                self.full_field_path = full_field_path
                 self.full_field_present = True
             else:
-                full_field_tiff = ''
+                full_field_path = ''
                 logger.error("Full field tiff is absent in session folder, provide offline path")
                 self.full_field_present = False
 
-        return full_field_tiff
+        return full_field_path
 
+    def stitch_full_field(self, summ=True):
 
+        full_field_tiff_path = self.get_full_field_tiff()
+        image_stitched = np.zeros([slices, pixelResY, pixelResX * roi_num], dtype='int16')
+        if full_field_tiff_path != '':
+            tiff = tifffile.TiffFile(full_field_tiff_path)
+            meta = tiff.scanimage_metadata
+            image = tiff.asarray()
+            tiff.close()
+            slices = image.shape[0]
+            rois = meta['RoiGroups']['imagingRoiGroup']['rois']
+            roi_num = len(rois)
+            pixelResY = rois[1]['scanfields']['pixelResolutionXY'][1]
+            pixelResX = rois[1]['scanfields']['pixelResolutionXY'][0]
+            image_nlines = image.shape[1]
+            image_npixels = image.shape[2]
+            y_gap = np.int16((image_nlines - pixelResY * roi_num) / (roi_num - 1))
+            image_stitched = np.zeros([slices, pixelResY, pixelResX * roi_num], dtype='int16')
+            FF_image_name = FF_path.replace(os.path.dirname(FF_path) + '/', '')
+            FF_stitch_name = os.path.dirname(FF_path) + '/' + FF_image_name.split('.')[0] + '_stitched.tif'
+            FF_stitch_summ_name = os.path.dirname(FF_path) + '/' + FF_image_name.split('.')[0] + '_stitched_sum.tif'
+            for j in range(slices):
+                for i in range(roi_num):
+                    image_stitched[j, :, i * image_npixels:(i + 1) * image_npixels] = image[j, i * (pixelResY + y_gap):(i + 1) * pixelResY + i * y_gap, :]
+            if summ:
+                image_sum = np.int16(image_stitched.mean(axis=0))
+                tifffile.imsave(FF_stitch_summ_name, image_sum)
+            tifffile.imsave(FF_stitch_name, image_stitched)
 
+        else:
+            full_field_path = ''
+            logger.error("Full field tiff is not in session folder, provide offline path")
+        return image_stitched, meta
 
 
 
