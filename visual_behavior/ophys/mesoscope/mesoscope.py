8143eec6cb5fcde1d4b51968ef89c22f969f97b7
Natalia Orlova <nataliao@allenisntitute.org>
2019-03-11 13:47:11 -0700

    full field stitch debugged

--- visual_behavior/ophys/mesoscope/mesoscope.py
+++ visual_behavior/ophys/mesoscope/mesoscope.py
@@ -18,6 +18,7 @@ class MesoscopeDataset(object):
 
         self.session_id = session_id
         self.experiment_id = experiment_id
+        self.data_pointer = None
         self.data_present = False
 
         self._database = 'lims2'
@@ -25,6 +26,13 @@ class MesoscopeDataset(object):
         self._port = 5432
         self._username = 'limsreader'
         self._password = 'limsro'
+        self.session_folder = None
+        self.splitting_json = None
+        self.full_field_path = None
+        self.full_field_present = None
+        self.data_present = None
+        self.full_field_present = None
+        self.splitting_json_present = None
 
     def psycopg2_select(self, query):
 
@@ -55,7 +63,7 @@ class MesoscopeDataset(object):
                     ", os.date_of_acquisition as date",
                     ", imaging_depths.depth as depth",
                     ", st.acronym as structure",
-                    ", os.parent_session_id as parent_id",
+                    ", os.≤parent_session_id as parent_id",
                     ", oe.workflow_state",
                     ", os.stimulus_name as stimulus",
                     " FROM ophys_experiments oe",
@@ -64,12 +72,13 @@ class MesoscopeDataset(object):
                     "join projects p on p.id = os.project_id "
                     "join imaging_depths on imaging_depths.id = oe.imaging_depth_id "
                     "join structures st on st.id = oe.targeted_structure_id "
-                    "where p.code = 'MesoscopeDevelopment' and (oe.workflow_state = 'processing' or oe.workflow_state = 'qc') and os.workflow_state ='uploaded' "
+                    "where p.code = 'MesoscopeDevelopment' and (oe.workflow_state = 'processing' or oe.workflow_state "
+                    "= 'qc') and os.workflow_state ='uploaded' "
                     " and os.id='{}'  ",
                 ))
 
                 lims_data = self.psycopg2_select(query.format(session_id))
-                if lims_data == []:
+                if not lims_data:
                     self.data_present = False
                 else:
                     self.data_pointer = lims_data
@@ -84,9 +93,8 @@ class MesoscopeDataset(object):
         return lims_data
 
     def get_mesoscope_experiment_data(self, experiment_id=''):
-
+        lims_data = []
         if experiment_id != '':
-            lims_data = []
             self.experiment_id = experiment_id
             try:
 
@@ -106,13 +114,14 @@ class MesoscopeDataset(object):
                     "join projects p on p.id = os.project_id "
                     "join imaging_depths on imaging_depths.id = oe.imaging_depth_id "
                     "join structures st on st.id = oe.targeted_structure_id "
-                    "where p.code = 'MesoscopeDevelopment' and (oe.workflow_state = 'processing' or oe.workflow_state = 'qc') and os.workflow_state ='uploaded' "
+                    "where p.code = 'MesoscopeDevelopment' and (oe.workflow_state = 'processing' or oe.workflow_state "
+                    "= 'qc') and os.workflow_state ='uploaded' "
                     " and oe.id='{}'  ",
                 ))
 
                 lims_data = self.psycopg2_select(query.format(experiment_id))
 
-                if lims_data == []:
+                if not lims_data:
                     self.data_present = False
                 else:
                     self.data_pointer = lims_data
@@ -122,7 +131,6 @@ class MesoscopeDataset(object):
                 logger.error("Unable to query LIMS database: {}".format(e))
                 self.data_present = False
         else:
-            lims_data = []
             logger.error("Provide experiment ID")
 
         return lims_data
@@ -196,9 +204,10 @@ class MesoscopeDataset(object):
         return full_field_path
 
     def stitch_full_field(self, summ=True):
-
-        full_field_tiff_path = self.get_full_field_tiff()
-        image_stitched = np.zeros([slices, pixelResY, pixelResX * roi_num], dtype='int16')
+        full_field_tiff_path = self.full_field_path
+        ff_path = full_field_tiff_path
+        image_stitched = None
+        image_sum = None
         if full_field_tiff_path != '':
             tiff = tifffile.TiffFile(full_field_tiff_path)
             meta = tiff.scanimage_metadata
@@ -207,28 +216,24 @@ class MesoscopeDataset(object):
             slices = image.shape[0]
             rois = meta['RoiGroups']['imagingRoiGroup']['rois']
             roi_num = len(rois)
-            pixelResY = rois[1]['scanfields']['pixelResolutionXY'][1]
-            pixelResX = rois[1]['scanfields']['pixelResolutionXY'][0]
+            pixel_res_y = rois[1]['scanfields']['pixelResolutionXY'][1]
+            pixel_res_x = rois[1]['scanfields']['pixelResolutionXY'][0]
             image_nlines = image.shape[1]
             image_npixels = image.shape[2]
-            y_gap = np.int16((image_nlines - pixelResY * roi_num) / (roi_num - 1))
-            image_stitched = np.zeros([slices, pixelResY, pixelResX * roi_num], dtype='int16')
-            FF_image_name = FF_path.replace(os.path.dirname(FF_path) + '/', '')
-            FF_stitch_name = os.path.dirname(FF_path) + '/' + FF_image_name.split('.')[0] + '_stitched.tif'
-            FF_stitch_summ_name = os.path.dirname(FF_path) + '/' + FF_image_name.split('.')[0] + '_stitched_sum.tif'
+            y_gap = np.int16((image_nlines - pixel_res_y * roi_num) / (roi_num - 1))
+            image_stitched = np.zeros([slices, pixel_res_y, pixel_res_x * roi_num], dtype='int16')
+            ff_image_name = ff_path.replace(os.path.dirname(ff_path) + '/', '')
+            ff_stitch_name = os.path.dirname(ff_path) + '/' + ff_image_name.split('.')[0] + '_stitched.tif'
+            ff_stitch_summ_name = os.path.dirname(ff_path) + '/' + ff_image_name.split('.')[0] + '_stitched_sum.tif'
             for j in range(slices):
                 for i in range(roi_num):
-                    image_stitched[j, :, i * image_npixels:(i + 1) * image_npixels] = image[j, i * (pixelResY + y_gap):(i + 1) * pixelResY + i * y_gap, :]
+                    image_stitched[j, :, i * image_npixels:(i + 1) * image_npixels] = image[j, i * (pixel_res_y + y_gap):(i + 1) * pixel_res_y + i * y_gap,:]
             if summ:
                 image_sum = np.int16(image_stitched.mean(axis=0))
-                tifffile.imsave(FF_stitch_summ_name, image_sum)
-            tifffile.imsave(FF_stitch_name, image_stitched)
+                tifffile.imsave(ff_stitch_summ_name, image_sum)
+            tifffile.imsave(ff_stitch_name, image_stitched)
 
         else:
             full_field_path = ''
-            logger.error("Full field tiff is not in session folder, provide offline path")
-        return image_stitched, meta
-
-
-
-
+            logger.error("Full field tiff is absent in session folder, provide offline path")
+        return image_stitched, image_sum, meta
