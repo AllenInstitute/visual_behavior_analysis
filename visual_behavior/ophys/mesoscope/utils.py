896975d5f5a03fcdaa1fd18689822d19e5ebc631
Natalia Orlova <nataliao@alleninstitute.org>
2019-03-20 17:31:09 -0700

    run spatial demixing on offline traces (to use wiht local cache dir containing ica'ed traces)

--- visual_behavior/ophys/mesoscope/utils.py
+++ visual_behavior/ophys/mesoscope/utils.py
@@ -4,7 +4,13 @@ import time
 from multiprocessing import Process
 import visual_behavior.ophys.mesoscope.crosstalk_unmix as ica
 import visual_behavior.ophys.mesoscope.mesoscope as ms
+import allensdk.internal.core.lims_utilities as lu
+import visual_behavior.ophys.mesoscope.utils as mu
 import os
+import h5py
+import numpy as np
+import allensdk.internal.brain_observatory.demixer as demixer
+import logging
 
 meso_data = ms.get_all_mesoscope_data()
 meso_data['ICA_demix_exp'] = 0
@@ -71,3 +77,150 @@ def get_ica_sessions(sessions):
     ica_success = meso_data.loc[meso_data['ICA_demix_session'] == 1]
     ica_fail = meso_data.loc[meso_data['ICA_demix_session'] == 0]
     return ica_success, ica_fail
+
+def run_demixing_on_ica(exp_id, an_dir='/media/NCRAID/MesoscopeAnalysis/'):
+    query = f"""
+    select *
+    from ophys_cell_segmentation_runs
+    where current = True and ophys_experiment_id = {exp_id}
+    """
+    seg_run = lu.query(query)[0]['id']
+
+    query = f"""
+    select *
+    from cell_rois
+    where ophys_cell_segmentation_run_id = {seg_run}
+    """
+
+    rois = lu.query(query)
+
+    nrois = {roi['id']: dict(width=roi['width'],
+                             height=roi['height'],
+                             x=roi['x'],
+                             y=roi['y'],
+                             id=roi['id'],
+                             valid=roi['valid_roi'],
+                             mask=roi['mask_matrix'],
+                             exclusion_labels=[])
+             for roi in rois}
+    md = ms.get_all_mesoscope_data()
+    s = md.session_id
+    us = s.drop_duplicates()
+    mds, _ = mu.get_ica_sessions(us)
+    exp_dir = mds['experiment_folder'].loc[mds['experiment_id'] == exp_id].values[0]
+
+    rois = f'traces_ica_output_{exp_id}.h5'
+
+    input_data = {
+        "movie_h5": os.path.join(exp_dir, "processed", "concat_31Hz_0.h5"),
+        "traces_h5_ica": os.path.join(an_dir, rois),
+        "traces_h5": os.path.join(exp_dir, "processed", "roi_traces.h5"),
+        "roi_masks": nrois.values(),
+        "output_file": os.path.join(an_dir, f"traces_ica_output_{exp_id}.h5")
+    }
+    traces, masks, valid, trace_ids, movie_h5, output_h5 = parse_input(input_data)
+
+    # only demix non-union, non-duplicate ROIs
+    valid_idxs = np.where(valid)
+    demix_traces = traces[valid_idxs]
+    demix_masks = masks[valid_idxs]
+
+    with h5py.File(movie_h5, 'r') as f:
+        movie = f['data'].value
+
+    demixed_traces, drop_frames = demixer.demix_time_dep_masks(demix_traces, movie, demix_masks)
+
+    demix_path = os.path.join(an_dir, f'demixing_{exp_id}')
+    os.mkdir(demix_path)
+    plot_dir = os.path.join(demix_path, 'plots')
+    os.mkdir(plot_dir)
+
+    nt_inds = demixer.plot_negative_transients(demix_traces,
+                                               demixed_traces,
+                                               valid[valid_idxs],
+                                               demix_masks,
+                                               trace_ids[valid_idxs],
+                                               plot_dir)
+
+    logging.debug("rois with negative transients: %s", str(trace_ids[valid_idxs][nt_inds]))
+
+    nb_inds = demixer.plot_negative_baselines(demix_traces,
+                                              demixed_traces,
+                                              demix_masks,
+                                              trace_ids[valid_idxs],
+                                              plot_dir)
+
+    # negative baseline rois (and those that overlap with them) become nans
+    logging.debug("rois with negative baselines (or overlap with them): %s", str(trace_ids[valid_idxs][nb_inds]))
+    demixed_traces[nb_inds, :] = np.nan
+
+    logging.info("Saving output")
+    out_traces = np.zeros(traces.shape, dtype=demix_traces.dtype)
+    out_traces[:] = np.nan
+    out_traces[valid_idxs] = demixed_traces
+
+    with h5py.File(output_h5, 'w') as f:
+        f.create_dataset("data", data=out_traces, compression="gzip")
+        f.create_dataset("roi_names", data=[str(rn) for rn in trace_ids])
+
+    return
+
+
+def parse_input(data, exclude_labels=["union", "duplicate", "motion_border"]):
+    movie_h5 = get_path(data, "movie_h5", True)
+    traces_h5 = get_path(data, "traces_h5", True)
+    traces_h5_ica = get_path(data, "traces_h5_ica", True)
+    output_h5 = get_path(data, "output_file", False)
+
+    with h5py.File(movie_h5, "r") as f:
+        movie_shape = f["data"].shape[1:]
+
+    with h5py.File(traces_h5_ica, "r") as f:
+        traces = f["data"].value
+    traces = traces[1, :, :]
+
+    with h5py.File(traces_h5, "r") as f:
+        trace_ids = [int(rid) for rid in f["roi_names"].value]
+
+    rois = get_path(data, "roi_masks", False)
+    masks = None
+    valid = None
+
+    for roi in rois:
+        mask = np.zeros(movie_shape, dtype=bool)
+        mask_matrix = np.array(roi["mask"], dtype=bool)
+        mask[roi["y"]:roi["y"] + roi["height"], roi["x"]:roi["x"] + roi["width"]] = mask_matrix
+
+        if masks is None:
+            masks = np.zeros((len(rois), mask.shape[0], mask.shape[1]), dtype=bool)
+            valid = np.zeros(len(rois), dtype=bool)
+
+        rid = int(roi["id"])
+        try:
+            ridx = trace_ids.index(rid)
+        except ValueError as e:
+            raise ValueError("Could not find cell roi id %d in roi traces file" % rid)
+
+        masks[ridx, :, :] = mask
+
+        valid[ridx] = len(set(exclude_labels) & set(roi.get("exclusion_labels", []))) == 0
+
+    return traces, masks, valid, np.array(trace_ids), movie_h5, output_h5
+
+def assert_exists(file_name):
+    if not os.path.exists(file_name):
+        raise IOError("file does not exist: %s" % file_name)
+
+
+def get_path(obj, key, check_exists):
+    try:
+         path = obj[key]
+    except KeyError:
+        raise KeyError("required input field '%s' does not exist" % key)
+    if check_exists:
+        assert_exists(path)
+    return path
+
+def run_neuropil_s_on_exp(exp_id):
+
+    return
