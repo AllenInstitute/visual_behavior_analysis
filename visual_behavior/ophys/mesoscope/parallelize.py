a9372dfd2c8d2da63c2b56deaf4b6bc4b8a3f842
Natalia Orlova <nataliao@alleninstitute.org>
2019-03-26 20:42:30 -0700

    parallelizing with Pool

--- visual_behavior/ophys/mesoscope/parallelize.py
+++ visual_behavior/ophys/mesoscope/parallelize.py
@@ -1,18 +1,20 @@
 import psutil
 import resource
 import time
-from multiprocessing import Process
+from multiprocessing import Pool
 import visual_behavior.ophys.mesoscope.utils as mu
 import visual_behavior.ophys.mesoscope.mesoscope as ms
 
-meso_data = ms.get_all_mesoscope_data()
-meso_data['ICA_demix_exp'] = 0
-meso_data['ICA_demix_session'] = 0
 
-sessions = meso_data['session_id']
-sessions = sessions.drop_duplicates()
 
 if __name__ == '__main__':
+
+    meso_data = ms.get_all_mesoscope_data()
+    meso_data['ICA_demix_exp'] = 0
+    meso_data['ICA_demix_session'] = 0
+
+    sessions = meso_data['session_id']
+    sessions = sessions.drop_duplicates()
 
     thread_count = 20
     process_name = []
@@ -21,27 +23,6 @@ if __name__ == '__main__':
     nproc = resource.getrlimit(resource.RLIMIT_NPROC)
     if nproc[0] < nproc[1]:
         resource.setrlimit(resource.RLIMIT_NPROC, (nproc[1] - 1000, nproc[1]))
+    with Pool(thread_count) as p:
+        p.map(mu.run_ica_on_session, sessions)
 
-    while len(sessions) > 0:
-        for session in sessions:
-            if thread_count > 0:
-                print(f'number of active threads : {thread_count}')
-                p = Process(target=mu.run_ica_on_session, args=(session, meso_data,))
-                print(f'processing session :{session}')
-                p.daemon = True
-                p.start()
-                p.join()
-                process_name.append([p.pid])
-                process_status.append([p.is_alive()])
-                thread_count -= 1
-                sessions = sessions.drop(sessions.index[sessions == session])
-                print(f'remaining session: {len(sessions)}')
-            else:
-                if process_status.count(True) == thread_count:
-                    time.sleep(0.25)
-                    # update current process statuses here:
-                    process_status = []
-                    for pid in process_name:
-                        process_status.append(psutil.pid_exists(pid[0]))
-                else:
-                    thread_count = thread_count + process_status.count(False)
