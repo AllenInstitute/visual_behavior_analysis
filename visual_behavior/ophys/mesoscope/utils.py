51a8fe5505bd0d5185307b5c506d3e7fb6677d91
Natalia Orlova <nataliao@alleninstitute.org>
2019-04-03 14:04:26 -0700

    neuropil correction on ica traces

--- visual_behavior/ophys/mesoscope/utils.py
+++ visual_behavior/ophys/mesoscope/utils.py
@@ -8,6 +8,16 @@ import numpy as np
 import visual_behavior.ophys.mesoscope.crosstalk_unmix as ica
 import visual_behavior.ophys.mesoscope.mesoscope as ms
 
+import shutil
+import matplotlib
+
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+from allensdk.brain_observatory.r_neuropil import estimate_contamination_ratios
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
 meso_data = ms.get_all_mesoscope_data()
 meso_data['ICA_demix_exp'] = 0
 meso_data['ICA_demix_session'] = 0
@@ -201,3 +211,176 @@ def run_demixing_on_ica(session, an_dir='/media/NCRAID/MesoscopeAnalysis/'):
 
     return
 
+def debug_plot(file_name, roi_trace, neuropil_trace, corrected_trace, r, r_vals=None, err_vals=None):
+    fig = plt.figure(figsize=(15, 10))
+
+    ax = fig.add_subplot(211)
+    ax.plot(roi_trace, 'r', label="raw")
+    ax.plot(corrected_trace, 'b', label="fc")
+    ax.plot(neuropil_trace, 'g', label="neuropil")
+    ax.set_xlim(0, roi_trace.size)
+    ax.set_title('raw(%.02f, %.02f) fc(%.02f, %.02f) r(%f)' % (
+    roi_trace.min(), roi_trace.max(), corrected_trace.min(), corrected_trace.max(), r))
+    ax.legend()
+
+    if r_vals is not None:
+        ax = fig.add_subplot(212)
+        ax.plot(r_vals, err_vals, "o")
+
+    plt.savefig(file_name)
+    plt.close()
+
+
+def run_neuropil_correction_on_ica(session, an_dir='/media/NCRAID/MesoscopeAnalysis/'):
+    dataset = ms.MesoscopeDataset(session)
+    pairs = dataset.get_paired_planes()
+    for pair in pairs:
+        for exp_id in pair:
+            #######################################################################
+            # prelude -- get processing metadata
+            ses_dir = os.path.join(an_dir, f'session_{session}')
+            demix_dir = os.path.join(ses_dir, f'demixing_{exp_id}')
+            trace_file = os.path.join(demix_dir, f'traces_demixing_output_{exp_id}.h5')
+            neuropil_file = os.path.join(ses_dir, f'ica_neuropil_{pair[0]}_{pair[1]}',
+                                         f'neuropil_ica_output_{exp_id}.h5')
+
+            storage_dir = os.path.join(ses_dir, f'neuropil_corrected_{exp_id}')
+            if not os.path.isdir(storage_dir):
+                os.mkdir(storage_dir)
+
+            plot_dir = os.path.join(storage_dir, "neuropil_subtraction_plots")
+
+            if os.path.exists(plot_dir):
+                shutil.rmtree(plot_dir)
+
+            try:
+                os.makedirs(plot_dir)
+            except:
+                pass
+
+            logging.info("Neuropil correcting '%s'", trace_file)
+
+            ########################################################################
+            # process data
+
+            try:
+                roi_traces = h5py.File(trace_file, "r")
+            except:
+                logging.error("Error: unable to open ROI trace file '%s'", trace_file)
+                raise
+
+            try:
+                neuropil_traces = h5py.File(neuropil_file, "r")
+            except:
+                logging.error("Error: unable to open neuropil trace file '%s'", neuropil_file)
+                raise
+
+            '''
+            get number of traces, length, etc.
+            '''
+            num_traces, T = roi_traces['data'].shape
+            T_orig = T
+            T_cross_val = int(T / 2)
+            if (T - T_cross_val > T_cross_val):
+                T = T - 1
+
+            # make sure that ROI and neuropil trace files are organized the same
+            n_id = roi_traces["roi_names"]
+            r_id = roi_traces["roi_names"]
+
+            logging.info("Processing %d traces", len(n_id))
+            assert len(n_id) == len(r_id), "Input trace files are not aligned (ROI count)"
+            for i in range(len(n_id)):
+                assert n_id[i] == r_id[i], "Input trace files are not aligned (ROI IDs)"
+            '''
+            initialize storage variables and analysis routine
+            '''
+            r_list = [None] * num_traces
+            RMSE_list = [-1] * num_traces
+            roi_names = n_id
+            corrected = np.zeros((num_traces, T_orig))
+            r_vals = [None] * num_traces
+
+            for n in range(num_traces):
+                roi = roi_traces['data'][n]
+                neuropil = neuropil_traces['data'][0][n]
+
+                if np.any(np.isnan(neuropil)):
+                    logging.warning("neuropil trace for roi %d contains NaNs, skipping", n)
+                    continue
+
+                if np.any(np.isnan(roi)):
+                    logging.warning("roi trace for roi %d contains NaNs, skipping", n)
+                    continue
+
+                r = None
+
+                logging.info("Correcting trace %d (roi %s)", n, str(n_id[n]))
+                results = estimate_contamination_ratios(roi, neuropil)
+                logging.info("r=%f err=%f it=%d", results["r"], results["err"], results["it"])
+
+                r = results["r"]
+                fc = roi - r * neuropil
+                RMSE_list[n] = results["err"]
+                r_vals[n] = results["r_vals"]
+
+                debug_plot(os.path.join(plot_dir, "initial_%04d.png" % n),
+                           roi, neuropil, fc, r, results["r_vals"], results["err_vals"])
+
+                # mean of the corrected trace must be positive
+                if fc.mean() > 0:
+                    r_list[n] = r
+                    corrected[n, :] = fc
+                else:
+                    logging.warning("fc has negative baseline, skipping this r value")
+
+            # compute mean valid r value
+            r_mean = np.array([r for r in r_list if r is not None]).mean()
+
+            # fill in empty r values
+            for n in range(num_traces):
+                roi = roi_traces['data'][n]
+                neuropil = neuropil_traces['data'][0][n]
+
+                if r_list[n] is None:
+                    logging.warning("Error estimated r for trace %d. Setting to zero.", n)
+                    r_list[n] = 0
+                    corrected[n, :] = roi
+
+                # save a debug plot
+                debug_plot(os.path.join(plot_dir, "final_%04d.png" % n),
+                           roi, neuropil, corrected[n, :], r_list[n])
+
+                # one last sanity check
+                eps = -0.0001
+                if np.mean(corrected[n, :]) < eps:
+                    raise Exception("Trace %d baseline is still negative value after correction" % n)
+
+                if r_list[n] < 0.0:
+                    raise Exception("Trace %d ended with negative r" % n)
+
+            ########################################################################
+            # write out processed data
+
+            try:
+                savefile = os.path.join(storage_dir, "neuropil_correction.h5")
+                hf = h5py.File(savefile, 'w')
+                hf.create_dataset("r", data=r_list)
+                hf.create_dataset("RMSE", data=RMSE_list)
+                hf.create_dataset("FC", data=corrected, compression="gzip")
+                hf.create_dataset("roi_names", data=roi_names)
+
+                for n in range(num_traces):
+                    r = r_vals[n]
+                    if r is not None:
+                        hf.create_dataset("r_vals/%d" % n, data=r)
+                hf.close()
+            except:
+                logging.error("Error creating output h5 file")
+                raise
+
+            roi_traces.close()
+            neuropil_traces.close()
+
+            logging.info("finished")
+    return
