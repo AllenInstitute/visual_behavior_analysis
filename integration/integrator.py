1616891d976a87bfe7b15d4f3d0b81996d760366
christopher mochizuki <mochic808@gmail.com>
2019-04-04 15:25:57 -0700

    add integration testing utilies

---
+++ integration/integrator.py
@@ -0,0 +1,21 @@
+import subprocess
+
+
+def init_assets(
+        assets_container_name,
+):
+    subprocess.run(
+        '{} {}'.format(),
+    )
+
+
+def run_tests(
+        assets_container_name,
+):
+    subprocess.run(
+        '{} {}'.format(),
+    )
+
+
+if __name__ == '__main__':
+    import sys
