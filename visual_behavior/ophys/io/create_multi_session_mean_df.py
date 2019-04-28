d1591c8613a259077cc9962ca3ad77bf479d1f7a
matchings <marinag@alleninstitute.org>
2019-04-27 20:29:12 -0700

    re process with omitted flash fix

--- visual_behavior/ophys/io/create_multi_session_mean_df.py
+++ visual_behavior/ophys/io/create_multi_session_mean_df.py
@@ -103,8 +103,8 @@ if __name__ == '__main__':
                       790709081, 791119849, 791453282, 791453299, 792812544, 792813858,
                       792815735, 792816531, 794378505, 794381992, 795073741, 795075034,
                       795076128, 795948257, 795952471, 795952488, 795953296, 796105304,
-                      796105823, 796106321, 796106850, 796108483, 796306417, 796308505,
-                      797255551, 798392580, 798403387, 798404219, 799366517, 799368262,
+                      796106321, 796106850, 796108483, 796306417, 796308505,
+                      797255551, 798392580, 798403387, 798404219, 799366517,  # 799368262,
                       799368904, 803736273, 805100431, 805784313, 805784331, 806455766,
                       806456687, 806989729, 807752719, 807753318, 807753334, 807753920,
                       808619543, 808621015, 808621034, 808621958, 809497730, 809501118,
@@ -126,10 +126,10 @@ if __name__ == '__main__':
 
     get_multi_session_mean_df(experiment_ids, cache_dir,
                               conditions=['cell_specimen_id', 'change_image_name', 'trial_type'])
-    get_multi_session_mean_df(experiment_ids, cache_dir,
-                              conditions=['cell_specimen_id', 'image_name', 'repeat'], flashes=True)
-    get_multi_session_mean_df(experiment_ids, cache_dir,
-                              conditions=['cell_specimen_id', 'image_name'], flashes=True)
+    # get_multi_session_mean_df(experiment_ids, cache_dir,
+    #                           conditions=['cell_specimen_id', 'image_name', 'repeat'], flashes=True)
+    # get_multi_session_mean_df(experiment_ids, cache_dir,
+    #                           conditions=['cell_specimen_id', 'image_name'], flashes=True)
     get_multi_session_mean_df(experiment_ids, cache_dir,
                               conditions=['cell_specimen_id', 'image_name'], flashes=True, omitted=True)
 
