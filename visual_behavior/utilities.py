c469ecc695b31566d5b7bd034dc13ada33aa523d
Doug Ollerenshaw <d.ollerenshaw@gmail.com>
2019-07-23 10:21:55 -0700

    small changes to movie object code + example notebook

--- visual_behavior/utilities.py
+++ visual_behavior/utilities.py
@@ -290,9 +290,11 @@ class Movie(object):
             self.array = None
 
     def get_frame(self, frame=None, time=None, timestamps='sync'):
-        if timestamps == 'sync':
+        if time and timestamps == 'sync':
+            assert self.sync_timestamps, 'sync timestamps do not exist'
             timestamps = self.sync_timestamps
-        elif timestamps == 'file':
+        elif time and timestamps == 'file':
+            assert self.timestamps_from_file, 'timestamps from file do not exist'
             timestamps = self.timestamps_from_file
         else:
             timestamps = None
