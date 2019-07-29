805b25c4d46478c09a4b439a6de8e59a59af0eac
matchings <marinag@alleninstitute.org>
2019-07-28 20:42:43 -0700

    run next df

--- scripts/create_multi_session_mean_df.py
+++ scripts/create_multi_session_mean_df.py
@@ -47,14 +47,14 @@ if __name__ == '__main__':
 
     platform = 'scientifica'
 
-    get_multi_session_mean_df(experiment_ids, cache_dir, platform=platform,
-                              conditions=['cell_specimen_id', 'image_name', ], flashes=True)
-    get_multi_session_mean_df(experiment_ids, cache_dir, platform=platform,
-                              conditions=['cell_specimen_id', 'image_name', ], flashes=True, omitted=True)
+    # get_multi_session_mean_df(experiment_ids, cache_dir, platform=platform,
+    #                           conditions=['cell_specimen_id', 'image_name', ], flashes=True)
+    # get_multi_session_mean_df(experiment_ids, cache_dir, platform=platform,
+    #                           conditions=['cell_specimen_id', 'image_name', ], flashes=True, omitted=True)
     # get_multi_session_mean_df(experiment_ids, cache_dir, platform=platform,
     #                           conditions=['cell_specimen_id', 'change_image_name', 'trial_type'])
-    # get_multi_session_mean_df(experiment_ids, cache_dir, platform=platform,
-    #                                   conditions=['cell_specimen_id', 'image_name', 'repeat'], flashes=True)
+    get_multi_session_mean_df(experiment_ids, cache_dir, platform=platform,
+                                      conditions=['cell_specimen_id', 'image_name', 'repeat'], flashes=True)
 
     # get_multi_session_mean_df(experiment_ids, cache_dir,
     #                               conditions=['cell_specimen_id', 'change_image_name', 'trial_type'], use_events=True)
