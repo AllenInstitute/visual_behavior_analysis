dcbbac1bf201d5b7c1031864724e4e3fe39ca64d
Doug Ollerenshaw <d.ollerenshaw@gmail.com>
2019-08-22 12:25:49 -0700

    remove qc warning on un-implemented functions

--- visual_behavior/validation/qc.py
+++ visual_behavior/validation/qc.py
@@ -4,16 +4,6 @@ from . import extended_trials as et
 from . import core as cd
 from ..translator.core import create_extended_dataframe
 
-functions_yet_to_implement = [
-
-]
-warnings.warn('The following are turned off; remove this warning when done {}'.format(
-    '\n' +
-    ' * validate_schema' + '\n' +
-    ' * validate_monotonically_decreasing_number_of_change_times' + '\n' +
-    ' * validate_frame_intervals_exists'
-))
-
 
 def define_validation_functions(core_data):
     '''
