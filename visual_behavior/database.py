24c4d59554a93da76adee60d9c42b47090ed1e6d
Doug Ollerenshaw <d.ollerenshaw@gmail.com>
2019-08-26 13:05:12 -0700

    updates to database.py

--- visual_behavior/database.py
+++ visual_behavior/database.py
@@ -5,6 +5,8 @@ import numpy as np
 import json
 import os
 import glob
+import traceback
+import datetime
 from .translator.foraging2 import data_to_change_detection_core
 from .translator.core import create_extended_dataframe
 from .change_detection.trials import summarize
@@ -123,9 +125,16 @@ def get_pkl_path(session_id=None, id_type='behavior_session_uuid'):
     res = db.query('db','behavior_session_log',query={id_type:session_id})
     db.close()
     
-    if len(res) > 0:
-        storage_directory = res['storage_directory'].item()
-        return glob.glob(os.path.join(storage_directory,'*.pkl'))[0]
+    if res is not None and len(res) > 0:
+        if len(res) == 1:
+            storage_directory = res['storage_directory'].item()
+        elif len(res) > 1:
+            # there are occassionally duplicate entries in mouseseeks. return only the first:
+            storage_directory = res['storage_directory'].iloc[0]
+        pkls = glob.glob(os.path.join(storage_directory,'*.pkl'))
+        if len(pkls) == 1:
+            return pkls[0]
+        
     
 def uuid_to_lims_id(uuid):
     '''translates uuid to lims_id'''
@@ -133,8 +142,18 @@ def uuid_to_lims_id(uuid):
     res = db.query('db','behavior_session_log',query={'foraging_id':uuid})
     db.close
     
-    if len(res) > 0:
-        return res['id'].item()
+    if len(res) == 0:
+        return None
+    elif len(res) == 1:
+        return int(res['id'].item())
+    elif len(res) > 1:
+        return int(res['id'].iloc[0])
+    
+def is_int(n):
+    return isinstance(n, (int, np.integer))
+
+def is_float(n):
+    return isinstance(n, (float, np.float))
     
 def add_behavior_record(behavior_session_uuid, overwrite=False,db_connection=None):
     '''
@@ -151,10 +170,7 @@ def add_behavior_record(behavior_session_uuid, overwrite=False,db_connection=Non
         - metadata
         - log
     '''
-    def is_int(n):
-        return isinstance(n, (int, np.integer))
-    def is_float(n):
-        return isinstance(n, (float, np.float))
+    
     def insert(db, table, lims_id, behavior_session_uuid, dtype, data_to_insert):
         db[table].insert_one({
             'lims_id':lims_id,
@@ -163,36 +179,65 @@ def add_behavior_record(behavior_session_uuid, overwrite=False,db_connection=Non
             'data':data_to_insert
         })
         
+    def insert_summary_row(summary_row):
+        if type(summary_row) == pd.DataFrame:
+            summary_row = summary_row.iloc[0].to_dict()
+        # cast to simple int or float
+        summary_row = {k:(int(v) if is_int(v) else v) for k,v in summary_row.items()}
+        summary_row = {k:(float(v) if is_float(v) else v) for k,v in summary_row.items()}
+        db.summary.insert_one(summary_row)
+        
+    def add_metadata_to_summary(summary, lims_id, pkl_path, behavior_session_uuid):
+        summary['lims_id'] = lims_id
+        summary['pkl_path'] = pkl_path
+        summary['behavior_session_uuid'] = behavior_session_uuid
+        return summary
+    
+    def log_error_data(err):
+        tb = traceback.format_tb(err.__traceback__)
+        db['error_log'].insert_one({
+            'lims_id':lims_id,
+            'behavior_session_uuid':behavior_session_uuid,
+            'error_log':tb,
+            'local_time':str(datetime.datetime.now()),
+            'utc_time':str(datetime.datetime.utcnow())
+        })
+        print('error on session {}, writing to error table'.format(behavior_session_uuid))
+        summary = {}
+        summary = add_metadata_to_summary(summary, lims_id, pkl_path, behavior_session_uuid)
+        summary['error_on_load'] = True
+        insert_summary_row(summary)
+        
     if db_connection is None:
         db_conn = Database('visual_behavior_data')
         db = db_conn.behavior_data
     else:
         db = db_connection
     
-    pkl_path = get_pkl_path(behavior_session_uuid)
-    
-    # load behavior data
-    data = pd.read_pickle(pkl_path)
-    core_data = data_to_change_detection_core(data)
-    trials = create_extended_dataframe(**core_data).drop(columns=['date','LDT_mode'])
-    summary = summarize.session_level_summary(trials).sort_values(by='startdatetime').reset_index(drop=True)
-
     # get lims ID
     lims_id = uuid_to_lims_id(behavior_session_uuid)
-
+    
+    # load behavior data
+    try:
+        pkl_path = get_pkl_path(behavior_session_uuid)
+        data = pd.read_pickle(pkl_path)
+        core_data = data_to_change_detection_core(data)
+        trials = create_extended_dataframe(**core_data).drop(columns=['date','LDT_mode'])
+        summary = summarize.session_level_summary(trials).sort_values(by='startdatetime').reset_index(drop=True)
+    except Exception as err:
+        log_error_data(err)
+        return
+        
     # add some columns to trials and summary
     trials['lims_id'] = lims_id
-    summary['lims_id'] = lims_id
-    summary['pkl_path'] = pkl_path
-    summary['behavior_session_uuid'] = behavior_session_uuid
+    summary = add_metadata_to_summary(summary, lims_id, pkl_path, behavior_session_uuid)
     trials['behavior_session_uuid'] = behavior_session_uuid
 
     if behavior_session_uuid in db.summary.distinct('behavior_session_uuid'):
         print('session with uuid {} already in summary'.format(behavior_session_uuid))
     else:
         # insert summary
-        summary_row = {k:v for k,v in zip(list(summary.keys()),list(summary.values[0]))}
-        db.summary.insert_one(summary_row)
+        insert_summary_row(summary)
 
     if behavior_session_uuid in db.trials.distinct('behavior_session_uuid'):
         print('session with uuid {} already in trials'.format(behavior_session_uuid,'trials'))
