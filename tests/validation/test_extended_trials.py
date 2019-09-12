8bcfceae5a061ca8b3eb26f7c8f68ec69ddca3e9
Doug Ollerenshaw <d.ollerenshaw@gmail.com>
2019-09-12 13:54:55 -0700

    update tests

--- tests/validation/test_extended_trials.py
+++ tests/validation/test_extended_trials.py
@@ -426,44 +426,50 @@ def test_validate_autorewards_after_N_consecutive_misses():
 
 
 def test_validate_reward_when_lick_in_window():
-    GOOD_DATA = pd.DataFrame({
-        'trial_type': ['go', 'go', 'catch', 'aborted'],
-        'auto_rewarded': [False, False, False, False],
-        'change_time': [867.943295, 2132.735950, 2359.626023, 2500],
-        'lick_times': [
-            [868.07670185, 868.243553692, 868.343638907],
-            [2132.8193812, 2133.00285741, 2134.73767532],
-            [2359.72610696, 2359.95964697], [2499]
-        ],
-        'reward_times': [
-            [867.943295],
-            [2132.8193812],
-            [],
-            [],
-        ],
-        'response_window': [[0.15, 0.75], [0.15, 0.75], [0.15, 0.75], [0.15, 0.75]]
-    })
+    GOOD_DATA = {
+        'trials': pd.DataFrame({
+            'trial_type': ['go', 'go', 'catch', 'aborted'],
+            'rewarded': [True, True, False, False],
+            'auto_rewarded': [False, False, False, False],
+            'change_time': [867.943295, 2132.735950, 2359.626023, 2500],
+            'lick_times': [
+                [868.07670185, 868.243553692, 868.343638907],
+                [2132.8193812, 2133.00285741, 2134.73767532],
+                [2359.72610696, 2359.95964697], [2499]
+            ],
+            'reward_times': [
+                [867.943295],
+                [2132.8193812],
+                [],
+                [],
+            ],
+        }),
+        'metadata': {'response_window':[0.15, 0.75]}
+    }
 
     assert validate_reward_when_lick_in_window(GOOD_DATA) == True
 
     # missing reward on second go trial
-    BAD_DATA = pd.DataFrame({
-        'trial_type': ['go', 'go', 'catch', 'aborted'],
-        'auto_rewarded': [False, False, False, False],
-        'change_time': [867.943295, 2132.735950, 2359.626023, 2500],
-        'lick_times': [
-            [868.07670185, 868.243553692, 868.343638907],
-            [2132.8193812, 2133.00285741, 2134.73767532],
-            [2359.72610696, 2359.95964697], [2499]
-        ],
-        'reward_times': [
-            [867.943295],
-            [],
-            [],
-            [],
-        ],
-        'response_window': [[0.15, 0.75], [0.15, 0.75], [0.15, 0.75], [0.15, 0.75]]
-    })
+    BAD_DATA = {
+        'trials': pd.DataFrame({
+            'trial_type': ['go', 'go', 'catch', 'aborted'],
+            'rewarded': [True, True, False, False],
+            'auto_rewarded': [False, False, False, False],
+            'change_time': [867.943295, 2132.735950, 2359.626023, 2500],
+            'lick_times': [
+                [868.07670185, 868.243553692, 868.343638907],
+                [2132.8193812, 2133.00285741, 2134.73767532],
+                [2359.72610696, 2359.95964697], [2499]
+            ],
+            'reward_times': [
+                [867.943295],
+                [],
+                [],
+                [],
+            ],
+        }),
+        'metadata': {'response_window':[0.15, 0.75]}
+    }
 
     assert validate_reward_when_lick_in_window(BAD_DATA) == False
 
