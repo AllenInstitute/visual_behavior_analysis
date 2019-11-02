aec8a71fa11ad802e59073f0ca126891b9abffb6
matchings <marinag@alleninstitute.org>
2019-11-02 15:48:46 -0700

    update to new pbstools location

--- scripts/run_event_detection.py
+++ scripts/run_event_detection.py
@@ -1,7 +1,8 @@
 import sys
 import platform
 if platform.system() == 'Linux':
-    sys.path.append('/allen/programs/braintv/workgroups/nc-ophys/Doug/pbstools')
+    # sys.path.append('/allen/programs/braintv/workgroups/nc-ophys/Doug/pbstools')
+    sys.path.append('/allen/programs/braintv/workgroups/nc-ophys/nick.ponvert/src/pbstools')
 from pbstools import PythonJob # flake8: noqa: E999
 
 # #VisualBehaviorDevelopment - complete dataset as of 11/15/18
