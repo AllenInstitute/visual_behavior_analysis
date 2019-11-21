94cbbbc4d65b639a93e77a45f4b8ab4573cf58ba
Doug Ollerenshaw <dougo@alleninstitute.org>
2019-11-21 09:55:22 -0800

    adding metrics to metrics.py

--- visual_behavior/metrics.py
+++ visual_behavior/metrics.py
@@ -6,17 +6,39 @@ import numpy as np
 from scipy import stats
 from sklearn.metrics import confusion_matrix
 from .utilities import dprime as __dprime
+from .utilities import trial_number_limit
+from scipy.stats import norm
 
 
-def hit_rate(y_true, y_pred):
-    return y_pred[y_true].mean()
+def N_go_trials(y_true, y_pred):
+    return len(y_pred[y_true])
 
 
-def false_alarm_rate(y_true, y_pred):
-    return y_pred[~y_true].mean()
+def N_catch_trials(y_true, y_pred):
+    return len(y_pred[~y_true])
 
 
-def d_prime(y_true, y_pred, eps=None):
+def hit_rate(y_true, y_pred, apply_trial_number_limit=False):
+    go_responses = y_pred[y_true]
+    response_probability = go_responses.mean()
+    if apply_trial_number_limit:
+        trial_count = N_go_trials(y_true, y_pred)
+        return trial_number_limit(response_probability, trial_count)
+    else:
+        return response_probability
+
+
+def false_alarm_rate(y_true, y_pred, apply_trial_number_limit=False):
+    catch_responses = y_pred[~y_true]
+    response_probability = catch_responses.mean()
+    if apply_trial_number_limit:
+        trial_count = N_catch_trials(y_true, y_pred)
+        return trial_number_limit(response_probability, trial_count)
+    else:
+        return response_probability
+
+
+def d_prime(y_true, y_pred, apply_trial_number_limit=False, eps=None):
     if len(y_true) > 0:
         if eps is None:
             # a reasonable minimum value is 1 / sum of trials
@@ -24,8 +46,8 @@ def d_prime(y_true, y_pred, eps=None):
 
         limits = (eps, 1 - eps)
 
-        HR = hit_rate(y_true, y_pred)
-        FAR = false_alarm_rate(y_true, y_pred)
+        HR = hit_rate(y_true, y_pred, apply_trial_number_limit=apply_trial_number_limit)
+        FAR = false_alarm_rate(y_true, y_pred, apply_trial_number_limit=apply_trial_number_limit)
 
         return __dprime(HR, FAR, limits)
     else:
@@ -40,3 +62,11 @@ def discrim_p(y_true, y_pred):
         return stats.chi2_contingency(C)[1]
     except ValueError:
         return 1.0
+
+
+def criterion(hit_rate, false_alarm_rate):
+    '''see chapter 2 of Macmillan Creelman Detection Theory'''
+    Z = norm.ppf
+    c = -0.5*(Z(hit_rate) + Z(false_alarm_rate))
+
+    return c
