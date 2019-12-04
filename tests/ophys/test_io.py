0264cdb22cab68ae19d3a585b7da46ed1c8ac1f9
Doug Ollerenshaw <dougo@alleninstitute.org>
2019-12-03 17:42:02 -0800

    simplify test_io.py, remove skipif

--- tests/ophys/test_io.py
+++ tests/ophys/test_io.py
@@ -1,33 +1,31 @@
 import os
 import pytest
 import platform
-pytestmark = pytest.mark.skipif(platform.python_version() < '3.0.0',
-                                reason='Requires Python 3')
+import matplotlib
+matplotlib.use('Agg')
+from visual_behavior.ophys.io.convert_level_1_to_level_2 import convert_level_1_to_level_2
+from visual_behavior.ophys.io.create_analysis_files import create_analysis_files
+from visual_behavior.ophys.io.create_multi_session_mean_df import get_multi_session_mean_df
+EXPERIMENT_ID = 702134928
+PLATFORM = 'scientifica'
 
-def setup_module(module):
-    import matplotlib
-    matplotlib.use('Agg')
-    from visual_behavior.ophys.io.convert_level_1_to_level_2 import convert_level_1_to_level_2
-    from visual_behavior.ophys.io.create_analysis_files import create_analysis_files
-    from visual_behavior.ophys.io.create_multi_session_mean_df import get_multi_session_mean_df
-    EXPERIMENT_ID = 702134928
 
 @pytest.mark.slow
-@pytest.mark.skipif(os.environ.get('PYTHONPATH','').startswith('/home/circleci'), reason='Cannot test against real files on CircleCI')
+@pytest.mark.skipif(os.environ.get('PYTHONPATH', '').startswith('/home/circleci'), reason='Cannot test against real files on CircleCI')
 def test_convert_level_1_to_level_2(cache_dir):
-
+    print('experiment_id = {}'.format(EXPERIMENT_ID))
     convert_level_1_to_level_2(EXPERIMENT_ID, cache_dir=cache_dir)
 
 
 @pytest.mark.slow
-@pytest.mark.skipif(os.environ.get('PYTHONPATH','').startswith('/home/circleci'), reason='Cannot test against real files on CircleCI')
+@pytest.mark.skipif(os.environ.get('PYTHONPATH', '').startswith('/home/circleci'), reason='Cannot test against real files on CircleCI')
 def test_create_analysis(cache_dir):
 
     create_analysis_files(EXPERIMENT_ID, cache_dir, overwrite_analysis_files=True)
 
 
 @pytest.mark.slow
-@pytest.mark.skipif(os.environ.get('PYTHONPATH','').startswith('/home/circleci'), reason='Cannot test against real files on CircleCI')
+@pytest.mark.skipif(os.environ.get('PYTHONPATH', '').startswith('/home/circleci'), reason='Cannot test against real files on CircleCI')
 def test_create_multi_session_mean_df(cache_dir):
 
-    get_multi_session_mean_df([EXPERIMENT_ID], cache_dir=cache_dir)
+    get_multi_session_mean_df([EXPERIMENT_ID], cache_dir=cache_dir, platform=PLATFORM)
