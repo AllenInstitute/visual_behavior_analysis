c39bdf0b03dd59bd83b6439f13244aacb85c4b82
Nick Ponvert <nickponvert@gmail.com>
2020-01-22 11:31:09 -0800

    linting fixes

--- visual_behavior/translator/allensdk_sessions/sdk_utils.py
+++ visual_behavior/translator/allensdk_sessions/sdk_utils.py
@@ -1,13 +1,12 @@
-import numpy as np
 from visual_behavior.translator.allensdk_sessions import session_attributes as sa
 
 
-# SDK utilities 
+# SDK utilities
 # Created by Alex Piet & Nick Ponvert, 01/17/2019
 # Currently works on SDK v.1.3.2
 
 
-def get_bsid_from_osid(osid,cache):
+def get_bsid_from_osid(osid, cache):
     '''
         Finds the behavior_session_id associated with an ophys_session_id
         ARGS
@@ -21,7 +20,8 @@ def get_bsid_from_osid(osid,cache):
         raise Exception('ophys_session_id not in session table')
     return ophys_sessions.loc[osid].behavior_session_id
 
-def get_osid_from_bsid(bsid,cache):
+
+def get_osid_from_bsid(bsid, cache):
     '''
         Finds the ophys_session_id associated with an behavior_session_id
         ARGS
@@ -35,7 +35,8 @@ def get_osid_from_bsid(bsid,cache):
         raise Exception('behavior_session_id not in behavior session table')
     return behavior_sessions.loc[bsid].ophys_session_id.astype(int)
 
-def get_oeid_from_bsid(bsid,cache,exp_num=0):
+
+def get_oeid_from_bsid(bsid, cache, exp_num=0):
     '''
         Finds the ophys_experiment_id associated with an behavior_session_id
         ARGS
@@ -47,10 +48,11 @@ def get_oeid_from_bsid(bsid,cache,exp_num=0):
                     For scientifica sessions, there is only one experiment per behavior_session, so exp_num = 0
                     For mesoscope, there are 8 experiments, so exp_num = (0,7)
     '''
-    osid = get_osid_from_bsid(bsid,cache)
-    return get_oeid_from_osid(osid,cache, exp_num=exp_num)
+    osid = get_osid_from_bsid(bsid, cache)
+    return get_oeid_from_osid(osid, cache, exp_num=exp_num)
 
-def get_oeid_from_osid(osid,cache,exp_num = 0):
+
+def get_oeid_from_osid(osid, cache, exp_num=0):
     '''
         Finds the behavior_session_id associated with an ophys_session_id
         ARGS
@@ -65,10 +67,11 @@ def get_oeid_from_osid(osid,cache,exp_num = 0):
     ophys_sessions = cache.get_session_table()
     if osid not in ophys_sessions.index:
         raise Exception('ophys_session_id not in session table')
-    experiments= ophys_sessions.loc[osid].ophys_experiment_id
+    experiments = ophys_sessions.loc[osid].ophys_experiment_id
     return experiments[0]
 
-def get_bsid_from_oeid(oeid,cache):
+
+def get_bsid_from_oeid(oeid, cache):
     '''
         Finds the behavior_session_id associated with an ophys_experiment_id
         ARGS
@@ -83,7 +86,7 @@ def get_bsid_from_oeid(oeid,cache):
     return ophys_experiments.loc[oeid].behavior_session_id
 
 
-def get_osid_from_oeid(oeid,cache):
+def get_osid_from_oeid(oeid, cache):
     '''
         Finds the ophys_session_id associated with an ophys_experiment_id
         ARGS
@@ -91,7 +94,7 @@ def get_osid_from_oeid(oeid,cache):
             cache   cache from BehaviorProjectCache
         Returns
             osid    ophys_session_id for that ophys_experiment
-    '''   
+    '''
     ophys_experiments = cache.get_experiment_table()
     if oeid not in ophys_experiments.index:
         raise Exception('ophys_experiment_id not in experiment table')
@@ -101,27 +104,28 @@ def get_osid_from_oeid(oeid,cache):
 def get_specimen_id_from_donor_id(d_id):
     '''
         Gets the specimen_id associated with a donor_id
-        
-        WARNING, this will not work if the donor_id does not have an associated specimen_id. 
-        
-        WARNING, this function was meant as a temporary holdover while waiting for SDK support 
+
+        WARNING, this will not work if the donor_id does not have an associated specimen_id.
+
+        WARNING, this function was meant as a temporary holdover while waiting for SDK support
     '''
     cache = get_cache()
-    ophys_sessions = cache.get_session_table()   
+    ophys_sessions = cache.get_session_table()
     behavior_sessions = cache.get_behavior_session_table()
     x = behavior_sessions.query('donor_id == @d_id')['ophys_session_id']
     osid = x[~x.isnull()].values[0].astype(int)
     specimen_id = ophys_sessions.query('ophys_session_id ==@osid')['specimen_id'].values[0]
     return specimen_id
 
+
 def get_donor_id_from_specimen_id(s_id):
     '''
         Gets the donor_id associated with a specimen_id
-        
-        WARNING, this function was meant as a temporary holdover while waiting for SDK support 
+
+        WARNING, this function was meant as a temporary holdover while waiting for SDK support
     '''
     cache = get_cache()
-    ophys_sessions = cache.get_session_table()   
+    ophys_sessions = cache.get_session_table()
     behavior_sessions = cache.get_behavior_session_table()
     osid = ophys_sessions.query('specimen_id == @s_id').iloc[0].name
     donor_id = behavior_sessions.query('ophys_session_id ==@osid')['donor_id'].values[0]
@@ -130,16 +134,16 @@ def get_donor_id_from_specimen_id(s_id):
 
 def add_stimulus_presentations_analysis(session):
     '''
-        Adds a series of columns to the stimulus_presentations table  
-        
-        WARNING, this function was meant as a temporary holdover while waiting for SDK support. 
-            If these columns are already implemented in the SDK, then using this function will 
-            overwrite them. Check before using. 
+        Adds a series of columns to the stimulus_presentations table
+
+        WARNING, this function was meant as a temporary holdover while waiting for SDK support.
+            If these columns are already implemented in the SDK, then using this function will
+            overwrite them. Check before using.
     '''
-    trials = session.trials # NEED TO QUERY THIS FIRST BECAUSE OF CONVERT_LICKS() 
+    trials = session.trials  # NEED TO QUERY THIS FIRST BECAUSE OF CONVERT_LICKS()
     # allensdk/brain_observatory/behavior/trials_processing.get_trials() has an assertion
-    # that session.rewards has timestamps as an index. And the code requres that session.licks 
-    # has 'time' as a column. Therefore, before modifying those attributes, we load the trials 
+    # that session.rewards has timestamps as an index. And the code requres that session.licks
+    # has 'time' as a column. Therefore, before modifying those attributes, we load the trials
     # table once, and this uses the memoize attribute to calculate this dataframe first. We realize
     # this is a terrible hack, but its the easiest way forward until the naming conventions are fixed
     sa.convert_licks_inplace(session.licks)
@@ -150,5 +154,3 @@ def add_stimulus_presentations_analysis(session):
     sa.add_time_from_last_lick_inplace(session)
     sa.add_time_from_last_reward_inplace(session)
     sa.add_time_from_last_change_inplace(session)
-
-
--- visual_behavior/translator/allensdk_sessions/session_attributes.py
+++ visual_behavior/translator/allensdk_sessions/session_attributes.py
@@ -1,25 +1,27 @@
 import pandas as pd
 import visual_behavior.ophys.dataset.extended_stimulus_processing as esp
 
-# This file contains functions to reformat sdk session attributes to conform with our 
+# This file contains functions to reformat sdk session attributes to conform with our
 # design decisions. If we can get some of these changes backported into the SDK, then they
 # can be removed from this module.
+
 
 def convert_licks(licks_df):
     '''
     licks has a column called 'time', inconsistent with the rest of the sdk.
     should be 'timestamps'
-    
+
     ARGS: licks_df, from the SDK
     RETURNS: licks_df
     MODIFIES: licks_df, renaming column 'time' to 'timestamps'
-    
+
     WARNING. session.trials will not load after this function is called. Therefore, before this 
         function is called, you need to run session.trials and then it will be cached and will work.
         This is done for you in sdk_utils.add_stimulus_presentations_analysis. 
     '''
     assert 'time' in licks_df.columns
-    return licks_df.rename(columns={'time':'timestamps'})
+    return licks_df.rename(columns={'time': 'timestamps'})
+
 
 def convert_rewards(rewards_df):
     '''
@@ -37,6 +39,7 @@ def convert_rewards(rewards_df):
     assert rewards_df.index.name == 'timestamps'
     return rewards_df.reset_index()
 
+
 def convert_licks_inplace(licks_df):
     '''
     licks has a column called 'time', inconsistent with the rest of the sdk.
@@ -47,7 +50,8 @@ def convert_licks_inplace(licks_df):
     MODIFIES: licks_df, renaming column 'time' to 'timestamps'
     '''
     assert 'time' in licks_df.columns
-    licks_df.rename(columns={'time':'timestamps'}, inplace=True)
+    licks_df.rename(columns={'time': 'timestamps'}, inplace=True)
+
 
 def convert_rewards_inplace(rewards_df):
     '''
@@ -61,6 +65,7 @@ def convert_rewards_inplace(rewards_df):
     assert rewards_df.index.name == 'timestamps'
     rewards_df.reset_index(inplace=True)
 
+
 def convert_running_speed(running_speed_obj):
     '''
     running speed is returned as a custom object, inconsistent with other attrs.
@@ -70,14 +75,15 @@ def convert_running_speed(running_speed_obj):
     RETURNS: dataframe with columns timestamps and speed
     '''
     return pd.DataFrame({
-        'timestamps':running_speed_obj.timestamps,
-        'speed':running_speed_obj.values
+        'timestamps': running_speed_obj.timestamps,
+        'speed': running_speed_obj.values
     })
+
 
 def add_change_each_flash_inplace(session):
     '''
         Adds a column to session.stimulus_presentations, ['change'], which is True if the stimulus was a change image, and False otherwise
-        
+
         ARGS: SDK session object
         RETURNS: nothing
         MODIFIES: session.stimulus_presentations dataframe
@@ -85,7 +91,8 @@ def add_change_each_flash_inplace(session):
     changes = esp.find_change(session.stimulus_presentations['image_index'], esp.get_omitted_index(session.stimulus_presentations))
     session.stimulus_presentations['change'] = changes
 
-def add_mean_running_speed_inplace(session,range_relative_to_stimulus_start=[0, 0.75]):
+
+def add_mean_running_speed_inplace(session, range_relative_to_stimulus_start=[0, 0.75]):
     '''
     Append a column to stimulus_presentations which contains the mean running speed between
 
@@ -101,12 +108,12 @@ def add_mean_running_speed_inplace(session,range_relative_to_stimulus_start=[0, 
         nothing, modifies session in place. Same as the input, but with 'mean_running_speed' column added
     '''
     mean_running_speed_df = esp.mean_running_speed(session.stimulus_presentations,
-                                            session.running_speed,
-                                            range_relative_to_stimulus_start)
+                                                   session.running_speed,
+                                                   range_relative_to_stimulus_start)
     session.stimulus_presentations["mean_running_speed"] = mean_running_speed_df
 
 
-def add_licks_each_flash_inplace(session,range_relative_to_stimulus_start=[0, 0.75]):
+def add_licks_each_flash_inplace(session, range_relative_to_stimulus_start=[0, 0.75]):
     '''
     Append a column to stimulus_presentations which contains the timestamps of licks that occur
     in a range relative to the onset of the stimulus.
@@ -123,12 +130,12 @@ def add_licks_each_flash_inplace(session,range_relative_to_stimulus_start=[0, 0.
     '''
 
     licks_each_flash_df = esp.licks_each_flash(session.stimulus_presentations,
-                                            session.licks,
-                                            range_relative_to_stimulus_start)
+                                               session.licks,
+                                               range_relative_to_stimulus_start)
     session.stimulus_presentations["licks"] = licks_each_flash_df
 
 
-def add_rewards_each_flash_inplace(session,range_relative_to_stimulus_start=[0, 0.75]):
+def add_rewards_each_flash_inplace(session, range_relative_to_stimulus_start=[0, 0.75]):
     '''
     Append a column to stimulus_presentations which contains the timestamps of rewards that occur
     in a range relative to the onset of the stimulus.
@@ -145,15 +152,16 @@ def add_rewards_each_flash_inplace(session,range_relative_to_stimulus_start=[0, 
     '''
 
     rewards_each_flash_df = esp.rewards_each_flash(session.stimulus_presentations,
-                                                 session.rewards,
-                                                 range_relative_to_stimulus_start)
+                                                   session.rewards,
+                                                   range_relative_to_stimulus_start)
     session.stimulus_presentations["rewards"] = rewards_each_flash_df
+
 
 def add_time_from_last_lick_inplace(session):
     '''
         Adds a column in place to session.stimulus_presentations['time_from_last_lick'], which is the time, in seconds
         since the last lick
-        
+
         ARGS: SDK session object
         MODIFIES: session.stimulus_presentations
         RETURNS: nothing
@@ -161,11 +169,12 @@ def add_time_from_last_lick_inplace(session):
     lick_times = session.licks['timestamps'].values
     reward_times = session.rewards['timestamps'].values
     flash_times = session.stimulus_presentations["start_time"].values
-    if len(lick_times) < 5: #Passive sessions
+    if len(lick_times) < 5:  # Passive sessions
         time_from_last_lick = np.full(len(flash_times), np.nan)
     else:
         time_from_last_lick = esp.time_from_last(flash_times, lick_times)
-    session.stimulus_presentations["time_from_last_lick"] = time_from_last_lick   
+    session.stimulus_presentations["time_from_last_lick"] = time_from_last_lick
+
 
 def add_time_from_last_reward_inplace(session):
     '''
@@ -180,11 +189,12 @@ def add_time_from_last_reward_inplace(session):
     reward_times = session.rewards['timestamps'].values
     flash_times = session.stimulus_presentations["start_time"].values
 
-    if len(reward_times) < 1: # Sometimes mice are bad
+    if len(reward_times) < 1:  # Sometimes mice are bad
         time_from_last_reward = np.full(len(flash_times), np.nan)
     else:
         time_from_last_reward = esp.time_from_last(flash_times, reward_times)
     session.stimulus_presentations["time_from_last_reward"] = time_from_last_reward
+
 
 def add_time_from_last_change_inplace(session):
     '''
@@ -199,9 +209,3 @@ def add_time_from_last_change_inplace(session):
     change_times = session.stimulus_presentations.query('change')['start_time'].values
     time_from_last_change = esp.time_from_last(flash_times, change_times)
     session.stimulus_presentations["time_from_last_change"] = time_from_last_change
-
-
-
-
-
-
