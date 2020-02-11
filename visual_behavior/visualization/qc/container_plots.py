15f68901a178924b6b9f7695f17c85275325891d
matchings <marinag@alleninstitute.org>
2020-02-11 12:05:50 -0800

    pseudocode example of container plot function

---
+++ visual_behavior/visualization/qc/container_plots.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+
+def plot_max_projection_images_for_container(ophys_container_id):
+    ## function for getting experiment_ids belonging to container
+    ## create figure with sub-axes for each experiment
+    ## loop through experiments
+    ##### load max projection using data_loading.py function
+    ## plot each on axis
+    ## return fig? or save fig? 
+
+
+
+
+
