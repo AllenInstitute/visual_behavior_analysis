dda14ebf02e50b92874e94802c69b46cfe9e8794
Kate Roll <kater@alleninstitute.org>
2020-02-11 14:52:53 -0800

    changed to ophys_experiment_id, added more data retrieval from LIMS

--- visual_behavior/visualization/qc/data_loading.py
+++ visual_behavior/visualization/qc/data_loading.py
@@ -99,14 +99,17 @@ def get_sdk_roi_masks(ophys_experiment_id):
 
 ################  FROM LIMS DATABASE  ################ # NOQA: E402
 
-####### EXPERIMENT ####### # NOQA: E402
 
 
-def get_lims_experiment_info(experiment_id):
+
+####### EXPERIMENT LEVEL ####### # NOQA: E402
+
+
+def get_lims_experiment_info(ophys_experiment_id):
     """uses an sqlite query to retrieve data from the lims2 database
 
     Arguments:
-        experiment_id {[type]} -- [description]
+        ophys_experiment_id {int} -- 9 digit ophys experiment ID
 
     Returns:
         table -- table with the following columns:
@@ -124,7 +127,7 @@ def get_lims_experiment_info(experiment_id):
                     "rig":
 
     """
-    experiment_id = int(experiment_id)
+    experiment_id = int(ophys_experiment_id)
     mixin = PostgresQueryMixin()
     # build query
     query = '''
@@ -156,14 +159,96 @@ def get_lims_experiment_info(experiment_id):
     join imaging_depths on imaging_depths.id = oe.imaging_depth_id
     join equipment on equipment.id = os.equipment_id
 
-    where oe.id = {}'''.format(experiment_id)
+    where oe.id = {}'''.format(ophys_experiment_id)
 
     lims_experiment_info = mixin.select(query)
 
     return lims_experiment_info
 
 
-####### CONTAINER ####### # NOQA: E402
+def get_current_segmentation_run_id(ophys_experiment_id):
+    """gets the id for the current cell segmentation run for a given experiment.
+        Queries LIMS via AllenSDK PostgresQuery function.
+
+    Arguments:
+       ophys_experiment_id {int} -- 9 digit ophys experiment ID
+
+    Returns:
+        int -- current cell segmentation run id
+    """
+
+    segmentation_run_table = get_lims_cell_segmentation_run_info(ophys_experiment_id)
+    current_segmentation_run_id = segmentation_run_table.loc[segmentation_run_table["current"] == True, ["id"][0]][0]
+    return current_segmentation_run_id
+
+
+def get_lims_cell_segmentation_run_info(ophys_experiment_id):
+    """Queries LIMS via AllenSDK PostgresQuery function to retrieve information on all segmentations run in the
+        ophys_cell_segmenatation_runs table for a given experiment
+    
+    Arguments:
+         ophys_experiment_id {int} -- 9 digit ophys experiment ID
+    
+    Returns:
+        dataframe --  dataframe with the following columns:
+                        id {int}:  9 digit segmentation run id
+                        run_number {int}: segmentation run number
+                        ophys_experiment_id{int}: 9 digit ophys experiment id
+                        current{boolean}: True/False True: most current segmentation run; False: not the most current segmentation run
+                        created_at{timestamp}:
+                        updated_at{timestamp}:
+    """
+
+    mixin = PostgresQueryMixin()
+    query = '''
+    select *
+    FROM ophys_cell_segmentation_runs
+    WHERE ophys_experiment_id = {} '''.format(ophys_experiment_id)
+    return mixin.select(query)
+
+
+def get_lims_cell_rois_table(ophys_experiment_id):
+    """Queries LIMS via AllenSDK PostgresQuery function to retrieve everything in the cell_rois table for a given experiment
+
+    Arguments:
+        experiment_id {int} -- 9 digit unique identifier for an ophys experiment
+
+    Returns:
+        dataframe -- returns dataframe with the following columns:
+            id:
+            cell_specimen_id:
+            ophys_experiment_id:
+            x:
+            y:
+            width:
+            height:
+            valid_roi: boolean(true/false), whether the roi passes or fails roi filtering
+            mask_matrix: boolean mask of roi
+            max_correction_up:
+            max_correction_down:
+            max_correction_right:
+            max_correction_left:
+            mask_image_plane:
+            ophys_cell_segmentation_run_id:
+
+    """
+    # query from AllenSDK
+
+    mixin = PostgresQueryMixin()
+    query = '''select cell_rois.*
+
+    from
+
+    ophys_experiments oe
+    join cell_rois on oe.id = cell_rois.ophys_experiment_id
+
+    where oe.id = {}'''.format(ophys_experiment_id)
+    lims_cell_rois_table = mixin.select(query)
+    return lims_cell_rois_table
+
+
+
+####### CONTAINER  LEVEL ####### # NOQA: E402
 
 
 def get_lims_container_info(ophys_container_id):
@@ -229,7 +314,7 @@ def get_lims_container_info(ophys_container_id):
 
 def get_timeseries_ini_wkf_info(lims_session_id):
     """use SQL and the LIMS well known file system to get the timeseries_XYT.ini file
-        for a given ophys session from a Scientifica rig 
+        for a given ophys session *from a Scientifica rig* 
 
     Arguments:
         lims_session_id {int} -- 9 digit lims_session_id
@@ -286,3 +371,9 @@ def get_pmt_gain_for_session(lims_session_id):
 
 # def get_pmt_gain_for_container(container_id):
 
+
+
+
+################  FROM MTRAIN DATABASE  ################ # NOQA: E402
+
+mtrain_api = PostgresQueryMixin(dbname="mtrain", user="mtrainreader", host="prodmtrain1", password="mtrainro", port=5432)
