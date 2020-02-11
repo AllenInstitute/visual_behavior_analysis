e597fe7853f8a8665fe3a037b56c2cea75ed2f32
Kate Roll <kater@alleninstitute.org>
2020-02-11 15:02:54 -0800

    added dependency package convert_level_1_to_level_2

--- visual_behavior/visualization/qc/data_loading.py
+++ visual_behavior/visualization/qc/data_loading.py
@@ -1,5 +1,6 @@
 from allensdk.brain_observatory.behavior.behavior_ophys_session import BehaviorOphysSession
 from allensdk.internal.api.behavior_ophys_api import BehaviorOphysLimsApi
+import visual_behavior.ophys.io.convert_level_1_to_level_2 as convert
 
 from allensdk.internal.api import PostgresQueryMixin
 
@@ -12,9 +13,9 @@ config = configp.ConfigParser()
 
 ####function inputs
 # ophys_experiment_id
-# ophys_session_id 
-# behavior_session_id 
-# ophys_container_id 
+# ophys_session_id
+# behavior_session_id
+# ophys_container_id
 
 
 ################  FROM SDK  ################ # NOQA: E402
@@ -24,74 +25,74 @@ def get_sdk_session_obj(ophys_experiment_id):
     """Use LIMS API from SDK to return session object
 
     Arguments:
-        experiment_id {int} -- 9 digit ophys experiment ID 
+        experiment_id {int} -- 9 digit ophys experiment ID
 
     Returns:
         session object -- session object from SDK
     """
-    api = BehaviorOphysLimsApi(experiment_id)
+    api = BehaviorOphysLimsApi(ophys_experiment_id)
     session = BehaviorOphysSession(api)
     return session
 
 
 def get_sdk_max_projection(ophys_experiment_id):
     """ uses SDK to return 2d max projection image of the microscope field of view
-   
-    
+
+
     Arguments:
         ophys_experiment_id {int} -- 9 digit ophys experiment ID
-    
+
     Returns:
         image -- can be visualized via plt.imshow(max_projection)
     """
-    session = get_sdk_session_obj(experiment_id)
+    session = get_sdk_session_obj(ophys_experiment_id)
     max_projection = session.max_projection
     return max_projection
 
 
 def get_sdk_ave_projection(ophys_experiment_id):
     """uses SDK to return 2d image of the 2-photon microscope filed of view, averaged
-        across the experiment. 
-    
+        across the experiment.
+
     Arguments:
-        ophys_experiment_id {int} -- 9 digit ophys experiment ID 
-    
+        ophys_experiment_id {int} -- 9 digit ophys experiment ID
+
     Returns:
         image -- can be visualized via plt.imshow(ave_projection)
     """
-    session = get_sdk_session_obj(experiment_id)
+    session = get_sdk_session_obj(ophys_experiment_id)
     ave_projection = session.average_projection
     return ave_projection
 
 
 def get_sdk_segmentation_mask_image(ophys_experiment_id):
     """uses SDK to return an array containing the masks of all cell ROIS
-    
+
     Arguments:
-       ophys_experiment_id {int} -- 9 digit ophys experiment ID 
-    
+       ophys_experiment_id {int} -- 9 digit ophys experiment ID
+
     Returns:
        array -- a 2D boolean array
                 visualized via plt.imshow(seg_mask_image)
     """
-    session = get_sdk_session_obj(experiment_id)
+    session = get_sdk_session_obj(ophys_experiment_id)
     seg_mask_image = session.segmentation_mask_image
     return seg_mask_image
 
 
 def get_sdk_roi_masks(ophys_experiment_id):
-    """uses sdk to return a dictionary with individual ROI masks for each cell specimen ID. 
-    
+    """uses sdk to return a dictionary with individual ROI masks for each cell specimen ID.
+
     Arguments:
         ophys_experiment_id {int} -- 9 digit ophys experiment ID
-    
+
     Returns:
         dictonary -- keys are cell specimen ids(ints)
                     values are 2d numpy arrays(binary array the size of the motion corrected 2photon FOV where 1's are the ROI/Cell mask).
                     specific cell masks can be visualized via plt.imshow(roi_masks[cell_specimen_id])
     """
 
-    session = get_sdk_session_obj(experiment_id)
+    session = get_sdk_session_obj(ophys_experiment_id)
     roi_masks = session.get_roi_masks
     return roi_masks
 
@@ -127,7 +128,7 @@ def get_lims_experiment_info(ophys_experiment_id):
                     "rig":
 
     """
-    experiment_id = int(ophys_experiment_id)
+    ophys_experiment_id = int(ophys_experiment_id)
     mixin = PostgresQueryMixin()
     # build query
     query = '''
@@ -185,10 +186,10 @@ def get_current_segmentation_run_id(ophys_experiment_id):
 def get_lims_cell_segmentation_run_info(ophys_experiment_id):
     """Queries LIMS via AllenSDK PostgresQuery function to retrieve information on all segmentations run in the
         ophys_cell_segmenatation_runs table for a given experiment
-    
+
     Arguments:
          ophys_experiment_id {int} -- 9 digit ophys experiment ID
-    
+
     Returns:
         dataframe --  dataframe with the following columns:
                         id {int}:  9 digit segmentation run id
@@ -272,7 +273,7 @@ def get_lims_container_info(ophys_container_id):
                     "rig":
                     "date_of_acquisition":
     """
-    container_id = int(ophys_container_id)
+    ophys_container_id = int(ophys_container_id)
 
     mixin = PostgresQueryMixin()
     # build query
@@ -312,12 +313,12 @@ def get_lims_container_info(ophys_container_id):
 ################  FROM WELL KNOWN FILE  ################ # NOQA: E402
 
 
-def get_timeseries_ini_wkf_info(lims_session_id):
+def get_timeseries_ini_wkf_info(ophys_session_id):
     """use SQL and the LIMS well known file system to get the timeseries_XYT.ini file
-        for a given ophys session *from a Scientifica rig* 
+        for a given ophys session *from a Scientifica rig*
 
     Arguments:
-        lims_session_id {int} -- 9 digit lims_session_id
+        ophys_session_id {int} -- 9 digit ophys session id
 
     Returns:
         list -- nested list with
@@ -333,7 +334,7 @@ def get_timeseries_ini_wkf_info(lims_session_id):
     AND wkf.storage_directory LIKE '%ophys_session_{0}%'
     AND os.id = {0}
 
-    '''.format(lims_session_id)
+    '''.format(ophys_session_id)
 
     lims_cursor = get_psql_dict_cursor()
     lims_cursor.execute(QUERY)
@@ -342,17 +343,17 @@ def get_timeseries_ini_wkf_info(lims_session_id):
     return timeseries_ini_wkf_info
 
 
-def get_timeseries_ini_location(lims_session_id):
+def get_timeseries_ini_location(ophys_session_id):
     """use SQL and the LIMS well known file system to get info for the timeseries_XYT.ini file
         for a given ophys session, and then parses that information to get the filepath
 
     Arguments:
-        llims_session_id {int} -- 9 digit lims_session_id
+        ophys_session_id {int} -- 9 digit ophys session id
 
     Returns:
         filepath -- [description]
     """
-    timeseries_ini_wkf_info = get_timeseries_ini_wkf_info(lims_session_id)
+    timeseries_ini_wkf_info = get_timeseries_ini_wkf_info(ophys_session_id)
     timeseries_ini_path = timeseries_ini_wkf_info[0]['?column?'] #idk why it's ?column? but it is :(
     timeseries_ini_path = timeseries_ini_path.replace('/allen', '//allen')  # works with windows and linux filepaths
     return timeseries_ini_path
@@ -364,8 +365,8 @@ def pmt_gain_from_timeseries_ini(timeseries_ini_path):
     return pmt_gain
 
 
-def get_pmt_gain_for_session(lims_session_id):
-    timeseries_ini_path = get_timeseries_ini_location(lims_session_id)
+def get_pmt_gain_for_session(ophys_session_id):
+    timeseries_ini_path = get_timeseries_ini_location(ophys_session_id)
     pmt_gain = pmt_gain_from_timeseries_ini(timeseries_ini_path)
     return pmt_gain
 
