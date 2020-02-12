cb413022c5cdf3a61f65d70f1abc73e25f7722c0
matchings <marinag@alleninstitute.org>
2020-02-11 16:30:44 -0800

    first container plot example

--- visual_behavior/visualization/qc/container_plots.py
+++ visual_behavior/visualization/qc/container_plots.py
@@ -1,13 +1,34 @@
 import matplotlib.pyplot as plt
+from visual_behavior.visualization import utils as ut
+from visual_behavior.visualization.qc import data_loading as dl
+from visual_behavior.visualization.qc import experiment_plots as ep
+from visual_behavior.visualization.qc import session_plots as sp
 
 
-def plot_max_projection_images_for_container(ophys_container_id):
+
+def plot_max_projection_images_for_container(ophys_container_id, save_figure=True):
     ## function for getting experiment_ids belonging to container
     ## create figure with sub-axes for each experiment
     ## loop through experiments
     ##### load max projection using data_loading.py function
     ## plot each on axis
-    ## return fig? or save fig? 
+    ## return fig? or save fig?
+    ophys_experiment_ids = dl.get_ophys_experiment_ids_for_ophys_container_id(ophys_container_id)
+
+    figsize = (25, 5)
+    fig, ax = plt.subplots(1,6, figsize=figsize)
+    for i,ophys_experiment_id in enumerate(ophys_experiment_ids):
+        ax[i] = ep.plot_max_intensity_projection_for_experiment(ophys_experiment_id, ax=ax[i])
+        ax[i].axis('off')
+        session_type = dl.get_session_type_for_ophys_experiment_id(ophys_experiment_id)
+        ax[i].set_title(str(ophys_experiment_id)+'\n'+session_type)
+
+    if save_figure:
+        ut.save_figure(fig, figsize, dl.get_container_plots_dir(), 'max_intensity_projection',
+                       'container_'+str(ophys_container_id))
+
+
+
 
 
 
--- visual_behavior/visualization/qc/experiment_plots.py
+++ visual_behavior/visualization/qc/experiment_plots.py
@@ -1,4 +1,15 @@
+import numpy as np
 import matplotlib.pyplot as plt
+from visual_behavior.visualization.qc import data_loading as dl
 
-from allensdk.brain_observatory.behavior.behavior_ophys_session import BehaviorOphysSession
+
+
+def plot_max_intensity_projection_for_experiment(ophys_experiment_id, ax=None):
+    if ax is None:
+        fig, ax = plt.subplots()
+    max_projection = dl.get_sdk_max_projection(ophys_experiment_id)
+    ax.imshow(max_projection, cmap='gray', vmax=np.amax(max_projection)/2.)
+    ax.axis('off')
+    return ax
+
 
