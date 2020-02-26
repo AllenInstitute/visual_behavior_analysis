06b9e7f78e8459d395b9c4ffe11c0f265a8d101e
Doug Ollerenshaw <dougo@alleninstitute.org>
2020-02-26 10:20:37 -0800

    function to initialize legend

--- visual_behavior/plotting.py
+++ visual_behavior/plotting.py
@@ -102,3 +102,26 @@ def save_figure(
             orientation='landscape',
             dpi=dpi
         )
+
+def initialize_legend(ax, colors, linewidth=1, linestyle='-', marker=None, markersize=8, alpha=1):
+    """ initializes a legend on an axis to ensure that first entries match desired line colors
+    Parameters
+    ----------
+    ax : matplotlib axis
+        the axis to apply the legend to
+    colors : list
+        marker colors for the legend items
+    linewdith : int, optional
+        width of lines (default 1)
+    linestyle : str, optional
+        matplotlib linestyle (default '-')
+    marker : str, optional
+        matplotlib marker style (default None)
+    markersize : int, optional
+        matplotlib marker size (default 8)
+    alpha : float, optional
+        matplotlib opacity, varying from 0 to 1 (default 1)
+    """
+    for color in colors:
+        ax.plot(np.nan, np.nan, color=color, linewidth=linewidth,
+                linestyle=linestyle, marker=marker, markersize=markersize, alpha=alpha)
