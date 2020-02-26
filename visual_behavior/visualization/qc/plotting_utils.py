fac5874d5e9cf9cb6c404689e69503ef6b519469
Kate Roll <kater@alleninstitute.org>
2020-02-25 21:32:23 -0800

    color dictionary by experiment id

--- visual_behavior/visualization/qc/plotting_utils.py
+++ visual_behavior/visualization/qc/plotting_utils.py
@@ -3,6 +3,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from visual_behavior.visualization.qc import data_processing as dp
+
 
 def gen_stage_name_color_palettes():
     reds = sns.color_palette('Reds_r', 6)[:5][::2]
@@ -12,20 +14,31 @@ def gen_stage_name_color_palettes():
 
 
 def gen_ophys_stage_name_colors_dict():
-    color_palettes = gen_stage_name_color_palettes()
-    stage_name_colors_dict = {'OPHYS_1_images_A': color_palettes[0],
-                              'OPHYS_4_images_A': color_palettes[0],
-                              'OPHYS_2_images_A_passive': color_palettes[1],
-                              'OPHYS_5_images_A_passive': color_palettes[1],
-                              'OPHYS_3_images_A': color_palettes[2],
-                              'OPHYS_6_images_A': color_palettes[2],
-                              'OPHYS_4_images_B': color_palettes[3],
-                              'OPHYS_1_images_B': color_palettes[3],
-                              'OPHYS_5_images_B_passive': color_palettes[4],
-                              'OPHYS_2_images_B_passive': color_palettes[4],
-                              'OPHYS_6_images_B': color_palettes[5],
-                              'OPHYS_3_images_B': color_palettes[5]}
+    stage_color_palette = gen_stage_name_color_palettes()
+    stage_name_colors_dict = {'OPHYS_0_images_A_habituation': stage_color_palette[8],
+                              'OPHYS_1_images_A': stage_color_palette[0],
+                              'OPHYS_4_images_A': stage_color_palette[0],
+                              'OPHYS_2_images_A_passive': stage_color_palette[1],
+                              'OPHYS_5_images_A_passive': stage_color_palette[1],
+                              'OPHYS_3_images_A': stage_color_palette[2],
+                              'OPHYS_6_images_A': stage_color_palette[2],
+                              'OPHYS_4_images_B': stage_color_palette[3],
+                              'OPHYS_1_images_B': stage_color_palette[3],
+                              'OPHYS_5_images_B_passive': stage_color_palette[4],
+                              'OPHYS_2_images_B_passive': stage_color_palette[4],
+                              'OPHYS_6_images_B': stage_color_palette[5],
+                              'OPHYS_3_images_B': stage_color_palette[5],
+                              'VisCodingTargetedMovieClips': stage_color_palette[7],
+                              'OPHYS_7_receptive_field_mapping': stage_color_palette[7],
+                              'full_field_test': stage_color_palette[7]}
     return stage_name_colors_dict
+
+
+def map_stage_name_colors_to_ophys_experiment_ids(dataframe):
+    stage_name_colors = gen_ophys_stage_name_colors_dict()
+    ophys_experiment_id_stage_name_dict = dp.ophys_experiment_id_stage_name_dict(dataframe)
+    experiment_id_color_dict = dict((ophys_experiment_id_stage_name_dict.get(key), value) for (key, value) in stage_name_colors.items())
+    return experiment_id_color_dict
 
 
 def boxoff(ax, keep='left', yaxis=True):
