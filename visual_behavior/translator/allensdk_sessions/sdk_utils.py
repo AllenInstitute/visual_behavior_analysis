942a47270016b001984f95c0efb97fb1a36e6682
Nick Ponvert <nickponvert@gmail.com>
2020-03-25 23:04:57 -0700

    osid -> ophys_session_id

--- visual_behavior/translator/allensdk_sessions/sdk_utils.py
+++ visual_behavior/translator/allensdk_sessions/sdk_utils.py
@@ -6,29 +6,29 @@ import numpy as np
 # Currently works on SDK v.1.3.2
 
 
-def get_bsid_from_osid(osid, cache):
+def get_bsid_from_ophys_session_id(ophys_session_id, cache):
     '''
         Finds the behavior_session_id associated with an ophys_session_id
         ARGS
-            osid    ophys_session_id
+            ophys_session_id    ophys_session_id
             cache   cache from BehaviorProjectCache
         Returns
             bsid    behavior_session_id for that ophys_session
     '''
     ophys_sessions = cache.get_session_table()
-    if osid not in ophys_sessions.index:
+    if ophys_session_id not in ophys_sessions.index:
         raise Exception('ophys_session_id not in session table')
-    return ophys_sessions.loc[osid].behavior_session_id
+    return ophys_sessions.loc[ophys_session_id].behavior_session_id
 
 
-def get_osid_from_bsid(bsid, cache):
+def get_ophys_session_id_from_bsid(bsid, cache):
     '''
         Finds the ophys_session_id associated with an behavior_session_id
         ARGS
             bsid    behavior_session_id
             cache   cache from BehaviorProjectCache
         Returns
-            osid    ophys_session_id for that behavior_session
+            ophys_session_id    ophys_session_id for that behavior_session
     '''
     behavior_sessions = cache.get_behavior_session_table()
     if bsid not in behavior_sessions.index:
@@ -48,15 +48,15 @@ def get_oeid_from_bsid(bsid, cache, exp_num=0):
                     For scientifica sessions, there is only one experiment per behavior_session, so exp_num = 0
                     For mesoscope, there are 8 experiments, so exp_num = (0,7)
     '''
-    osid = get_osid_from_bsid(bsid, cache)
-    return get_oeid_from_osid(osid, cache, exp_num=exp_num)
+    ophys_session_id = get_ophys_session_id_from_bsid(bsid, cache)
+    return get_oeid_from_ophys_session_id(ophys_session_id, cache, exp_num=exp_num)
 
 
-def get_oeid_from_osid(osid, cache, exp_num=0):
+def get_oeid_from_ophys_session_id(ophys_session_id, cache, exp_num=0):
     '''
         Finds the behavior_session_id associated with an ophys_session_id
         ARGS
-            osid    ophys_session_id
+            ophys_session_id    ophys_session_id
             cache   cache from BehaviorProjectCache
             exp_num index for which experiment to grab the id for
         Returns
@@ -65,9 +65,9 @@ def get_oeid_from_osid(osid, cache, exp_num=0):
                     For mesoscope, there are 8 experiments, so exp_num = (0,7)
     '''
     ophys_sessions = cache.get_session_table()
-    if osid not in ophys_sessions.index:
+    if ophys_session_id not in ophys_sessions.index:
         raise Exception('ophys_session_id not in session table')
-    experiments = ophys_sessions.loc[osid].ophys_experiment_id
+    experiments = ophys_sessions.loc[ophys_session_id].ophys_experiment_id
     return experiments[0]
 
 
@@ -86,14 +86,14 @@ def get_bsid_from_oeid(oeid, cache):
     return ophys_experiments.loc[oeid].behavior_session_id
 
 
-def get_osid_from_oeid(oeid, cache):
+def get_ophys_session_id_from_oeid(oeid, cache):
     '''
         Finds the ophys_session_id associated with an ophys_experiment_id
         ARGS
             oeid    ophys_experiment_id
             cache   cache from BehaviorProjectCache
         Returns
-            osid    ophys_session_id for that ophys_experiment
+            ophys_session_id    ophys_session_id for that ophys_experiment
     '''
     ophys_experiments = cache.get_experiment_table()
     if oeid not in ophys_experiments.index:
@@ -115,8 +115,8 @@ def get_specimen_id_from_donor_id(d_id, cache):
     ophys_sessions = cache.get_session_table()
     behavior_sessions = cache.get_behavior_session_table()
     x = behavior_sessions.query('donor_id == @d_id')['ophys_session_id']
-    osid = x[~x.isnull()].values[0].astype(int)  # noqa: F841
-    specimen_id = ophys_sessions.query('ophys_session_id ==@osid')['specimen_id'].values[0]
+    ophys_session_id = x[~x.isnull()].values[0].astype(int)  # noqa: F841
+    specimen_id = ophys_sessions.query('ophys_session_id ==@ophys_session_id')['specimen_id'].values[0]
     return specimen_id
 
 
@@ -131,8 +131,8 @@ def get_donor_id_from_specimen_id(s_id, cache):
     '''
     ophys_sessions = cache.get_session_table()
     behavior_sessions = cache.get_behavior_session_table()
-    osid = ophys_sessions.query('specimen_id == @s_id').iloc[0].name  # noqa: F841
-    donor_id = behavior_sessions.query('ophys_session_id ==@osid')['donor_id'].values[0]
+    ophys_session_id = ophys_sessions.query('specimen_id == @s_id').iloc[0].name  # noqa: F841
+    donor_id = behavior_sessions.query('ophys_session_id ==@ophys_session_id')['donor_id'].values[0]
     return donor_id
 
 
