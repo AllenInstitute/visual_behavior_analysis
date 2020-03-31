321985871c077324bdd2f5bea68d968bcbf94aaf
Doug Ollerenshaw <dougo@alleninstitute.org>
2020-03-31 08:15:03 -0700

    make cluster generation code more general

--- visual_behavior/visualization/qc/run_save_all_container_plots.py
+++ visual_behavior/visualization/qc/run_save_all_container_plots.py
@@ -1,12 +1,16 @@
+from pbstools import pbstools
 import sys
 from visual_behavior.visualization.qc import data_loading
+import argparse
+import os
 
-sys.path.append('/allen/programs/braintv/workgroups/nc-ophys/nick.ponvert/src/')
-from pbstools import pbstools
+sys.path.append('/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/src/')
 
-python_executable = r"/home/marinag/anaconda2/envs/visual_behavior_sdk/bin/python"
-python_file = r"/home/marinag/visual_behavior_analysis/visual_behavior/visualization/qc/save_all_container_plots.py"
-job_dir = r"/allen/programs/braintv/workgroups/nc-ophys/Marina/ClusterJobs/JobRecords"
+parser = argparse.ArgumentParser(description='run container qc plot generation functions on the cluster')
+parser.add_argument('--env', type=str, default='', metavar='name of conda environment to use')
+parser.add_argument('--scriptname', type=str, default='save_all_container_plots.py', metavar='name of script to run (must be in same folder)')
+
+job_dir = r"/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/cluster_jobs/vba_qc_plots"
 
 job_settings = {'queue': 'braintv',
                 'mem': '30g',
@@ -18,6 +22,10 @@ job_settings = {'queue': 'braintv',
 container_ids = data_loading.get_filtered_ophys_container_ids()
 
 if __name__ == "__main__":
+    args = parser.parse_args()
+    python_executable = "{}/.conda/envs/{}/bin/python".format(os.path.expanduser('~'), args.env)
+    python_file = os.path.join(os.getcwd(), args.scriptname)
+
     for container_id in container_ids:
 
         job_title = 'container_{}'.format(container_id)
