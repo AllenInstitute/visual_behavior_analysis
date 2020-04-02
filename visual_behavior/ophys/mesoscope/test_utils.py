0bdcca59a7d592d19f72cd2a1c6bfe301c3b9591
Natalia Orlova <nataliao@alleninstitute.org>
2020-04-02 15:59:03 -0700

    tests for utils

--- visual_behavior/ophys/mesoscope/test_utils.py
+++ visual_behavior/ophys/mesoscope/test_utils.py
@@ -1,10 +1,18 @@
 from visual_behavior.ophys.mesoscope.utils import *
 
+
 def test_get_lims_done_sessions():
     s = 'session_id'
     lims_done, lims_not_done, ica_success = get_lims_done_sessions()
+
     lims_done = lims_done[s].drop_duplicates().values
     lims_not_done = lims_not_done[s].drop_duplicates().values
+    ica_success = ica_success[s].drop_duplicates().values
+
+    print(f'lims done: {len(lims_done)}')
+    print(f'lims_not_done: {len(lims_not_done)}')
+    print(f'ica_success: {len(ica_success)}')
+
     assert len(lims_done) != 0, 'did not find any lims_done sessions'
     assert len(lims_not_done) != 0, 'did not find any lims_done sessions'
     assert len(lims_done)+len(lims_not_done) == len(ica_success), 'numbers of lims_done and lims_not_done sessions do not add up'
@@ -16,6 +24,12 @@ def test_get_ica_done_sessions():
     ica_done, ica_not_done, meso_data = get_ica_done_sessions()
     ica_done = ica_done[s].drop_duplicates().values
     ica_not_done = ica_not_done[s].drop_duplicates().values
+    meso_data = meso_data[s].drop_duplicates().values
+
+    print(f'lims done: {len(ica_done)}')
+    print(f'lims_not_done: {len(ica_not_done)}')
+    print(f'ica_success: {len(meso_data)}')
+    
     assert len(ica_done) != 0, 'did not find any ica_done sessions'
     assert len(ica_not_done) != 0, 'did not find any ica_done sessions'
     assert len(ica_done)+len(ica_not_done) == len(meso_data), 'numbers of ica_done and ica_not_done sessions do not add up'
