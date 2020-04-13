eab9de2bd0f25108fa921abbf2d5591ae9001710
Natalia Orlova <nataliao@alleninstitute.org>
2020-04-13 10:10:30 -0700

    more plot adjustments - mixing matrix ploting

--- visual_behavior/ophys/mesoscope/crosstalk_unmix.py
+++ visual_behavior/ophys/mesoscope/crosstalk_unmix.py
@@ -758,12 +758,12 @@ class MesoscopeICA(object):
                                                                                                           traces_after[1],
                                                                                                           generate_plot_data=True)
             f = plt.figure(figsize=(30, 10))
+            plt.rcParams.update({'font.size': 28})
             plt.suptitle(f"Crosstalk plost for cell {roi_name}")
             xlabel = "signal"
             ylabel = "crosstalk"
             # plot crosstalk before demxing
             plt.subplot(131)
-            plt.rcParams.update({'font.size': 18})
             plt.imshow(h_b, interpolation='nearest', origin='low',
                        extent=[xedges_b[0], xedges_b[-1], yedges_b[0], yedges_b[-1]], aspect='auto', norm=LogNorm())
             cbar = plt.colorbar()
@@ -774,12 +774,12 @@ class MesoscopeICA(object):
             plt.ylim((yedges_b[0], yedges_b[-1]))
             plt.xlabel(xlabel)
             plt.ylabel(ylabel)
-            title = f"Crosstalk before: {crosstalk[1]}\n{fitfn_b} R2={round(r_value_b, 2)}"
+            title = f"Crosstalk before: {round(crosstalk[0], 2)}\n{fitfn_b} R2={round(r_value_b, 2)}"
             plt.title(title, linespacing=0.5)
 
             # plot crosstalk after demxing
             plt.subplot(132)
-            plt.rcParams.update({'font.size': 18})
+            plt.rcParams.update({'font.size': 28})
             plt.imshow(h_a, interpolation='nearest', origin='low',
                        extent=[xedges_a[0], xedges_a[-1], yedges_a[0], yedges_a[-1]], aspect='auto', norm=LogNorm())
             cbar = plt.colorbar()
@@ -790,13 +790,13 @@ class MesoscopeICA(object):
             plt.ylim((yedges_a[0], yedges_a[-1]))
             plt.xlabel(xlabel)
             plt.ylabel(ylabel)
-            title = f"Crosstalk after: {crosstalk[1]}\n{fitfn_a} R2={round(r_value_a, 2)}"
+            title = f"Crosstalk after: {round(crosstalk[1], 2)}\n{fitfn_a} R2={round(r_value_a, 2)}"
             plt.title(title, linespacing=0.5)
 
             # add mixing matrix info to the plot
             plt.subplot(133)
             ax = plt.gca()
-            plt.rcParams.update({'font.size': 18})
+            plt.rcParams.update({'font.size': 28})
             a = mixing
             plt.text(2, 6, f"Mixing Matrix:\n[{round(a[0,0])}, [{round(a[0,1])}\n[{round(a[1,0])},[{round(a[1,1])}]]] ", fontsize=35, linespacing=1.5)
             ax.spines['top'].set_visible(False)
