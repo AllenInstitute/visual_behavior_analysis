04c6a44269099a447e5de3233b8ffafc40ae4d34
Doug Ollerenshaw <dougo@alleninstitute.org>
2020-04-13 08:38:59 -0700

    updates to cluster generation code

--- scripts/run_sdk_validation.py
+++ scripts/run_sdk_validation.py
@@ -2,9 +2,12 @@ import os
 import argparse
 import sys
 import time
+import pandas as pd
+import numpy as  np
 import visual_behavior.validation.sdk as sdk_validation
+import visual_behavior.visualization.qc.data_loading as dl
 sys.path.append('/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/src/')
-from pbstools import pbstools # NOQA E402
+from pbstools import pbstools  # NOQA E402
 
 
 parser = argparse.ArgumentParser(description='run sdk validation')
@@ -13,7 +16,7 @@ parser.add_argument('--env', type=str, default='visual_behavior', metavar='name 
 job_dir = r"/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/cluster_jobs/sdk_validation"
 
 job_settings = {'queue': 'braintv',
-                'mem': '8g',
+                'mem': '10g',
                 'walltime': '0:10:00',
                 'ppn': 1,
                 }
@@ -26,11 +29,16 @@ if __name__ == "__main__":
 
     cache = sdk_validation.get_cache()
     behavior_session_table = cache.get_behavior_session_table()
+    filtered_ophys_session_table = dl.get_filtered_ophys_sessions_table()
 
     validation_results = sdk_validation.get_validation_results().sort_index()
 
-    for ii,behavior_session_id in enumerate(behavior_session_table.index.values):
-        if behavior_session_id not in validation_results.index:
+    for ii, behavior_session_id in enumerate(behavior_session_table.index.values):
+        condition_1 = behavior_session_id not in validation_results.index
+        condition_2 = pd.notnull(behavior_session_table.loc[behavior_session_id]['ophys_session_id'])
+        condition_3 = behavior_session_table.loc[behavior_session_id]['ophys_session_id'] in filtered_ophys_session_table['ophys_session_id'].tolist()
+
+        if condition_1:# and condition_2 and condition_3:
             print('starting cluster job for {}'.format(behavior_session_id))
             job_title = 'sdk_validation_bsid_{}'.format(behavior_session_id)
             pbstools.PythonJob(
@@ -42,3 +50,4 @@ if __name__ == "__main__":
                 **job_settings
             ).run(dryrun=False)
             time.sleep(0.001)
+
