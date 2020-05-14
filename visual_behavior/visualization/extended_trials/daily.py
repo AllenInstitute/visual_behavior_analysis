3525cfff6a22f3de21e28b4cf0111b0fdeafb019
Corbett Bennett <corbettb@alleninstitute.org>
2020-05-13 20:33:55 -0700

    add dprime legend to make_daily_figure

--- visual_behavior/visualization/extended_trials/daily.py
+++ visual_behavior/visualization/extended_trials/daily.py
@@ -205,22 +205,25 @@ def make_rolling_response_probability_plot(hit_rate, fa_rate, ax, palette='trial
     ax.set_xlim(-0.1, 1.1)
 
 
-def make_rolling_dprime_plot(d_prime, ax, format='vertical', peak_dprime=None, color='black', append_ticks=False):
+def make_rolling_dprime_plot(d_prime, ax, format='vertical', peak_dprime=None, color='black', append_ticks=False, line_label=None):
     # if d_prime is a scalar, make it an array to avoid issues below
     if not hasattr(d_prime, '__len__'):
         d_prime = np.array([d_prime])
     if format == 'vertical':
-        ax.plot(d_prime, np.arange(len(d_prime)), color=color, linewidth=2)
+        line, = ax.plot(d_prime, np.arange(len(d_prime)), color=color, linewidth=2)
         ax.set_xlabel("d'", fontsize=14)
         if peak_dprime is not None:
             ax.axvline(peak_dprime, linestyle='--', color=color)  # peak dprime line
     elif format == 'horizontal':
-        ax.plot(np.arange(len(d_prime)), d_prime, color=color, linewidth=2)
+        line, = ax.plot(np.arange(len(d_prime)), d_prime, color=color, linewidth=2)
         ax.set_ylabel("d'", fontsize=14)
         if peak_dprime is not None:
             ax.axhline(peak_dprime, linestyle='--', color=color)  # peak dprime line
     ax.set_title("Rolling d'", fontsize=16)
-
+    
+    if line_label is not None:
+        line.set_label(line_label)
+        
     if peak_dprime is not None:
         if append_ticks:
             current_xticks = ax.get_xticks()
@@ -330,7 +333,8 @@ def make_daily_figure(
     ax[2].axvline(mean_rate, color='0.5', linestyle=':')
     
     # CB added to display both the trial adjusted and legacy dprime metric on same plot
-    for apply_trial_number_limit, first_valid_trial, append_ticks, color in zip([False, True], [50, 100], [False, True], ['black', 'grey']):
+    for apply_trial_number_limit, first_valid_trial, append_ticks, line_label, color \
+    in zip([False, True], [50, 100], [False, True], ['raw', 'adjusted'],['black', 'grey']):
         hit_rate, fa_rate, d_prime = get_response_rates(
                 df_nonaborted,
                 sliding_window=sliding_window,
@@ -338,10 +342,10 @@ def make_daily_figure(
                 )
         peak_dprime = session_metrics.peak_dprime(extended_trials, first_valid_trial=first_valid_trial, apply_trial_number_limit=apply_trial_number_limit)
         if not np.isnan(peak_dprime):
-            make_rolling_dprime_plot(d_prime, ax[3], peak_dprime=peak_dprime, append_ticks=append_ticks, color=color)
+            make_rolling_dprime_plot(d_prime, ax[3], peak_dprime=peak_dprime, append_ticks=append_ticks, line_label=line_label, color=color)
         else:
-            make_rolling_dprime_plot(d_prime, ax[3], append_ticks=append_ticks, color=color)
-
+            make_rolling_dprime_plot(d_prime, ax[3], append_ticks=append_ticks, line_label=line_label, color=color)
+    ax[3].legend(loc=0)
     plt.subplots_adjust(top=0.9)
     fig.suptitle('mouse = ' + mouse_id + ', ' + date, fontsize=20)
 
