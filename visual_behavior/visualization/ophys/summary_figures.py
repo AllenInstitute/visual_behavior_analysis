46b1630b1aacdd879a80d200605af18a640518f1
Doug Ollerenshaw <dougo@alleninstitute.org>
2020-05-21 13:00:57 -0700

    move placeAxesOnGrid to utils

--- visual_behavior/visualization/ophys/summary_figures.py
+++ visual_behavior/visualization/ophys/summary_figures.py
@@ -9,8 +9,9 @@ import numpy as np
 import seaborn as sns
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from visual_behavior.visualization.utils import save_figure
+from visual_behavior.visualization.utils import save_figure, placeAxesOnGrid
 from visual_behavior.ophys.response_analysis import utilities as ut
+import visual_behavior.data_access.loading as loading
 
 # from visual_behavior.visualization.ophys import population_summary_figures as psf
 
@@ -20,55 +21,6 @@ sns.set_context('notebook', font_scale=1.5, rc={'lines.markeredgewidth': 2})
 sns.set_style('white',
               {'axes.spines.right': False, 'axes.spines.top': False, 'xtick.bottom': True, 'ytick.left': True, })
 sns.set_palette('deep')
-
-
-def placeAxesOnGrid(fig, dim=[1, 1], xspan=[0, 1], yspan=[0, 1], wspace=None, hspace=None, sharex=False, sharey=False):
-    '''
-    Takes a figure with a gridspec defined and places an array of sub-axes on a portion of the gridspec
-
-    Takes as arguments:
-        fig: figure handle - required
-        dim: number of rows and columns in the subaxes - defaults to 1x1
-        xspan: fraction of figure that the subaxes subtends in the x-direction (0 = left edge, 1 = right edge)
-        yspan: fraction of figure that the subaxes subtends in the y-direction (0 = top edge, 1 = bottom edge)
-        wspace and hspace: white space between subaxes in vertical and horizontal directions, respectively
-
-    returns:
-        subaxes handles
-    '''
-    import matplotlib.gridspec as gridspec
-
-    outer_grid = gridspec.GridSpec(100, 100)
-    inner_grid = gridspec.GridSpecFromSubplotSpec(dim[0], dim[1],
-                                                  subplot_spec=outer_grid[int(100 * yspan[0]):int(100 * yspan[1]),
-                                                               # flake8: noqa: E999
-                                                               int(100 * xspan[0]):int(100 * xspan[1])], wspace=wspace,
-                                                  hspace=hspace)  # flake8: noqa: E999
-
-    # NOTE: A cleaner way to do this is with list comprehension:
-    # inner_ax = [[0 for ii in range(dim[1])] for ii in range(dim[0])]
-    inner_ax = dim[0] * [dim[1] * [
-        fig]]  # filling the list with figure objects prevents an error when it they are later replaced by axis handles
-    inner_ax = np.array(inner_ax)
-    idx = 0
-    for row in range(dim[0]):
-        for col in range(dim[1]):
-            if row > 0 and sharex == True:
-                share_x_with = inner_ax[0][col]
-            else:
-                share_x_with = None
-
-            if col > 0 and sharey == True:
-                share_y_with = inner_ax[row][0]
-            else:
-                share_y_with = None
-
-            inner_ax[row][col] = plt.Subplot(fig, inner_grid[idx], sharex=share_x_with, sharey=share_y_with)
-            fig.add_subplot(inner_ax[row, col])
-            idx += 1
-
-    inner_ax = np.array(inner_ax).squeeze().tolist()  # remove redundant dimension
-    return inner_ax
 
 
 def plot_max_projection_image(dataset, save_dir=None, folder='max_projection'):
