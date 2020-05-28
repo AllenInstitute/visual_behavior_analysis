69cf6a05e6bb312123f400e0e6f7ca4ddbdcdeb7
Doug Ollerenshaw <dougo@alleninstitute.org>
2020-05-27 17:12:45 -0700

    Move more imports to avoid test failures

--- tests/ophys/test_io.py
+++ tests/ophys/test_io.py
@@ -3,8 +3,7 @@ import pytest
 import platform
 import matplotlib
 matplotlib.use('Agg')
-from visual_behavior.ophys.io.create_analysis_files import create_analysis_files
-from visual_behavior.ophys.io.create_multi_session_mean_df import get_multi_session_mean_df
+
 EXPERIMENT_ID = 702134928
 PLATFORM = 'scientifica'
 
@@ -21,12 +20,14 @@ def test_convert_level_1_to_level_2(cache_dir):
 @pytest.mark.slow
 @pytest.mark.skipif(os.environ.get('PYTHONPATH', '').startswith('/home/circleci'), reason='Cannot test against real files on CircleCI')
 def test_create_analysis(cache_dir):
-
+    # import here to avoid errors on circleci
+    from visual_behavior.ophys.io.create_analysis_files import create_analysis_files
     create_analysis_files(EXPERIMENT_ID, cache_dir, overwrite_analysis_files=True)
 
 
 @pytest.mark.slow
 @pytest.mark.skipif(os.environ.get('PYTHONPATH', '').startswith('/home/circleci'), reason='Cannot test against real files on CircleCI')
 def test_create_multi_session_mean_df(cache_dir):
-
+    # import here to avoid errors on circleci
+    from visual_behavior.ophys.io.create_multi_session_mean_df import get_multi_session_mean_df
     get_multi_session_mean_df([EXPERIMENT_ID], cache_dir=cache_dir, platform=PLATFORM)
