d306582238f182e1e1dfe0dfbd9ffc8afdfe59bf
Natalia Orlova <nataliao@alleninstitute.org>
2020-07-13 12:07:28 -0700

    bug in validate_cells_crosstalk

--- visual_behavior/ophys/mesoscope/crosstalk_unmix.py
+++ visual_behavior/ophys/mesoscope/crosstalk_unmix.py
@@ -965,11 +965,17 @@ class MesoscopeICA(object):
                 self.rois_valid_ct[pkey] = copy.deepcopy(self.rois_valid[pkey])
                 crosstalk = self.crosstalk[pkey][tkey]
                 roi_names = self.rois_names_valid[pkey][tkey]
+
                 assert len(roi_names) == len(
                     crosstalk), "number of crosstalk values doesn't align with number of valid rois in ica.rois_names_valid"
+
                 for roi_name in roi_names:
-                    if crosstalk[str(roi_name)][0] > 130:
+                    if crosstalk[str(roi_name)][0]:
+                        if crosstalk[str(roi_name)][0] > 130:
+                            self.rois_valid_ct[pkey][str(roi_name)] = False
+                    else:
                         self.rois_valid_ct[pkey][str(roi_name)] = False
+
                 ju.write(ct_fn_roi, self.rois_valid_ct[pkey])
                 ju.write(ct_fn_np, self.rois_valid_ct[pkey])
             else:
