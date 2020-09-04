21972ec75e3df21df438d0fb09cd01ae221b4344
Doug Ollerenshaw <dougo@alleninstitute.org>
2020-09-04 13:18:49 -0700

    whitespace fixes on tests

--- tests/data_access/test_loading.py
+++ tests/data_access/test_loading.py
@@ -1,7 +1,9 @@
 import visual_behavior.data_access.loading as loading
 
+
 def test_get_filtered_ophys_session_table():
     session_table = session_table = loading.get_filtered_ophys_session_table()
+
 
 def test_get_filtered_ophys_experiment_table():
     experiment_table = loading.get_filtered_ophys_experiment_table()
