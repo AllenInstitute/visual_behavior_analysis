9ab542eb4018c83a9f991a90849ea4b4bfb125dd
Doug Ollerenshaw <dougo@alleninstitute.org>
2020-09-04 17:00:23 -0700

    modify loading tests to avoid running on CircleCI

--- tests/data_access/test_loading.py
+++ tests/data_access/test_loading.py
@@ -1,9 +1,14 @@
-import visual_behavior.data_access.loading as loading
+import os
+import pytest
 
+CIRCLECI = os.environ.get('PYTHONPATH', '').startswith('/home/circleci')
 
+@pytest.mark.skipif(CIRCLECI, reason='Function requires LIMS access')
 def test_get_filtered_ophys_session_table():
+    import visual_behavior.data_access.loading as loading
     session_table = session_table = loading.get_filtered_ophys_session_table()
 
-
+@pytest.mark.skipif(CIRCLECI, reason='Function requires LIMS access')
 def test_get_filtered_ophys_experiment_table():
+    import visual_behavior.data_access.loading as loading
     experiment_table = loading.get_filtered_ophys_experiment_table()
