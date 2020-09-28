dae104264c786e30c4902def6be0685961916046
matchings <marinag@alleninstitute.org>
2020-09-28 14:33:44 -0700

    add import

--- scripts/concatenate_stimulus_presentations.py
+++ scripts/concatenate_stimulus_presentations.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import numpy as np
 import pandas as pd
 import visual_behavior.data_access.loading as loading
 
@@ -8,34 +9,34 @@ import visual_behavior.data_access.loading as loading
 if __name__ == '__main__':
     import sys
     project_code = sys.argv[1][1:-1]
-    session_number = sys.argv[2][:-1]
+    session_number = int(sys.argv[2][:-1])
     print(project_code, session_number)
     print(type(project_code), type(session_number))
     save_dir = r'/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/decoding/data'
 
     experiments_table = loading.get_filtered_ophys_experiment_table()
     experiments = experiments_table[(experiments_table.project_code == project_code) &
-                                    (experiments_table.session_number == int(session_number))].copy()
+                                    (experiments_table.session_number == session_number)].copy()
 
     stim_df = pd.DataFrame()
     for ophys_session_id in experiments.ophys_session_id.unique():
-        try:
-            print(ophys_session_id, '-', np.where(experiments.ophys_session_id.unique()==ophys_session_id),
-                 'out of ', len(experiments.ophys_session_id.unique()))
-            ophys_experiment_id = loading.get_ophys_experiment_id_for_ophys_session_id(ophys_session_id)
-            dataset = loading.get_ophys_dataset(ophys_experiment_id)
-            stim = dataset.extended_stimulus_presentations.copy()
-            if 'hit_fraction' not in stim.keys():
-                stim['reward_rate'] = None
-                stim['hit_fraction'] = None
-                stim['engagement_state'] = None
-                stim['lick_on_next_flash'] = None
-            stim['rewarded'] = [True if len(rewards)>0 else False for rewards in stim.rewards.values]
-            stim = stim[['image_index', 'image_name', 'image_name_next_flash', 'image_index_next_flash',
-                    'omitted', 'change', 'pre_change', 'mean_running_speed', 'licked', 'rewarded',
-                    'reward_rate', 'hit_fraction', 'engagement_state', 'lick_on_next_flash']]
-            stim_df = pd.concat([stim_df, stim])
-        except:
-            print('problem for ophys_session_id:', ophys_session_id)
+        # try:
+        print(ophys_session_id, '-', np.where(experiments.ophys_session_id.unique()==ophys_session_id),
+             'out of ', len(experiments.ophys_session_id.unique()))
+        ophys_experiment_id = loading.get_ophys_experiment_id_for_ophys_session_id(ophys_session_id)
+        dataset = loading.get_ophys_dataset(ophys_experiment_id)
+        stim = dataset.extended_stimulus_presentations.copy()
+        if 'hit_fraction' not in stim.keys():
+            stim['reward_rate'] = None
+            stim['hit_fraction'] = None
+            stim['engagement_state'] = None
+            stim['lick_on_next_flash'] = None
+        stim['rewarded'] = [True if len(rewards)>0 else False for rewards in stim.rewards.values]
+        stim = stim[['image_index', 'image_name', 'image_name_next_flash', 'image_index_next_flash',
+                'omitted', 'change', 'pre_change', 'mean_running_speed', 'licked', 'rewarded',
+                'reward_rate', 'hit_fraction', 'engagement_state', 'lick_on_next_flash']]
+        stim_df = pd.concat([stim_df, stim])
+        # except:
+        #     print('problem for ophys_session_id:', ophys_session_id)
     stim_df.to_hdf(os.path.join(save_dir, 'stimulus_presentations_'+project_code+'_session_'+session_number+'.h5'), key='df')
     print('done')
