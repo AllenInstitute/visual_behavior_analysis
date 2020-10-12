2748f10714bdbafae6623a31e7cfd32186dfe0ab
Unknown <marinag@alleninstitute.org>
2020-10-12 11:18:18 -0700

    fix import for events smoothing

--- visual_behavior/data_access/loading.py
+++ visual_behavior/data_access/loading.py
@@ -3,6 +3,7 @@ from allensdk.internal.api.behavior_ophys_api import BehaviorOphysLimsApi
 from allensdk.brain_observatory.behavior.behavior_ophys_session import BehaviorOphysSession
 from allensdk.brain_observatory.behavior.behavior_project_cache import BehaviorProjectCache as bpc
 # from allensdk.core.lazy_property import LazyProperty, LazyPropertyMixin
+from visual_behavior.ophys.response_analysis import response_processing as rp
 from visual_behavior.data_access import filtering
 from visual_behavior.data_access import reformat
 from visual_behavior.data_access import processing
