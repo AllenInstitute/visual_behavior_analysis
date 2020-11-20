1dc1b222e59dc01204dcaa558a81e0489b11424a
matchings <marinag@alleninstitute.org>
2020-11-20 12:55:54 -0800

    fix events plots

--- visual_behavior/visualization/qc/container_plots.py
+++ visual_behavior/visualization/qc/container_plots.py
@@ -29,7 +29,7 @@ def plot_container_session_sequence(ophys_container_id, save_figure=True):
     experiments_table = data_loading.get_filtered_ophys_experiment_table(include_failed_data=True)
     expts = experiments_table[experiments_table.container_id == ophys_container_id].sort_values('date_of_acquisition')
     specimen_id = expts.specimen_id.unique()[0]
-    experiment_ids = expts.ophys_experiment_id.values
+    experiment_ids = expts.index.values
     session_type_color_map = ut.get_session_type_color_map()
 
     n_expts = len(expts)
@@ -37,7 +37,7 @@ def plot_container_session_sequence(ophys_container_id, save_figure=True):
     fail_x = []
     fail_tags = []
     for expt_ind, expt_id in enumerate(experiment_ids):
-        this_expt = expts[expts.ophys_experiment_id == expt_id]
+        this_expt = expts.loc[expt_id]
         img[expt_ind, 0, :] = session_type_color_map[this_expt['session_type'].values[0]]
         if this_expt['experiment_workflow_state'].values[0] == 'failed':
             fail_x.append(expt_ind)
@@ -51,7 +51,7 @@ def plot_container_session_sequence(ophys_container_id, save_figure=True):
 
     # plot text for acquisition date and session type
     for i, expt_id in enumerate(experiment_ids):
-        this_expt = expts[expts.ophys_experiment_id == expt_id]
+        this_expt = expts.loc[expt_id]
         ax.text(x=0.75, y=i, s=str(this_expt['date_of_acquisition'].values[0]).split(' ')[0],
                 ha='left', va='center', fontsize=20)
         ax.text(x=3, y=i, s=this_expt['session_type'].values[0], ha='left', va='center', fontsize=20)
