239e91b6fc3cd4d97663cf4cd5c815a8133a5f6c
matchings <marinag@alleninstitute.org>
2020-11-27 18:05:01 -0800

    create trials df for running

--- scripts/create_multi_session_df.py
+++ scripts/create_multi_session_df.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     session_number = int(sys.argv[2][:-1])
     print(project_code, session_number)
 
-    df_name = 'stimulus_run_speed_df'
+    df_name = 'trials_run_speed_df'
     conditions = ['change', 'image_name', 'engagement_state']
 
     df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False)
