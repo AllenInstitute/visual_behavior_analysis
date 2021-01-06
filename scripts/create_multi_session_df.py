d65b3692b852bf0a4aa7eb50e1586c2888d69da4
matchings <marinag@alleninstitute.org>
2021-01-05 17:07:24 -0800

    omission response df

--- scripts/create_multi_session_df.py
+++ scripts/create_multi_session_df.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     session_number = int(sys.argv[2][:-1])
     print(project_code, session_number)
 
-    df_name = 'stimulus_response_df'
-    conditions = ['change', 'image_name', 'engagement_state']
+    df_name = 'omission_response_df'
+    conditions = ['cell_specimen_id']
 
-    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True)
+    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False)
     print('done')
