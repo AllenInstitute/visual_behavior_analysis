bcbe4e645dd3f3a2a6d018ab26a5b6cec6735d8c
matchings <marinag@alleninstitute.org>
2021-01-19 17:36:42 -0800

    run speed df for omissions

--- scripts/create_multi_session_df.py
+++ scripts/create_multi_session_df.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     session_number = int(sys.argv[2][:-1])
     print(project_code, session_number)
 
-    df_name = 'stimulus_pupil_area_df'
-    conditions = ['change', 'image_name', 'engagement_state']
+    df_name = 'omission_run_speed_df'
+    conditions = ['image_name_previous_flash', 'engagement_state']
 
     df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, use_extended_stimulus_presentations=True)
     print('done')
