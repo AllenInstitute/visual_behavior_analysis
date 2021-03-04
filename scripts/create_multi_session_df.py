1327c4e53cf2e069c4c7072eec74fb93d3e0dc63
matchings <marinag@alleninstitute.org>
2021-03-03 21:45:46 -0800

    fix timeseries plots

--- scripts/create_multi_session_df.py
+++ scripts/create_multi_session_df.py
@@ -14,10 +14,10 @@ if __name__ == '__main__':
     session_number = int(sys.argv[2][:-1])
     print(project_code, session_number)
 
-    df_name = 'trials_response_df'
-    conditions = ['cell_specimen_id', 'go', 'hit', 'image_name']
+    df_name = 'stimulus_response_df'
+    # conditions = ['cell_specimen_id', 'go', 'hit', 'image_name']
     # conditions = ['cell_specimen_id', 'change', 'image_name', 'licked', 'hit_bout']
-    # conditions = ['cell_specimen_id', 'change', 'image_name', 'epoch']
+    conditions = ['cell_specimen_id', 'change', 'image_name', 'epoch']
 
     # df_name = 'trials_pupil_area_df'
     # conditions = ['ophys_experiment_id', 'go', 'hit', 'change_image_name']
