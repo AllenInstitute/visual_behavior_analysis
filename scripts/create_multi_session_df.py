b599feb66d6fc41f0b076a34f7d27158f81053ce
matchings <marinag@alleninstitute.org>
2021-03-03 17:11:38 -0800

    change responses df

--- scripts/create_multi_session_df.py
+++ scripts/create_multi_session_df.py
@@ -14,12 +14,13 @@ if __name__ == '__main__':
     session_number = int(sys.argv[2][:-1])
     print(project_code, session_number)
 
-    # df_name = 'stimulus_response_df'
-    # conditions = ['cell_specimen_id', 'change', 'image_name', 'licked', 'hit_bout']
+    df_name = 'trials_response_df'
+    conditions = ['cell_specimen_id', 'go', 'hit', 'image_name']
     # conditions = ['cell_specimen_id', 'change', 'image_name', 'epoch']
 
-    df_name = 'stimulus_pupil_area_df'
-    conditions = ['ophys_experiment_id', 'change', 'image_name', 'licked', 'hit_bout']
+    # df_name = 'stimulus_pupil_area_df'
+    # conditions = ['cell_specimen_id', 'hit', 'image_name'
+    # conditions = ['ophys_experiment_id', 'change', 'image_name', 'licked', 'hit_bout']
 
     df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, use_extended_stimulus_presentations=True)
     print('done')
