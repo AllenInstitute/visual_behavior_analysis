e9c52f8237ff8d49d31f3e978b92572a262e60a8
matchings <marinag@alleninstitute.org>
2021-03-04 16:04:38 -0800

    create trial response df for hit/miss

--- scripts/create_multi_session_df.py
+++ scripts/create_multi_session_df.py
@@ -14,14 +14,22 @@ if __name__ == '__main__':
     session_number = int(sys.argv[2][:-1])
     print(project_code, session_number)
 
-    df_name = 'stimulus_response_df'
-    # conditions = ['cell_specimen_id', 'go', 'hit', 'image_name']
+    ### have already generated
+
+    # df_name = 'stimulus_response_df'
+    # conditions = ['cell_specimen_id', 'change', 'image_name', 'epoch']
     # conditions = ['cell_specimen_id', 'change', 'image_name', 'licked', 'hit_bout']
-    conditions = ['cell_specimen_id', 'change', 'image_name', 'epoch']
 
     # df_name = 'trials_pupil_area_df'
     # conditions = ['ophys_experiment_id', 'go', 'hit', 'change_image_name']
+    # df_name = 'stimulus_pupil_area_df'
     # conditions = ['ophys_experiment_id', 'change', 'image_name', 'licked', 'hit_bout']
+
+
+    ### to generate
+
+    df_name = 'trials_response_df'
+    conditions = ['cell_specimen_id', 'go', 'hit', 'image_name']
 
     df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, use_extended_stimulus_presentations=True)
     print('done')
