2fd9262d08ffc1ebbf96e05d6939614bd1862841
Kate Roll <kater@alleninstitute.org>
2021-03-05 21:17:27 -0800

    function to get well known file dff_traces filepath

--- visual_behavior/data_access/from_lims.py
+++ visual_behavior/data_access/from_lims.py
@@ -1,5 +1,6 @@
 import os
 import warnings
+import from_lims_utilities as utils
 from allensdk.internal.api import PostgresQueryMixin
 
 
@@ -366,6 +367,23 @@ def get_timeseries_ini_filepath(ophys_session_id):
     AND wkf.storage_directory LIKE '%ophys_session_{0}%'
     AND os.id = {0}
     '''.format(ophys_session_id)
+    RealDict_object = mixin.select(query)
+    filepath = utils.get_filepath_from_wkf_realdict_object(RealDict_object)
+    return filepath
+
+
+def get_dff_traces_filepath(ophys_experiment_id):
+    ophys_experiment_id = int(ophys_experiment_id)
+    mixin = lims_engine
+    # build query
+    query = '''
+    SELECT wkf.storage_directory || wkf.filename AS dff_file
+    FROM ophys_experiments oe
+    JOIN well_known_files wkf ON wkf.attachable_id = oe.id
+    JOIN well_known_file_types wkft
+    ON wkft.id = wkf.well_known_file_type_id
+    WHERE wkft.name = 'OphysDffTraceFile'
+    AND oe.id = {}'''.format(ophys_experiment_id)
     RealDict_object = mixin.select(query)
     filepath = utils.get_filepath_from_wkf_realdict_object(RealDict_object)
     return filepath
