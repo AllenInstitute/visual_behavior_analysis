e73247c597d1256fdc756feb083bd2ce6e954656
Kate Roll <kater@alleninstitute.org>
2021-03-05 22:13:10 -0800

    added get_platform_json_filepath

--- visual_behavior/data_access/from_lims.py
+++ visual_behavior/data_access/from_lims.py
@@ -634,3 +634,27 @@ def get_ellipse_filepath(ophys_session_id):
     RealDict_object = mixin.select(QUERY)
     filepath = utils.get_filepath_from_wkf_realdict_object(RealDict_object)
     return filepath
+
+
+def get_platform_json_filepath(ophys_session_id):
+    """use SQL and the LIMS well known file system to get the
+        platform.json file information for a given
+        ophys_session_id
+
+    Arguments:
+        ophys_session_id {int} -- 9 digit ophys session ID
+
+    Returns:
+        [type] -- [description]
+    """
+    mixin = lims_engine
+    QUERY = '''
+    SELECT storage_directory || filename
+    FROM well_known_files
+    WHERE well_known_file_type_id = 746251277 AND
+    attachable_id = {0}
+
+    '''.format(ophys_session_id)
+    RealDict_object = mixin.select(QUERY)
+    filepath = utils.get_filepath_from_wkf_realdict_object(RealDict_object)
+    return filepath
