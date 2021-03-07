e8547412524ab06798857a77ebf401796a09efc3
Kate Roll <kater@alleninstitute.org>
2021-03-06 16:59:50 -0800

    added get_all_ids_for_behavior_session_id

--- visual_behavior/data_access/from_lims.py
+++ visual_behavior/data_access/from_lims.py
@@ -315,7 +315,31 @@ def get_ophys_session_id_for_behavior_session_id(behavior_session_id):
     return ophys_session_id
 
 # def get_ophys_container_id_for_behavior_session_id(behavior_session_id):
-# def get_all_ids_for_ophys_behavior_session_id(ophys_behavior_session_id):
+
+def get_all_ids_for_behavior_session_id(behavior_session_id):
+    behavior_session_id = int(behavior_session_id)
+    query = '''
+    SELECT
+    experiments.id as ophys_experiment_id,
+    experiments.ophys_session_id,
+    behavior.id as behavior_session_id,
+    container.visual_behavior_experiment_container_id as ophys_container_id,
+    sessions.visual_behavior_supercontainer_id as supercontainer_id
+
+    FROM
+    ophys_experiments experiments
+
+    JOIN behavior_sessions behavior on behavior.ophys_session_id = experiments.ophys_session_id
+    JOIN ophys_experiments_visual_behavior_experiment_containers container on container.ophys_experiment_id = experiments.id
+    JOIN ophys_sessions sessions on sessions.id = experiments.ophys_session_id
+
+    WHERE
+    behavior.id = {}
+    '''.format(behavior_session_id)
+    all_ids = mixin.select(query)
+    return all_ids
+
+
 # def get_supercontainer_id_for_behavior_session_id(ophys_behavior_session_id):
 
 # for ophys_container_id
