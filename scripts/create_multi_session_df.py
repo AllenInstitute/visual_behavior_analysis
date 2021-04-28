fbc6d61602acdfb19c738af493e457ec25184b95
matchings <marinag@alleninstitute.org>
2021-04-27 19:38:46 -0700

    create pupil area df

--- scripts/create_multi_session_df.py
+++ scripts/create_multi_session_df.py
@@ -13,15 +13,20 @@ if __name__ == '__main__':
     session_number = int(sys.argv[2][:-1])
     print(project_code, session_number)
 
-    df_name = 'trials_response_df'
-    conditions = ['cell_specimen_id', 'go']
+    # df_name = 'trials_response_df'
+    # conditions = ['cell_specimen_id', 'go']
+    df_name = 'trials_pupil_area_df'
+    conditions = ['ophys_experiment_id', 'go']
 
     df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, use_extended_stimulus_presentations=False)
     print('done')
 
     # df_name = 'omission_response_df'
     # conditions = ['cell_specimen_id']
-    #
-    # df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False,
-    #                              use_extended_stimulus_presentations=False)
-    # print('done')
+
+    df_name = 'omission_pupil_area_df'
+    conditions = ['ophys_experiment_id']
+
+    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False,
+                                 use_extended_stimulus_presentations=False)
+    print('done')
