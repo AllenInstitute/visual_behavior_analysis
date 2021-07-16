96cb6de36d7f00dacd2b9bba61e8a699d7809373
Doug Ollerenshaw <dougo@alleninstitute.org>
2021-07-16 10:46:07 -0700

    fix whitespace issues

--- visual_behavior/utilities.py
+++ visual_behavior/utilities.py
@@ -1015,11 +1015,11 @@ def cache_behavior_stats(behavior_session_id, engaged_only=True, cache_dir='/all
     '''
     behavior_stats_df = pd.DataFrame(
         get_behavior_stats(behavior_session_id, engaged_only),
-        index = [0]
+        index=[0]
     )
 
     filename = 'behavior_summary_behavior_session_id={}.h5'.format(behavior_session_id)
     behavior_stats_df.to_hdf(
         os.path.join(cache_dir, filename),
-        key = 'data'
+        key='data'
     )
