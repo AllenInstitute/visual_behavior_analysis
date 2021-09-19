ddb54d2b4120a9ccd45f08be766c2fbce32b9ca9
matchings <marinag@alleninstitute.org>
2021-09-19 12:10:26 -0700

    typo

--- scripts/create_cell_metrics_table.py
+++ scripts/create_cell_metrics_table.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     from allensdk.brain_observatory.behavior.behavior_project_cache import VisualBehaviorOphysProjectCache as bpc
     cache_dir = r'/allen/programs/braintv/workgroups/nc-ophys/visual_behavior/platform_paper_cache'
     cache = bpc.from_s3_cache(cache_dir=cache_dir)
-    ophys_experiments_table = cache.get_ophys_experiment_table()
+    ophys_experiment_table = cache.get_ophys_experiment_table()
 
     import platform
     if platform.system() == 'Linux':
