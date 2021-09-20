4632462968a0ac66690488d7de9d88c52d664c69
matchings <marinag@alleninstitute.org>
2021-09-19 18:26:56 -0700

    dont use events with pupil

--- scripts/create_multi_session_df.py
+++ scripts/create_multi_session_df.py
@@ -23,6 +23,14 @@ if __name__ == '__main__':
     conditions = ['cell_specimen_id', 'epoch']
 
     print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
+    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
+                                 use_extended_stimulus_presentations=True)
+
+
+    df_name = 'omission_pupil_area_df'
+    conditions = ['cell_specimen_id']
+
+    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
     df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, filter_events=True,
                                  use_extended_stimulus_presentations=True)
 
@@ -31,24 +39,17 @@ if __name__ == '__main__':
     conditions = ['cell_specimen_id', 'stimulus_change', 'epoch']
 
     print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, filter_events=True,
+    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
                                  use_extended_stimulus_presentations=True)
 
 
     df_name = 'trials_pupil_area_df'
-    conditions = ['cell_specimen_id', 'engagement_state', 'stimulus_change', 'change_image_name']
+    conditions = ['cell_specimen_id', 'stimulus_change', 'change_image_name']
 
     print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, filter_events=True,
+    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
                                  use_extended_stimulus_presentations=True)
 
-
-    df_name = 'omission_pupil_area_df'
-    conditions = ['cell_specimen_id', 'engagement_state']
-
-    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, filter_events=True,
-                                 use_extended_stimulus_presentations=True)
 
 print('done')
 
