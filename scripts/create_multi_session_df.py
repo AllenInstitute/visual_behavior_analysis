079b957840f482036ab56a6c3b3e01eacbf09550
matchings <marinag@alleninstitute.org>
2021-09-19 23:49:35 -0700

    run for trials

--- scripts/create_multi_session_df.py
+++ scripts/create_multi_session_df.py
@@ -18,39 +18,38 @@ if __name__ == '__main__':
     session_number = float(args.session_number)
     print(project_code, session_number)
 
-
-    df_name = 'omission_response_df'
-    conditions = ['cell_specimen_id', 'epoch']
-
-    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, filter_events=True,
-                                 use_extended_stimulus_presentations=True)
-
-
-    df_name = 'omission_response_df'
-    conditions = ['cell_specimen_id']
-
-    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, filter_events=True,
-                                 use_extended_stimulus_presentations=True)
-
     #
-    # df_name = 'trials_response_df'
-    # conditions = ['cell_specimen_id', 'stimulus_change', 'epoch']
+    # df_name = 'omission_response_df'
+    # conditions = ['cell_specimen_id', 'epoch']
     #
     # print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
     # df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, filter_events=True,
     #                              use_extended_stimulus_presentations=True)
     #
     #
-    # df_name = 'trials_response_df'
-    # conditions = ['cell_specimen_id', 'stimulus_change', 'change_image_name']
+    # df_name = 'omission_response_df'
+    # conditions = ['cell_specimen_id']
     #
     # print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
     # df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, filter_events=True,
     #                              use_extended_stimulus_presentations=True)
-    #
-    #
+
+
+    df_name = 'trials_response_df'
+    conditions = ['cell_specimen_id', 'stimulus_change', 'change_image_name']
+
+    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
+    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, filter_events=True,
+                                 use_extended_stimulus_presentations=True)
+
+
+    df_name = 'trials_response_df'
+    conditions = ['cell_specimen_id', 'stimulus_change', 'epoch']
+
+    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
+    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, filter_events=True,
+                                 use_extended_stimulus_presentations=True)
+
     # print('done')
     #
 
@@ -69,19 +68,19 @@ if __name__ == '__main__':
     # print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
     # df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
     #                              use_extended_stimulus_presentations=True)
-    #
-    # df_name = 'trials_pupil_area_df'
-    # conditions = ['ophys_experiment_id', 'stimulus_change', 'epoch']
-    #
-    # print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    # df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
-    #                              use_extended_stimulus_presentations=True)
-    #
-    # df_name = 'trials_pupil_area_df'
-    # conditions = ['ophys_experiment_id', 'stimulus_change', 'change_image_name']
-    #
-    # print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    # df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
-    #                              use_extended_stimulus_presentations=True)
-    #
-    # print('done')
+
+    df_name = 'trials_pupil_area_df'
+    conditions = ['ophys_experiment_id', 'stimulus_change', 'epoch']
+
+    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
+    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
+                                 use_extended_stimulus_presentations=True)
+
+    df_name = 'trials_pupil_area_df'
+    conditions = ['ophys_experiment_id', 'stimulus_change', 'change_image_name']
+
+    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
+    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
+                                 use_extended_stimulus_presentations=True)
+
+    print('done')
