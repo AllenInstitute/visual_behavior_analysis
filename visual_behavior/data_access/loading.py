9c2296a85452f9f241bf716333009d7bf0a944d5
matchings <marinag@alleninstitute.org>
2021-09-26 15:13:24 -0700

    use dff suffix

--- visual_behavior/data_access/loading.py
+++ visual_behavior/data_access/loading.py
@@ -2505,31 +2505,31 @@ def add_superficial_deep_to_experiments_table(experiments_table):
 
     return experiments_table
 
-
-def get_file_name_for_multi_session_df_no_session_type(df_name, project_code, conditions, use_events, filter_events):
-    if use_events:
-        if filter_events:
-            suffix = '_filtered_events'
-        else:
-            suffix = '_events'
-    else:
-        suffix = ''
-
-    if len(conditions) == 5:
-        filename = 'mean_' + df_name + '_' + project_code + '_' + conditions[1] + '_' + conditions[
-            2] + '_' + conditions[3] + '_' + conditions[4] + suffix + '.h5'
-    elif len(conditions) == 4:
-        filename = 'mean_' + df_name + '_' + project_code + '_' + conditions[1] + '_' + conditions[
-            2] + '_' + conditions[3] + suffix + '.h5'
-    elif len(conditions) == 3:
-        filename = 'mean_' + df_name + '_' + project_code + '_' + conditions[1] + '_' + conditions[
-            2] + suffix + '.h5'
-    elif len(conditions) == 2:
-        filename = 'mean_' + df_name + '_' + project_code + '_' + conditions[1] + suffix + '.h5'
-    elif len(conditions) == 1:
-        filename = 'mean_' + df_name + '_' + project_code + '_' + conditions[0] + suffix + '.h5'
-
-    return filename
+#
+# def get_file_name_for_multi_session_df_no_session_type(df_name, project_code, conditions, use_events, filter_events):
+#     if use_events:
+#         if filter_events:
+#             suffix = '_filtered_events'
+#         else:
+#             suffix = '_events'
+#     else:
+#         suffix = ''
+#
+#     if len(conditions) == 5:
+#         filename = 'mean_' + df_name + '_' + project_code + '_' + conditions[1] + '_' + conditions[
+#             2] + '_' + conditions[3] + '_' + conditions[4] + suffix + '.h5'
+#     elif len(conditions) == 4:
+#         filename = 'mean_' + df_name + '_' + project_code + '_' + conditions[1] + '_' + conditions[
+#             2] + '_' + conditions[3] + suffix + '.h5'
+#     elif len(conditions) == 3:
+#         filename = 'mean_' + df_name + '_' + project_code + '_' + conditions[1] + '_' + conditions[
+#             2] + suffix + '.h5'
+#     elif len(conditions) == 2:
+#         filename = 'mean_' + df_name + '_' + project_code + '_' + conditions[1] + suffix + '.h5'
+#     elif len(conditions) == 1:
+#         filename = 'mean_' + df_name + '_' + project_code + '_' + conditions[0] + suffix + '.h5'
+#
+#     return filename
 
 
 def get_file_name_for_multi_session_df(df_name, project_code, session_type, conditions, use_events, filter_events):
@@ -2539,7 +2539,7 @@ def get_file_name_for_multi_session_df(df_name, project_code, session_type, cond
         else:
             suffix = '_events'
     else:
-        suffix = ''
+        suffix = '_dff'
     if len(conditions) == 6:
         filename = 'mean_' + df_name + '_' + project_code + '_' + session_type + '_' + conditions[1] + '_' + conditions[2] + '_' + conditions[3] + '_' + conditions[4] + '_' + conditions[5] + suffix + '.h5'
     elif len(conditions) == 5:
