6987cc33c937c335f7661fd83f0fd2430f1bf570
matchings <marinag@alleninstitute.org>
2021-09-30 19:13:59 -0700

    pre-change response df

--- scripts/create_multi_session_df.py
+++ scripts/create_multi_session_df.py
@@ -21,42 +21,42 @@ if __name__ == '__main__':
 
     # omissions
 
-    df_name = 'omission_response_df'
-    conditions = ['cell_specimen_id']
-
-    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
-                                 use_extended_stimulus_presentations=False)
-
-    df_name = 'omission_response_df'
-    conditions = ['cell_specimen_id', 'epoch']
-
-    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
-                                 use_extended_stimulus_presentations=True)
+    # df_name = 'omission_response_df'
+    # conditions = ['cell_specimen_id']
+    #
+    # print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
+    # df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, filter_events=False,
+    #                              use_extended_stimulus_presentations=False)
+    #
+    # df_name = 'omission_response_df'
+    # conditions = ['cell_specimen_id', 'epoch']
+    #
+    # print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
+    # df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, filter_events=False,
+    #                              use_extended_stimulus_presentations=True)
 
     # trials
 
     df_name = 'trials_response_df'
-    conditions = ['cell_specimen_id', 'stimulus_change']
+    conditions = ['cell_specimen_id', 'pre_change']
 
     print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
-                                 use_extended_stimulus_presentations=False)
-
-    df_name = 'trials_response_df'
-    conditions = ['cell_specimen_id', 'stimulus_change', 'epoch']
-
-    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
+    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, filter_events=False,
                                  use_extended_stimulus_presentations=True)
 
     df_name = 'trials_response_df'
-    conditions = ['cell_specimen_id', 'stimulus_change', 'change_image_name']
+    conditions = ['cell_specimen_id', 'pre_change', 'epoch']
 
     print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
-                                 use_extended_stimulus_presentations=False)
+    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, filter_events=False,
+                                 use_extended_stimulus_presentations=True)
+
+    df_name = 'trials_response_df'
+    conditions = ['cell_specimen_id', 'stimulus_change', 'change_image_name', 'epoch']
+
+    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
+    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True, filter_events=False,
+                                 use_extended_stimulus_presentations=True)
 
     print('done')
 
