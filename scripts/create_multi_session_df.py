707af0d4ce91bf8d6f954702c147cfb6de1be292
matchings <marinag@alleninstitute.org>
2021-10-14 14:30:02 -0700

    pre change df

--- scripts/create_multi_session_df.py
+++ scripts/create_multi_session_df.py
@@ -92,14 +92,21 @@ if __name__ == '__main__':
     #                              use_extended_stimulus_presentations=True)
     #
     #
-    # df_name = 'stimulus_response_df'
-    # conditions = ['cell_specimen_id', 'is_change', 'epoch']
-    #
-    # print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    # df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True,
-    #                              filter_events=False,
-    #                              use_extended_stimulus_presentations=True)
+    df_name = 'stimulus_response_df'
+    conditions = ['cell_specimen_id', 'pre_change']
 
+    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
+    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True,
+                                 filter_events=False,
+                                 use_extended_stimulus_presentations=True)
+
+    df_name = 'stimulus_response_df'
+    conditions = ['cell_specimen_id', 'pre_change']
+
+    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
+    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=True,
+                                 filter_events=True,
+                                 use_extended_stimulus_presentations=True)
 
     #
     # # pupil ###
@@ -123,27 +130,27 @@ if __name__ == '__main__':
     #
     # # trials
 
-    df_name = 'trials_run_speed_df'
-    conditions = ['ophys_experiment_id', 'stimulus_change']
-
-    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False,
-                                 filter_events=False,
-                                 use_extended_stimulus_presentations=False)
-
-    df_name = 'trials_run_speed_df'
-    conditions = ['ophys_experiment_id', 'stimulus_change', 'epoch']
-
-    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
-                                 use_extended_stimulus_presentations=True)
-
-    df_name = 'trials_run_speed_df'
-    conditions = ['ophys_experiment_id', 'stimulus_change', 'change_image_name']
-
-    print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
-    df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
-                                 use_extended_stimulus_presentations=False)
+    # df_name = 'trials_run_speed_df'
+    # conditions = ['ophys_experiment_id', 'stimulus_change']
+    #
+    # print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
+    # df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False,
+    #                              filter_events=False,
+    #                              use_extended_stimulus_presentations=False)
+    #
+    # df_name = 'trials_run_speed_df'
+    # conditions = ['ophys_experiment_id', 'stimulus_change', 'epoch']
+    #
+    # print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
+    # df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
+    #                              use_extended_stimulus_presentations=True)
+    #
+    # df_name = 'trials_run_speed_df'
+    # conditions = ['ophys_experiment_id', 'stimulus_change', 'change_image_name']
+    #
+    # print('creating multi_session_df for', df_name, ', ', project_code, ', session number', session_number, conditions)
+    # df = io.get_multi_session_df(project_code, session_number, df_name, conditions, use_events=False, filter_events=False,
+    #                              use_extended_stimulus_presentations=False)
     #
     # print('done')
 
