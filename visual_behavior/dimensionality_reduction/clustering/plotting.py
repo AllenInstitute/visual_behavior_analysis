dea8e2a6d7fd92d4b23f20af60747cdebb19f325
iryna.yavorska <irynayavorska@gmail.com>
2021-10-19 12:16:29 -0700

    labels

--- visual_behavior/dimensionality_reduction/clustering/plotting.py
+++ visual_behavior/dimensionality_reduction/clustering/plotting.py
@@ -5,28 +5,58 @@ from sklearn.cluster import KMeans
 from scipy.spatial.distance import cdist, pdist
 from visual_behavior.dimensionality_reduction.clustering.processing import get_silhouette_scores
 
+# figure settings
+plt.rcParams['font.size']=16
+plt.rcParams['ytick.labelsize']=16
+plt.rcParams['xtick.labelsize']=16
 
 def plot_clustered_dropout_scores(df, labels=None, cmap='RdBu_r', model_output_type='', ax=None,):
     sort_order = np.argsort(labels)
     sorted_labels = labels[sort_order]
     if ax is None:
         fig, ax = plt.subplots(figsize=(8, 12))
-    ax = sns.heatmap(df.values[sort_order], cmap=cmap, ax=ax, vmin=-1, vmax=1,
-                     robust=True, cbar_kws={"drawedges": False, "shrink": 0.7, "label": model_output_type})
+    ax = sns.heatmap(df.abs().values[sort_order], cmap='Blues', ax=ax, vmin=0, vmax=1,
+                     robust=True,
+                     cbar_kws={"drawedges": False, "shrink": 0.8, "label": 'fraction change in explained variance'})
     ax.set_ylabel('cells')
     ax.set_xlabel('features')
     ax.set_title('sorted dropout scores')
 
     ax.set_ylim(0, df.shape[0])
     ax.set_xlim(0, df.shape[1])
-    ax.set_xticks(np.arange(0, df.shape[1]))
-    ax.set_xticklabels(df.keys(), rotation=90)
+    ax.set_xticks(np.arange(0.5, df.shape[1] + 0.5))
+    ax.set_xticklabels([key[1] + ' - ' + key[0] for key in list(df.keys())], rotation=90)
     ax.set_xticklabels(df.keys(), rotation=90)
 
+    ax.set_xlabel('')
+    ax2 = ax.twinx()
+    ax2.set_yticks([0, len(df)])
+    ax2.set_yticklabels([0, len(df)])
+
+    # plot horizontal lines to seperate the clusters
     cluster_divisions = np.where(np.diff(sorted_labels) == 1)[0]
     for y in cluster_divisions:
         ax.hlines(y, xmin=0, xmax=df.shape[1], color='k')
+
+
+    # set cluster labels
+    cluster_divisions = np.hstack([0, cluster_divisions, len(labels)])
+    mid_cluster_divisions = []
+    for i in range(0, len(cluster_divisions) - 1):
+        mid_cluster_divisions.append(((cluster_divisions[i + 1] - cluster_divisions[i]) / 2) + cluster_divisions[i])
+
+    # label cluster ids
+    unique_cluster_ids = np.sort(np.unique(labels))
+    ax.set_yticks(mid_cluster_divisions)
+    ax.set_yticklabels(unique_cluster_ids)
+    ax.set_ylabel('cluster ID')
+
+    #separate regressors
+    for x in np.arange(0, df.shape[0], 3):
+        ax.vlines(x, ymin=0, ymax=df.shape[0], color='gray', linestyle='--', linewidth=0.5)
+
     plt.tight_layout()
+    return fig, ax
 
 
 def get_elbow_plots(X, n_clusters=range(2, 20), ax=None):
@@ -69,16 +99,30 @@ def get_elbow_plots(X, n_clusters=range(2, 20), ax=None):
     ax[1].set_title('Elbow for KMeans clustering')
 
 
-def plot_silhouette_scores(X, model=KMeans, n_clusters=np.arange(2, 10), metric='euclidean', n_boots=20, ax=None,
+def plot_silhouette_scores(X = None, model=KMeans, silhouette_scores = None, n_clusters=np.arange(2, 10), metric=None, n_boots=20, ax=None,
                          model_output_type=''):
-    silhouette_scores = get_silhouette_scores(X=X, model=model, n_clusters=n_clusters, metric=metric, n_boots=n_boots)
+    assert X is not None or silhouette_scores is not None, 'must provide either data to cluster or recomputed scores'
+    assert X is None or silhouette_scores is None, 'cannot provide data to cluster and silhouette scores'
+
+    if silhouette_scores is None:
+        if metric is None:
+            metric = 'euclidean'
+        silhouette_scores = get_silhouette_scores(X=X, model=model, n_clusters=n_clusters, metric=metric, n_boots=n_boots)
+    elif silhouette_scores is not None:
+        if len(silhouette_scores) != len(n_clusters):
+            n_clusters = np.arange(0,len(silhouette_scores))
+
+        if metric is None:
+            metric = ''
 
     if ax is None:
         fig, ax = plt.subplots(1, 1, figsize=(7, 7))
 
-    ax.plot(n_clusters, silhouette_scores)
-    ax.set_xlabel('N Clusters')
+    ax.plot(n_clusters, silhouette_scores, 'ko-')
+    ax.set_title('{}, {}'.format(model_output_type, metric), fontsize = 16)
+    ax.set_xlabel('Number of clusters')
     ax.set_ylabel('Silhouette score')
-    plt.title(model_output_type)
+    plt.grid()
+    plt.tight_layout()
 
-    return fig,ax,
+    return fig,ax
