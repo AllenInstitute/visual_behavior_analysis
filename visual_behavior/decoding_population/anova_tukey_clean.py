83c2f995e10a4982853327a45a1b768c008c106b
farznaj <farznaj@gmail.com>
2021-11-16 10:52:07 -0800

    anova tukey code turned to a clean function

--- visual_behavior/decoding_population/anova_tukey_clean.py
+++ visual_behavior/decoding_population/anova_tukey_clean.py
@@ -1,3 +1,7 @@
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+from statsmodels.stats.multicomp import (pairwise_tukeyhsd, MultiComparison)
+
 
 anova_all = [] # each index is for 1 cre line, and shows the results of Anova (1 way) across experience levels.
 tukey_all = [] # each index is for 1 cre line, and shows the results of pairwise tukey test for experience levels.
@@ -8,41 +12,41 @@ for cre in cres:
     
     thiscre = svm_df[svm_df['cre_allPlanes']==cre]
     thiscre = thiscre.rename(columns={'cre_allPlanes': 'cre'})
-    thiscre = thiscre.rename(columns={'peak_amp_allPlanes_allExp':'decoding_magnitude_train_test_shfl_chance'})
+    thiscre = thiscre.rename(columns={'peak_amp_allPlanes_allExp':'decoding_magnitude'})
 
     # take testing dataset decoding accuracy
-    test = np.array([thiscre['decoding_magnitude_train_test_shfl_chance'].values[i][1] for i in range(thiscre.shape[0])])
-    thiscre['decoding_magnitude_train_test_shfl_chance'] = list(test)
+    test = np.array([thiscre['decoding_magnitude'].values[i][1] for i in range(thiscre.shape[0])])
+    thiscre['decoding_magnitude'] = list(test)
 
     # only take the relevant columns
-    thiscre = thiscre[['cre', 'experience_levels', 'decoding_magnitude_train_test_shfl_chance']]
+    thiscre = thiscre[['cre', 'experience_levels', 'decoding_magnitude']]
     
     print(thiscre.shape)
     
         
-    ############ create dataframe "c", which is suitable for running anova ############
-
+    ############ create dataframe "c", which is suitable for doing anova ############
+    
     # rename the column that is used for doing anova to "value"
-    c = thiscre.rename(columns={'decoding_magnitude_train_test_shfl_chance': 'value'})
+    stats_df = thiscre.rename(columns={'decoding_magnitude': 'value'})
 
     # only take valid values
-    c = c[~np.isnan(c['value'])]
-    print(c.shape)
+    stats_df = stats_df[~np.isnan(stats_df['value'])]
+    print(stats_df.shape)
 
     # replace Familiar, Novel 1, and Novel >1 in the df with 0, 1, and 2
     cnt = -1
     dfall = pd.DataFrame()
     for expl in exp_level_all:
         cnt = cnt+1
-        dfnow = c[c['experience_levels']==expl]
+        dfnow = stats_df[stats_df['experience_levels']==expl]
         dfnow['experience_levels'] = [cnt for x in dfnow['experience_levels']]
         dfall = pd.concat([dfall, dfnow])
-    c = dfall
-#     c
+    stats_df = dfall
+#     stats_df
 
-
-    ############ ANOVA, 1-way ############
-    model = ols('value ~ C(experience_levels)', data=c).fit() 
+    
+    ############ ANOVA, 1-way : compare stats_df['value'] across stats_df['experience_levels]' ############
+    model = ols('value ~ C(experience_levels)', data=stats_df).fit() 
     anova_table = sm.stats.anova_lm(model, typ=2)
     print(anova_table)
     print('\n')
@@ -51,8 +55,8 @@ for cre in cres:
 
 
     ############ TUKEY HSD ############        
-    v = c['value']
-    f = c['experience_levels']
+    v = stats_df['value']
+    f = stats_df['experience_levels']
 
     MultiComp = MultiComparison(v, f)
     tukey_table = MultiComp.tukeyhsd().summary()    
@@ -60,5 +64,6 @@ for cre in cres:
     print(tukey_table) # Show all pair-wise comparisons
 
     tukey_all.append(tukey_table) # cres x 2 (test-shfl ; test) x tukey_table (ie 4 x7)
+
 
 
