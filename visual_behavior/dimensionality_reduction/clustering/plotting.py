41b9ca67c3e5b42d472ecdd2343c0aebfcc26546
matchings <marinag@alleninstitute.org>
2021-11-16 21:19:29 -0800

    functions to generate additional cluster plots

--- visual_behavior/dimensionality_reduction/clustering/plotting.py
+++ visual_behavior/dimensionality_reduction/clustering/plotting.py
@@ -1,20 +1,19 @@
+import os
+import umap
+import random
 import numpy as np
-# import pandas as pd
+import pandas as pd
+from scipy import signal
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.cluster import KMeans
 from scipy.spatial.distance import cdist, pdist
-from visual_behavior.dimensionality_reduction.clustering.processing import get_silhouette_scores
-import umap
-# import os
-from scipy import signal
 import visual_behavior.visualization.utils as utils
-import random
+import visual_behavior.data_access.loading as loading
+from visual_behavior.dimensionality_reduction.clustering.processing import get_silhouette_scores
 
+import seaborn as sns
 # figure settings
-plt.rcParams['font.size'] = 16
-plt.rcParams['ytick.labelsize'] = 16
-plt.rcParams['xtick.labelsize'] = 16
+sns.set_context('notebook', font_scale=1.5, rc={'lines.markeredgewidth': 2})
 
 
 def plot_clustered_dropout_scores(df, labels=None, cmap='RdBu_r', model_output_type='', ax=None,):
@@ -214,7 +213,7 @@ def plot_clusters(dropout_df, cluster_df=None, mean_response_df=None, save_plots
         # ax[i+ len(cluster_ids)].set_title('n mice = ' + str(N_mice.loc[cluster_id].values), fontsize=16)
 
         # set title and labels
-        ax[i].set_title('cluster ' + str(i) + '\n' + str(fraction[0][0]) + '%, n=' + str(n_cells),  fontsize=16)
+        ax[i].set_title('cluster ' + str(i) + '\n' + str(fraction[0][0]) + '%, n=' + str(n_cells), fontsize=16)
         ax[i].set_yticklabels(mean_dropout_df.index.values, rotation=0)
         ax[i].set_ylim(-0.5, 4.5)
         ax[i].set_xlabel('')
@@ -296,6 +295,244 @@ def plot_clusters(dropout_df, cluster_df=None, mean_response_df=None, save_plots
             ax[i + (len(cluster_ids) * 4)].set_ylabel('')
     return fig
     plt.tight_layout()
+
+
+def plot_clusters_columns_all_cre_lines(df, df_meta, labels_cre, multi_session_df, save_dir=None):
+    """
+    plots dropout scores per cluster, per cre line, in a compact format
+    df: reshaped dataframe of dropout scores for matched cells
+    df_meta: metadata for each cell in df
+    labels_cre: dict of cluster labels for each cre line
+    multi_session_df: dataframe with trial averaged traces to plot per cluster
+    """
+    cells_table = loading.get_cell_table()
+    cre_lines = np.sort(df_meta.cre_line.unique())[::-1]
+
+    for cre_line in cre_lines:
+        cell_type = df_meta[df_meta.cre_line == cre_line].cell_type.unique()[0]
+
+        print(cre_line, cell_type)
+        cids = df_meta[df_meta['cre_line'] == cre_line]['cell_specimen_id']
+        df_sel = df.loc[cids]
+        labels = labels_cre[cre_line]
+        cluster_df = pd.DataFrame(index=df_sel.index, columns=['cluster_id'], data=labels)
+        cluster_df = cluster_df.merge(cells_table[['cell_specimen_id', 'cell_type', 'cre_line', 'experience_level',
+                                                   'binned_depth', 'targeted_structure']], on='cell_specimen_id')
+        cluster_df = cluster_df.drop_duplicates(subset='cell_specimen_id')
+        cluster_df.reset_index(inplace=True)
+
+        dropout_df = df_sel.copy()
+
+        cluster_mdf = multi_session_df.merge(cluster_df[['cell_specimen_id', 'cluster_id']],
+                                             on='cell_specimen_id',
+                                             how='inner')
+
+        # Set up the variables
+
+        # cluster_ids = cluster_df['cluster_id'].value_counts().index.values  # sort cluster ids by size
+        cluster_ids = cluster_df.groupby(['cluster_id']).count()[['cell_specimen_id']].sort_values(by='cell_specimen_id').index.values
+        n_clusters = len(cluster_ids)
+        palette = utils.get_cre_line_colors()
+        palette_exp = utils.get_experience_level_colors()
+        depths = [75, 175, 275, 375]
+        areas = ['VISp', 'VISl']
+
+        # get number of animals per cluster
+        grouped_df = cluster_df.groupby('cluster_id')
+        # N_mice = grouped_df.agg({"mouse_id": "nunique"})
+
+        # Set up figure
+        n_cols = 4
+        figsize = (12, n_clusters * 2,)
+        fig, ax = plt.subplots(n_clusters, n_cols, figsize=figsize, sharex='col', gridspec_kw={'width_ratios': [2, 3, 2, 1.25]})
+        ax = ax.ravel()
+        for i, cluster_id in enumerate(cluster_ids[::-1]):
+
+            # % of total cells in this cluster
+            within_cluster_df = cluster_df[cluster_df['cluster_id'] == cluster_id].drop_duplicates('cell_specimen_id').groupby(
+                'cell_type').count()[['cluster_id']]
+            all_df = cluster_df.drop_duplicates('cell_specimen_id').groupby('cell_type').count()[['cluster_id']]
+            n_cells = within_cluster_df.cluster_id.values[0]
+            fraction = within_cluster_df / all_df
+            fraction = np.round(fraction.cluster_id.values[0] * 100, 1)
+
+            # 1. Mean dropout scores
+            this_cluster_ids = cluster_df[cluster_df['cluster_id'] == cluster_id]['cell_specimen_id'].unique()
+            mean_dropout_df = dropout_df.loc[this_cluster_ids].mean().unstack()
+            ax[(i * n_cols)] = sns.heatmap(mean_dropout_df,
+                                           cmap='RdBu',
+                                           vmin=-1,
+                                           vmax=1,
+                                           ax=ax[(i * n_cols)],
+                                           cbar=False, )
+
+            # set title and labels
+        #     ax[(i*n_cols)].set_title('cluster ' + str(i) + '\n' + str(fraction[0][0]) + '%, n=' + str(n_cells),  fontsize=14)
+            ax[(i * n_cols)].set_title(str(fraction) + '%, n=' + str(n_cells), fontsize=14)
+            ax[(i * n_cols)].set_yticks(np.arange(0.5, 4.5))
+            ax[(i * n_cols)].set_yticklabels(mean_dropout_df.index.values, rotation=0, fontsize=15)
+            ax[(i * n_cols)].set_ylim(0, 4)
+            ax[(i * n_cols)].set_xlabel('')
+            ax[(i * n_cols)].set_ylabel('cluster ' + str(i), fontsize=15)
+
+            # 3. Plot by depth
+            n_col = 2
+            within_cluster_df = \
+                cluster_df[cluster_df['cluster_id'] == cluster_id].drop_duplicates('cell_specimen_id').groupby(
+                    'binned_depth').count()[['cluster_id']]
+            all_df = cluster_df.drop_duplicates('cell_specimen_id').groupby('binned_depth').count()[['cluster_id']]
+            fraction_depth = within_cluster_df / all_df
+            fraction_depth.reset_index(inplace=True)
+            ax[(i * n_cols) + n_col] = sns.barplot(data=fraction_depth, orient='h',
+                                                   y='binned_depth',
+                                                   x='cluster_id',
+                                                   # orient='h',
+                                                   palette='gray',
+                                                   ax=ax[(i * n_cols) + n_col])
+            # set labels
+            ax[(i * n_cols) + n_col].set_ylabel('depth (um)', fontsize=15)
+            ax[(i * n_cols) + n_col].set_xlabel('')
+            ax[(i * n_cols) + n_col].set_ylim(-0.5, len(depths) - 0.5)
+
+            ax[(i * n_cols) + n_col].set_yticks(np.arange(0, len(depths)))
+            ax[(i * n_cols) + n_col].set_yticklabels(depths, rotation=0)
+            ax[(i * n_cols) + n_col].invert_yaxis()
+
+            # 4. Plot by area
+            n_col = 3
+            within_cluster_df = \
+                cluster_df[cluster_df['cluster_id'] == cluster_id].drop_duplicates('cell_specimen_id').groupby(
+                    'targeted_structure').count()[['cluster_id']]
+            all_df = cluster_df.drop_duplicates('cell_specimen_id').groupby('targeted_structure').count()[['cluster_id']]
+            fraction_area = within_cluster_df / all_df
+            fraction_area.reset_index(inplace=True, drop=False)
+
+            ax[(i * n_cols) + n_col] = sns.barplot(data=fraction_area,
+                                                   # orient='h',
+                                                   x='targeted_structure',
+                                                   y='cluster_id',
+                                                   palette='gray',
+                                                   ax=ax[(i * n_cols) + n_col])
+            # set labels
+            ax[(i * n_cols) + n_col].set_xlabel('')
+            ax[(i * n_cols) + n_col].set_ylabel('fraction cells\nper area', fontsize=15)
+            ax[(i * n_cols) + n_col].set_xticklabels(areas, rotation=0)
+
+            # plot mean traces
+
+            n_col = 1
+            # axes_column = 'cluster_id'
+            hue_column = 'experience_level'
+            hue_conditions = np.sort(cluster_mdf[hue_column].unique())
+            timestamps = cluster_mdf['trace_timestamps'][0]
+            xlim_seconds = [-1, 1.5]
+            change = False
+            omitted = True
+            xlabel = 'time (sec)'
+
+            for c, hue in enumerate(hue_conditions):
+
+                traces = cluster_mdf[(cluster_mdf['cluster_id'] == cluster_id) &
+                                     (cluster_mdf[hue_column] == hue)].mean_trace.values
+                for t in range(0, np.shape(traces)[0]):
+                    traces[t] = signal.resample(traces[t], len(timestamps))
+                ax[(i * n_cols) + n_col] = utils.plot_mean_trace(np.asarray(traces),
+                                                                 timestamps, ylabel='response',
+                                                                 legend_label=hue,
+                                                                 color=palette_exp[c],
+                                                                 interval_sec=1,
+                                                                 plot_sem=False,
+                                                                 xlim_seconds=xlim_seconds,
+                                                                 ax=ax[(i * n_cols) + n_col])
+                ax[(i * n_cols) + n_col] = utils.plot_flashes_on_trace(ax[(i * n_cols) + n_col], timestamps,
+                                                                       change=change, omitted=omitted)
+                ax[(i * n_cols) + n_col].axvline(x=0, ymin=0, ymax=1, linestyle='--', color='gray')
+                ax[(i * n_cols) + n_col].set_title('')
+                ax[(i * n_cols) + n_col].set_xlim(xlim_seconds)
+                ax[(i * n_cols) + n_col].set_xlabel('')
+                ax[(i * n_cols) + n_col].set_ylabel('response', fontsize=15)
+
+        ax[(i * n_cols) + 2].set_xlabel('fraction cells\nper depth')
+        ax[(i * n_cols) + 3].set_xlabel('area')
+        ax[(i * n_cols) + 1].set_xlabel('time (sec)')
+
+        plt.suptitle(cell_type, x=0.54, y=1.01)
+        plt.tight_layout()
+        if save_dir:
+            utils.save_figure(fig, figsize, save_dir, 'clustering', cre_line + '_clusters_column')
+
+
+def plot_clusters_compact_all_cre_lines(df, df_meta, labels_cre, save_dir=None):
+    """
+    plots dropout scores per cluster, per cre line, in a compact format
+    df: reshaped dataframe of dropout scores for matched cells
+    df_meta: metadata for each cell in df
+    labels_cre: dict of cluster labels for each cre line
+    """
+    cells_table = loading.get_cell_table()
+    cre_lines = np.sort(df_meta.cre_line.unique())[::-1]
+
+    for cre_line in cre_lines:
+        cell_type = df_meta[df_meta.cre_line == cre_line].cell_type.unique()[0]
+
+        print(cre_line, cell_type)
+        cids = df_meta[df_meta['cre_line'] == cre_line]['cell_specimen_id']
+        df_sel = df.loc[cids]
+        labels = labels_cre[cre_line]
+        cluster_df = pd.DataFrame(index=df_sel.index, columns=['cluster_id'], data=labels)
+        cluster_df = cluster_df.merge(cells_table[['cell_specimen_id', 'cell_type', 'cre_line', 'experience_level',
+                                                   'binned_depth', 'targeted_structure']], on='cell_specimen_id')
+        cluster_df = cluster_df.drop_duplicates(subset='cell_specimen_id')
+        cluster_df.reset_index(inplace=True)
+
+        dropout_df = df_sel.copy()
+
+        # Set up the variables
+
+        # cluster_ids = cluster_df['cluster_id'].value_counts().index.values  # sort cluster ids by size
+        cluster_ids = cluster_df.groupby(['cluster_id']).count()[['cell_specimen_id']].sort_values(by='cell_specimen_id').index.values
+        n_clusters = len(cluster_ids)
+
+        # get number of animals per cluster
+        grouped_df = cluster_df.groupby('cluster_id')
+        # N_mice = grouped_df.agg({"mouse_id": "nunique"})
+
+        # Set up figure
+        n_rows = int(np.ceil(n_clusters / 3.))
+        figsize = (5, 1.9 * n_rows)
+        fig, ax = plt.subplots(n_rows, 3, figsize=figsize, sharex=True, sharey=True)
+        ax = ax.ravel()
+        for i, cluster_id in enumerate(cluster_ids[::-1]):
+
+            # % of total cells in this cluster
+            within_cluster_df = cluster_df[cluster_df['cluster_id'] == cluster_id].drop_duplicates('cell_specimen_id').groupby(
+                'cell_type').count()[['cluster_id']]
+            all_df = cluster_df.drop_duplicates('cell_specimen_id').groupby('cell_type').count()[['cluster_id']]
+            n_cells = within_cluster_df.cluster_id.values[0]
+            fraction = within_cluster_df / all_df
+            fraction = np.round(fraction.cluster_id.values[0] * 100, 1)
+
+            # 1. Mean dropout scores
+            this_cluster_ids = cluster_df[cluster_df['cluster_id'] == cluster_id]['cell_specimen_id'].unique()
+            mean_dropout_df = dropout_df.loc[this_cluster_ids].mean().unstack()
+            ax[i] = sns.heatmap(mean_dropout_df,
+                                cmap='RdBu',
+                                vmin=-1,
+                                vmax=1,
+                                ax=ax[i],
+                                cbar=False, )
+
+            # set title and labels
+            ax[i].set_title('cluster ' + str(i) + '\n' + str(fraction) + '%, n=' + str(n_cells), fontsize=14)
+            ax[i].set_yticks(np.arange(0.5, 4.5))
+            ax[i].set_yticklabels(mean_dropout_df.index.values, rotation=0, fontsize=15)
+            ax[i].set_ylim(0, 4)
+            ax[i].set_xlabel('')
+
+        plt.suptitle(cell_type, x=0.5, y=1.01)
+        plt.subplots_adjust(wspace=0.4, hspace=0.8)
+        if save_dir:
+            utils.save_figure(fig, figsize, save_dir, 'clustering', cre_line + '_clusters_compact')
 
 
 def get_cluster_colors(labels):
