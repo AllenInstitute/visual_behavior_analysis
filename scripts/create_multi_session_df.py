9162cb104b7dbbb4a3312ea68ba7379761bb033f
matchings <marinag@alleninstitute.org>
2021-12-01 10:36:53 -0800

    events mdf engagement

--- scripts/create_multi_session_df.py
+++ scripts/create_multi_session_df.py
@@ -30,64 +30,67 @@ if __name__ == '__main__':
     physio_data_types = ['filtered_events', 'events', 'dff']
     behavior_data_types = ['pupil_width', 'running_speed', 'lick_rate']
 
-    physio_conditions = [['cell_specimen_id', 'omitted'],
-                         ['cell_specimen_id', 'omitted', 'epoch'],
-                         ['cell_specimen_id', 'is_change'],
+    physio_conditions = [['cell_specimen_id', 'is_change'],
                          ['cell_specimen_id', 'is_change', 'epoch'],
+                         ['cell_specimen_id', 'omitted'],
+                         ['cell_specimen_id', 'omitted', 'epoch'],
                          ['cell_specimen_id', 'is_change', 'image_name'],
                          ['cell_specimen_id', 'is_change', 'image_name', 'epoch'],
                          ['cell_specimen_id', 'is_change', 'hit'],
                          ['cell_specimen_id', 'pre_change', 'epoch'],
                          ['cell_specimen_id', 'is_change', 'hit', 'epoch']]
 
-    behavior_conditions = [['ophys_experiment_id', 'omitted'],
-                           ['ophys_experiment_id', 'omitted', 'epoch'],
-                           ['ophys_experiment_id', 'is_change'],
-                           ['ophys_experiment_id', 'is_change', 'epoch'],
-                           ['ophys_experiment_id', 'is_change', 'image_name'],
-                           ['ophys_experiment_id', 'is_change', 'image_name', 'epoch'],
-                           ['ophys_experiment_id', 'is_change', 'hit'],
-                           ['ophys_experiment_id', 'is_change', 'pre_change', 'epoch'],
-                           ['ophys_experiment_id', 'is_change', 'hit', 'epoch']]
+    behavior_conditions = [['ophys_experiment_id', 'is_change'],
+                            ['ophys_experiment_id', 'is_change', 'epoch'],
+                            ['ophys_experiment_id', 'omitted'],
+                            ['ophys_experiment_id', 'omitted', 'epoch'],
+                            ['ophys_experiment_id', 'is_change', 'image_name'],
+                            ['ophys_experiment_id', 'is_change', 'image_name', 'epoch'],
+                            ['ophys_experiment_id', 'is_change', 'hit'],
+                            ['ophys_experiment_id', 'is_change', 'pre_change', 'epoch'],
+                            ['ophys_experiment_id', 'is_change', 'hit', 'epoch']]
 
 
     # event types corresponding to the above physio and behavior conditions - must be in same sequential order
-    event_types_for_conditions = ['omissions', 'omissions',
-                                    'changes', 'changes', 'changes',
-                                    'changes', 'changes',
-                                    'all', 'all']
+    event_types_for_conditions = ['changes', 'changes',
+                                  'omissions', 'omissions',
+                                  'changes', 'changes', 'changes',
+                                  'all', 'all']
 
+    # add engagement state to all conditions
+    for i in range(len(conditions)):
+        conditions[i].insert(1, 'engagement_state')
 
     # create dfs for all data types and conditions for physio data
-    # # for data_type in physio_data_types:
-    # data_type = 'filtered_events'
-    # for i, conditions in enumerate(physio_conditions):
+    # for data_type in physio_data_types:
+    data_type = 'events'
+    for i, conditions in enumerate(physio_conditions):
+        event_type = event_types_for_conditions[i]
+        print('creating multi_session_df for', data_type, event_type, conditions)
+        try: # use try except so that it skips over any conditions that fail to generate for some reason
+            df = io.get_multi_session_df(project_code, session_number, conditions, data_type, event_type,
+                                         time_window=time_window, interpolate=interpolate, output_sampling_rate=output_sampling_rate,
+                                         response_window_duration_seconds=response_window_duration_seconds,
+                                         use_extended_stimulus_presentations=use_extended_stimulus_presentations,
+                                         overwrite=False)
+        except Exception as e:
+            print('failed to create multi_session_df for', data_type, event_type, conditions)
+            print(e)
+
+
+    # create dfs for all data types and conditions for behavior data
+    # for data_type in behavior_data_types:
+    # data_type = 'running_speed'
+    # for i, conditions in enumerate(behavior_conditions):
     #     event_type = event_types_for_conditions[i]
     #     print('creating multi_session_df for', data_type, event_type, conditions)
     #     try: # use try except so that it skips over any conditions that fail to generate for some reason
     #         df = io.get_multi_session_df(project_code, session_number, conditions, data_type, event_type,
-    #                                      time_window=time_window, interpolate=interpolate, output_sampling_rate=output_sampling_rate,
+    #                                      time_window=time_window, interpolate=interpolate,
+    #                                      output_sampling_rate=output_sampling_rate,
     #                                      response_window_duration_seconds=response_window_duration_seconds,
     #                                      use_extended_stimulus_presentations=use_extended_stimulus_presentations,
     #                                      overwrite=True)
     #     except Exception as e:
     #         print('failed to create multi_session_df for', data_type, event_type, conditions)
     #         print(e)
-
-
-    # create dfs for all data types and conditions for behavior data
-    for data_type in behavior_data_types:
-    data_type = 'running_speed'
-    for i, conditions in enumerate(behavior_conditions):
-        event_type = event_types_for_conditions[i]
-        print('creating multi_session_df for', data_type, event_type, conditions)
-        try: # use try except so that it skips over any conditions that fail to generate for some reason
-            df = io.get_multi_session_df(project_code, session_number, conditions, data_type, event_type,
-                                         time_window=time_window, interpolate=interpolate,
-                                         output_sampling_rate=output_sampling_rate,
-                                         response_window_duration_seconds=response_window_duration_seconds,
-                                         use_extended_stimulus_presentations=use_extended_stimulus_presentations,
-                                         overwrite=True)
-        except Exception as e:
-            print('failed to create multi_session_df for', data_type, event_type, conditions)
-            print(e)
