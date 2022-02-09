df3ae97d8464b204759a9c3b290fd50f1567b303
matchings <marinag@alleninstitute.org>
2022-02-08 19:11:01 -0800

    create multi session dfs

--- scripts/create_multi_session_df.py
+++ scripts/create_multi_session_df.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     interpolate = True
     output_sampling_rate = 30
     # response_window_duration_seconds = 0.5
-    use_extended_stimulus_presentations = True
+    use_extended_stimulus_presentations = False
 
     # set up conditions to make multi session dfs for
     physio_data_types = ['filtered_events', 'events', 'dff']
@@ -65,25 +65,25 @@ if __name__ == '__main__':
     #     behavior_conditions[i].insert(1, 'engagement_state')
 
     # create dfs for all data types and conditions for physio data
-    # for data_type in physio_data_types:
-    data_type = 'events'
-    for i, conditions in enumerate(physio_conditions[-1:]):
-        print(conditions)
-        event_type = event_types_for_conditions[i]
-        if event_type == 'omissions':
-            response_window_duration = 0.75
-        else:
-            response_window_duration = 0.5
-        print('creating multi_session_df for', data_type, event_type, conditions)
-        try: # use try except so that it skips over any conditions that fail to generate for some reason
-            df = io.get_multi_session_df(project_code, session_number, conditions, data_type, event_type,
-                                         time_window=time_window, interpolate=interpolate, output_sampling_rate=output_sampling_rate,
-                                         response_window_duration=response_window_duration,
-                                         use_extended_stimulus_presentations=use_extended_stimulus_presentations,
-                                         overwrite=True)
-        except Exception as e:
-            print('failed to create multi_session_df for', data_type, event_type, conditions)
-            print(e)
+    for data_type in physio_data_types:
+    # data_type = 'events'
+        for i, conditions in enumerate(physio_conditions[-1:]):
+            print(conditions)
+            event_type = event_types_for_conditions[i]
+            if event_type == 'omissions':
+                response_window_duration = 0.75
+            else:
+                response_window_duration = 0.5
+            print('creating multi_session_df for', data_type, event_type, conditions)
+            try: # use try except so that it skips over any conditions that fail to generate for some reason
+                df = io.get_multi_session_df(project_code, session_number, conditions, data_type, event_type,
+                                             time_window=time_window, interpolate=interpolate, output_sampling_rate=output_sampling_rate,
+                                             response_window_duration=response_window_duration,
+                                             use_extended_stimulus_presentations=use_extended_stimulus_presentations,
+                                             overwrite=False)
+            except Exception as e:
+                print('failed to create multi_session_df for', data_type, event_type, conditions)
+                print(e)
 
 
     # create dfs for all data types and conditions for behavior data
