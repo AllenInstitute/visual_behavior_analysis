e952bc43cf44d985110313be21543bbe0c164e6a
matchings <marinag@alleninstitute.org>
2022-04-20 17:23:14 -0700

    remove commented code

--- visual_behavior/dimensionality_reduction/clustering/figures.py
+++ visual_behavior/dimensionality_reduction/clustering/figures.py
@@ -13,10 +13,10 @@ sns.set_context('notebook', font_scale=1.5, rc={'lines.markeredgewidth': 2})
 
 # set critical params for processing and saving
 # folder = '220412_across_session_norm_signed_weights'
-folder = '220414_var_exp_full'
-across_session_norm = False
+folder = '220414_across_session_norm'
+across_session_norm = True
 use_signed_weights = False
-include_var_exp_full = True
+include_var_exp_full = False
 
 # number of clusters to use
 n_clusters_cre = {'Slc17a7-IRES2-Cre': 10,
@@ -58,88 +58,8 @@ save_dir = os.path.join(base_dir, folder)
 if not os.path.exists(save_dir):
     os.mkdir(save_dir)
 
-
 results_pivoted, weights_df, kernels = processing.generate_GLM_outputs(glm_version, experiments_table, cells_table, save_dir,
                                                                        use_signed_weights, across_session_norm, include_var_exp_full)
-
-# # get features
-# features = processing.get_features_for_clustering()
-#
-# if across_session_norm:
-#
-#     import visual_behavior_glm.GLM_across_session as gas
-#
-#     # get across session normalized dropout scores
-#     df, failed_cells = gas.load_cells(cells='all')
-#     df = df.set_index('identifier')
-#
-#     # only use across session values
-#     within = df[[key for key in df.keys() if '_within' in key] + ['cell_specimen_id', 'ophys_experiment_id',
-#                                                                   'experience_level']]
-#     across = df[[key for key in df.keys() if '_across' in key] + ['cell_specimen_id', 'ophys_experiment_id',
-#                                                                   'experience_level']]
-#     results_pivoted = across.copy()
-#     results_pivoted = results_pivoted.rename(
-#         columns={'omissions_across': 'omissions', 'all-images_across': 'all-images',
-#                  'behavioral_across': 'behavioral', 'task_across': 'task'})
-#     print(len(results_pivoted), 'len(results_pivoted)')
-#
-#     # limit results pivoted to to last familiar and second novel active
-#     results_pivoted = results_pivoted[results_pivoted.ophys_experiment_id.isin(matched_experiments)]
-#     results_pivoted = results_pivoted[results_pivoted.cell_specimen_id.isin(matched_cells)]
-#     print(len(results_pivoted.cell_specimen_id.unique()),
-#           'cells in results_pivoted after limiting to strictly matched cells')
-#
-#     # drop duplicates
-#     results_pivoted = results_pivoted.drop_duplicates(subset=['cell_specimen_id', 'experience_level'])
-#     print(len(results_pivoted), 'len(results_pivoted) after dropping duplicates')
-#
-# else:
-#     # get GLM results from saved file in save_dir or from mongo if file doesnt exist
-#     results_pivoted = processing.get_glm_results_pivoted_for_clustering(glm_version, model_output_type, save_dir)
-#
-# if use_signed_weights:
-#     import visual_behavior_glm.GLM_analysis_tools as gat
-#     import visual_behavior_glm.GLM_params as glm_params
-#
-#     # get weights
-#     run_params = glm_params.load_run_json(glm_version)
-#     weights_df = gat.build_weights_df(run_params, results_pivoted)
-#
-#     # modify regressors to include kernel weights as regressors with '_signed' appended
-#     results_pivoted = gat.append_kernel_excitation(weights_df, results_pivoted)
-#     # add behavioral results signed by duplicating behavioral dropouts
-#     results_pivoted['behavioral_signed'] = results_pivoted.behavioral.values
-#
-#     # limit to features of interest, in this case limit to the 4 feature categories with '_signed' in name
-#     results_pivoted = processing.limit_results_pivoted_to_features_for_clustering(results_pivoted,
-#                                                                                   take_absolute_value=False,
-#                                                                                   use_signed_features=True)
-#
-#     # now remove 'signed' from column names
-#     columns = {}
-#     for column in results_pivoted.columns:
-#         if 'signed' in column:
-#             # invert the sign so that "increased coding" is positive
-#             results_pivoted[column] = results_pivoted[column]*-1
-#             # get new column name without the '_signed'
-#             columns[column] = column.split('_')[0]
-#     results_pivoted = results_pivoted.rename(columns=columns)
-#
-#
-#     # drop duplicates again just in case
-#     results_pivoted = results_pivoted.reset_index().drop_duplicates(subset=['cell_specimen_id', 'experience_level'])
-#
-# # save results_pivoted
-# results_pivoted.to_hdf(os.path.join(save_dir, glm_version + '_results_pivoted.h5'), key='df')
-#
-# # get dropout scores just for the features we want to cluster on and take absolute value if not using signed weights
-# if use_signed_weights:
-#     take_absolute_value = False
-# else:
-#     take_absolute_value = True
-# dropouts = processing.limit_results_pivoted_to_features_for_clustering(results_pivoted, take_absolute_value)
-# dropouts.head()
 
 
 # unstack dropout scores to get a vector of features x experience levels for each cell
@@ -188,7 +108,7 @@ cluster_meta = processing.get_cluster_meta(cluster_labels, cell_metadata, featur
 cre_lines = processing.get_cre_lines(cell_metadata)
 for cre_line in cre_lines:
     plotting.plot_coclustering_matrix_sorted_by_cluster_size(coclustering_matrices, cluster_meta, cre_line,
-                                                             save_dir=None, folder=folder, ax=None)
+                                                             save_dir=base_dir, folder=folder, ax=None)
 
 # plot average dropout scores for each cluster
 
@@ -204,17 +124,17 @@ sort_col = 'cluster_id'
 plotting.plot_dropout_heatmaps_for_clusters(cluster_meta, feature_matrix, sort_col=sort_col, save_dir=base_dir,
                                             folder=folder)
 
-# plot dropout heatmaps in manually sorted order
-sort_col = 'manual_sort_order'
-plotting.plot_dropout_heatmaps_for_clusters(cluster_meta, feature_matrix, sort_col=sort_col, save_dir=base_dir,
-                                            folder=folder)
+# # plot dropout heatmaps in manually sorted order
+# sort_col = 'manual_sort_order'
+# plotting.plot_dropout_heatmaps_for_clusters(cluster_meta, feature_matrix, sort_col=sort_col, save_dir=base_dir,
+#                                             folder=folder)
 
 # plot feature matrix sorted by cluster ID
 plotting.plot_feature_matrix_sorted(feature_matrix, cluster_meta, sort_col='cluster_id', save_dir=base_dir,
                                     folder=folder)
 
-plotting.plot_feature_matrix_sorted(feature_matrix, cluster_meta, sort_col='manual_sort_order', save_dir=base_dir,
-                                    folder=folder)
+# plotting.plot_feature_matrix_sorted(feature_matrix, cluster_meta, sort_col='manual_sort_order', save_dir=base_dir,
+#                                     folder=folder)
 
 # umap with cluster labels
 label_col = 'cluster_id'
@@ -228,7 +148,9 @@ plotting.plot_within_cluster_correlations_all_cre(cluster_meta, n_clusters_cre, 
 # average dropouts per cre line
 plotting.plot_average_dropout_heatmap_for_cre_lines(results_pivoted, cluster_meta, save_dir=base_dir, folder=folder)
 
-# plot 100 cells per cluster to examine within cluster variability
+# # plot 100 cells per cluster to examine within cluster variability
+# dropouts = results_pivoted.merge(cells_table[['cell_specimen_id', 'experience_level', 'ophys_experiment_id']], on=['cell_specimen_id', 'experience_level'])
+# dropouts = dropouts.drop_duplicates(subset=['cell_specimen_id', 'ophys_experiment_id'])
 # plotting.plot_random_subset_of_cells_per_cluster(cluster_meta, dropouts, save_dir)
 
 
@@ -258,12 +180,12 @@ plotting.plot_fraction_cells_by_area_depth(cluster_meta, n_clusters_cre, normali
 # cluster_proportions_cre
 
 # cluster proportions for all cre lines
-proportions = processing.get_proportion_cells_rel_cluster_average(cluster_meta, cre_lines)
+proportions = processing.get_proportion_cells_rel_cluster_average(cluster_meta, cre_lines, groupby_columns=['targeted_structure', 'layer'])
 
 # plot fraction cells per area depth
 value_to_plot = 'proportion_cells'
 cbar_label = 'proportion relative\n to cluster avg'
-suffix = '_full_range'
+suffix = '_size_order'
 
 vmax = proportions[value_to_plot].max()
 
@@ -275,7 +197,9 @@ plotting.plot_cell_stats_per_cluster_for_areas_depths(cluster_meta, proportions,
 # sort clusters by the fraction of cells in VISp upper
 value_to_plot = 'proportion_cells'
 cbar_label = 'proportion cells\n rel. to cluster avg'
-suffix = '_full_range'
+suffix = '_VISp_upper_sort'
+
+proportions = processing.get_proportion_cells_rel_cluster_average(cluster_meta, cre_lines, groupby_columns=['targeted_structure', 'layer'])
 
 vmax = proportions[value_to_plot].max()
 cluster_order = processing.get_cluster_order_for_metric_location(proportions, cluster_meta,
@@ -338,7 +262,7 @@ plotting.plot_cell_stats_per_cluster_for_areas_depths(cluster_meta, area_proport
 for cre_line in cre_lines:
     plotting.plot_clusters_stats_pop_avg_rows(cluster_meta, feature_matrix, multi_session_df,
                                               area_proportions, fraction_cells, cre_line,
-                                              sort_order=cluster_order, suffix='_VISp_upper_proportion_rel_avg',
+                                              sort_order=cluster_order, suffix=suffix,
                                               save_dir=base_dir, folder=folder, )
 
 # depths
@@ -361,5 +285,14 @@ plotting.plot_cell_stats_per_cluster_for_areas_depths(cluster_meta, layer_propor
 
 for cre_line in cre_lines:
     plotting.plot_clusters_pop_avg_rows(cluster_meta, feature_matrix, multi_session_df, cre_line,
-                                        sort_order=cluster_order, suffix='_upper_proportion_rel_avg',
+                                        sort_order=cluster_order, suffix=suffix,
                                         save_dir=base_dir, folder=folder, )
+
+# plot in cluster size order
+
+for cre_line in cre_lines:
+    plotting.plot_clusters_stats_pop_avg_rows(cluster_meta, feature_matrix, multi_session_df,
+                                              proportions, fraction_cells, cre_line,
+                                              sort_order=None, suffix='_proportion_rel_avg_cluster_size_order',
+                                              save_dir=base_dir, folder=folder, )
+
