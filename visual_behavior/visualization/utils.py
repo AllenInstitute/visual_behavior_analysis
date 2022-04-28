1f262aacf32a238d878b6431536d9498a62a4d9b
matchings <marinag@alleninstitute.org>
2022-04-28 16:05:06 -0700

    add training stage colors

--- visual_behavior/visualization/utils.py
+++ visual_behavior/visualization/utils.py
@@ -78,7 +78,30 @@ def get_session_type_color_map():
     colors = np.floor(np.array([list(x) for x in get_colors_for_session_numbers()]) * 255).astype(np.uint8)
     black = np.array([0, 0, 0]).astype(np.uint8)
 
+
     session_type_color_map = {
+        'TRAINING_0_gratings_autorewards_15min': black,
+        'TRAINING_1_gratings': lighter(black, 0.2),
+        'TRAINING_2_gratings_flashed': lighter(black, 0.4),
+        'TRAINING_3_images_A_10uL_reward': lighter(black, 0.6),
+        'TRAINING_3_images_B_10uL_reward': lighter(black, 0.6),
+        'TRAINING_3_images_G_10uL_reward': lighter(black, 0.6),
+        'TRAINING_4_images_A_handoff_lapsed': lighter(black, 0.8),
+        'TRAINING_4_images_A_handoff_ready': lighter(black, 0.8),
+        'TRAINING_4_images_A_training': lighter(black, 0.8),
+        'TRAINING_4_images_B_training': lighter(black, 0.8),
+        'TRAINING_4_images_G_training': lighter(black, 0.8),
+        'TRAINING_5_images_A_epilogue': lighter(black, 0.8),
+        'TRAINING_5_images_A_handoff_lapsed': lighter(black, 0.8),
+        'TRAINING_5_images_A_handoff_ready': lighter(black, 0.8),
+        'TRAINING_5_images_B_epilogue': lighter(black, 0.8),
+        'TRAINING_5_images_B_handoff_lapsed': lighter(black, 0.8),
+        'TRAINING_5_images_B_handoff_ready': lighter(black, 0.8),
+        'TRAINING_5_images_G_epilogue': lighter(black, 0.8),
+        'TRAINING_5_images_G_handoff_lapsed': lighter(black, 0.8),
+        'TRAINING_5_images_G_handoff_ready': lighter(black, 0.8),
+
+
         'OPHYS_0_images_A_habituation': lighter(colors[0, :], 0.8),
         'OPHYS_1_images_A': colors[0, :],
         'OPHYS_2_images_A_passive': colors[1, :],
