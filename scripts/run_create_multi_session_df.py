6195230cb7649cec90d69217056beeef42cf1a84
matchings <marinag@alleninstitute.org>
2022-07-22 17:32:03 -0700

    try 2 min epoch window

--- scripts/run_create_multi_session_df.py
+++ scripts/run_create_multi_session_df.py
@@ -29,7 +29,7 @@ cache = bpc.from_s3_cache(cache_dir=cache_dir)
 print(cache_dir)
 
 experiments_table = cache.get_ophys_experiment_table()
-experiments_table = experiments_table[(experiments_table.session_number==4)&(experiments_table.project_code=='VisualBehaviorMultiscope')]
+# experiments_table = experiments_table[(experiments_table.session_number==4)&(experiments_table.project_code=='VisualBehaviorMultiscope')]
 
 # call the `sbatch` command to run the jobs.
 for project_code in experiments_table.project_code.unique():
--- visual_behavior/ophys/io/create_multi_session_df.py
+++ visual_behavior/ophys/io/create_multi_session_df.py
@@ -75,8 +75,8 @@ def get_multi_session_df(project_code, session_number, conditions, data_type, ev
 
     ##### limit to platform paper experiments
     experiments_table = loading.get_platform_paper_experiment_table(add_extra_columns=True,
-                                                                    limit_to_closest_active=False,
-                                                                    include_4x2_data=True)
+                                                                    limit_to_closest_active=True,
+                                                                    include_4x2_data=False)
 
     print(len(experiments_table), 'platform expts')
 
@@ -121,7 +121,7 @@ def get_multi_session_df(project_code, session_number, conditions, data_type, ev
                 # get stimulus_response_df
                 df = loading.get_stimulus_response_df(dataset, data_type=data_type, event_type=event_type, time_window=time_window,
                                                       interpolate=interpolate, output_sampling_rate=output_sampling_rate,
-                                                      load_from_file=False, epoch_duration_mins=1)
+                                                      load_from_file=True, epoch_duration_mins=2)
                 # use response_window duration from stim response df if it exists
                 if response_window_duration in df.keys():
                     response_window_duration = df.response_window_duration.values[0]
