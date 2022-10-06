5e9c36902ba68b06621ce60f1957cbbff0d6735a
matchings <marinag@alleninstitute.org>
2022-10-06 15:46:57 -0700

    remove try except

--- visual_behavior/ophys/io/create_multi_session_df.py
+++ visual_behavior/ophys/io/create_multi_session_df.py
@@ -96,55 +96,56 @@ def get_multi_session_df(project_code, mouse_id, conditions, data_type, event_ty
     if process_data:
         mega_mdf = pd.DataFrame()
         for experiment_id in experiments.index.unique():
-            try:
-                print(experiment_id)
-                # get dataset
-                dataset = loading.get_ophys_dataset(experiment_id,
-                                                    get_extended_stimulus_presentations=use_extended_stimulus_presentations)
-                # get stimulus_response_df
-                df = loading.get_stimulus_response_df(dataset, data_type=data_type, event_type=event_type, time_window=time_window,
-                                                      interpolate=interpolate, output_sampling_rate=output_sampling_rate,
-                                                      load_from_file=True)
-                # use response_window duration from stim response df if it exists
-                if response_window_duration in df.keys():
-                    response_window_duration = df.response_window_duration.values[0]
-                df['ophys_experiment_id'] = experiment_id
-                # if using omissions, only include omissions where time from last change is more than 3 seconds
-                # if event_type == 'omissions':
-                #     df = df[df.time_from_last_change>3]
-                # modify columns for specific conditions
-                if 'passive' in dataset.metadata['session_type']:
-                    df['lick_on_next_flash'] = False
-                    df['engaged'] = False
-                    df['engagement_state'] = 'disengaged'
-                if 'running_state' in conditions:  # create 'running_state' Boolean column based on threshold on mean_running_speed
-                    df['running'] = [True if mean_running_speed > 2 else False for mean_running_speed in
-                                     df.mean_running_speed.values]
-                if 'pupil_state' in conditions:  # create 'pupil_state' Boolean column based on threshold on mean_pupil_
-                    if 'mean_pupil_area' in df.keys():
-                        df = df[df.mean_pupil_area.isnull() == False]
-                        if len(df) > 100:
-                            median_pupil_area = df.mean_pupil_area.median()
-                            df['large_pupil'] = [True if mean_pupil_area > median_pupil_area else False for mean_pupil_area in
-                                                 df.mean_pupil_area.values]
-                if 'pre_change' in conditions:
-                    df = df[df.pre_change.isnull() == False]
-                # get params for mean df creation from stimulus_response_df
-                output_sampling_rate = df.frame_rate.unique()[0]
-                timestamps = df.trace_timestamps.values[0]
-                window_around_timepoint_seconds = [timestamps[0], timestamps[-1]]
+            # try:
+            print(experiment_id)
+            # get dataset
+            dataset = loading.get_ophys_dataset(experiment_id,
+                                                get_extended_stimulus_presentations=use_extended_stimulus_presentations)
+            # get stimulus_response_df
+            df = loading.get_stimulus_response_df(dataset, data_type=data_type, event_type=event_type, time_window=time_window,
+                                                  interpolate=interpolate, output_sampling_rate=output_sampling_rate,
+                                                  load_from_file=False)
+            print('stim response df loaded')
+            # use response_window duration from stim response df if it exists
+            if response_window_duration in df.keys():
+                response_window_duration = df.response_window_duration.values[0]
+            df['ophys_experiment_id'] = experiment_id
+            # if using omissions, only include omissions where time from last change is more than 3 seconds
+            # if event_type == 'omissions':
+            #     df = df[df.time_from_last_change>3]
+            # modify columns for specific conditions
+            if 'passive' in dataset.metadata['session_type']:
+                df['lick_on_next_flash'] = False
+                df['engaged'] = False
+                df['engagement_state'] = 'disengaged'
+            if 'running_state' in conditions:  # create 'running_state' Boolean column based on threshold on mean_running_speed
+                df['running'] = [True if mean_running_speed > 2 else False for mean_running_speed in
+                                 df.mean_running_speed.values]
+            if 'pupil_state' in conditions:  # create 'pupil_state' Boolean column based on threshold on mean_pupil_
+                if 'mean_pupil_area' in df.keys():
+                    df = df[df.mean_pupil_area.isnull() == False]
+                    if len(df) > 100:
+                        median_pupil_area = df.mean_pupil_area.median()
+                        df['large_pupil'] = [True if mean_pupil_area > median_pupil_area else False for mean_pupil_area in
+                                             df.mean_pupil_area.values]
+            if 'pre_change' in conditions:
+                df = df[df.pre_change.isnull() == False]
+            # get params for mean df creation from stimulus_response_df
+            output_sampling_rate = df.frame_rate.unique()[0]
+            timestamps = df.trace_timestamps.values[0]
+            window_around_timepoint_seconds = [timestamps[0], timestamps[-1]]
 
-                mdf = ut.get_mean_df(df, conditions=conditions, frame_rate=output_sampling_rate,
-                                     time_window=time_window, response_window_duration=response_window_duration,
-                                     get_pref_stim=get_pref_stim, exclude_omitted_from_pref_stim=True)
-                if 'correlation_values' in mdf.keys():
-                    mdf = mdf.drop(columns=['correlation_values'])
-                mdf['ophys_experiment_id'] = experiment_id
-                print('mean df created for', experiment_id)
-                mega_mdf = pd.concat([mega_mdf, mdf])
-            except Exception as e:  # flake8: noqa: E722
-                print(e)
-                print('problem for', experiment_id)
+            mdf = ut.get_mean_df(df, conditions=conditions, frame_rate=output_sampling_rate,
+                                 time_window=time_window, response_window_duration=response_window_duration,
+                                 get_pref_stim=get_pref_stim, exclude_omitted_from_pref_stim=True)
+            if 'correlation_values' in mdf.keys():
+                mdf = mdf.drop(columns=['correlation_values'])
+            mdf['ophys_experiment_id'] = experiment_id
+            print('mean df created for', experiment_id)
+            mega_mdf = pd.concat([mega_mdf, mdf])
+            # except Exception as e:  # flake8: noqa: E722
+            #     print(e)
+            #     print('problem for', experiment_id)
 
         if 'level_0' in mega_mdf.keys():
             mega_mdf = mega_mdf.drop(columns='level_0')
