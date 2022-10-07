ac3a5a47c5df48c5bc1d77c0e6e3cc9e99690f7b
matchings <marinag@alleninstitute.org>
2022-10-06 20:01:57 -0700

    set index

--- scripts/create_single_cell_plots.py
+++ scripts/create_single_cell_plots.py
@@ -24,6 +24,7 @@ if __name__ == '__main__':
 
     save_dir = r'/allen/programs/mindscope/workgroups/learning/ophys/learning_project_cache'
     experiments_table = pd.read_csv(os.path.join(save_dir, 'mFISH_project_expts.csv'))
+    experiments_table = experiments_table.set_index('ophys_experiment_id')
     print(len(experiments_table))
 
     # get cells that are matched in all sessions
