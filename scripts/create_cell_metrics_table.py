6e29fcfa070e4733360f7a2794d07523a8fdfaae
matchings <marinag@alleninstitute.org>
2024-01-28 17:22:29 -0800

    generate cell metrics

--- scripts/create_cell_metrics_table.py
+++ scripts/create_cell_metrics_table.py
@@ -17,4 +17,4 @@ if __name__ == '__main__':
     for data_type in ['events', 'filtered_events', 'dff']:
         cell_metrics.generate_and_save_all_metrics_tables_for_experiment(ophys_experiment_id, data_type=data_type, interpolate=interpolate,
                                                         time_window=time_window, response_window_duration=response_window_duration,
-                                                        overwrite=True)
+                                                        overwrite=False)
