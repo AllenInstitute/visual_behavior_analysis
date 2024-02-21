f346e4f3162690041998aa786524e1edfc3a80d2
matchings <marinag@alleninstitute.org>
2024-02-20 20:14:27 -0800

    remove container_id from run script

--- scripts/run_create_multi_session_df.py
+++ scripts/run_create_multi_session_df.py
@@ -27,7 +27,6 @@ python_executable = "{}/anaconda3/envs/{}/bin/python".format(os.path.expanduser(
 # define the job record output folder
 stdout_location = r"/allen/programs/mindscope/workgroups/learning/ophys/cluster_jobs/multi_session_dfs"
 
-
 # cache = VisualBehaviorOphysProjectCache.from_lims()
 # experiments_table = cache.get_ophys_experiment_table(passed_only=False)
 # experiments_table = experiments_table[experiments_table.project_code.isin(['LearningmFISHTask1A', 'LearningmFISHDevelopment',
@@ -37,14 +36,12 @@ stdout_location = r"/allen/programs/mindscope/workgroups/learning/ophys/cluster_
 
 save_dir = r'/allen/programs/mindscope/workgroups/learning/ophys/learning_project_cache'
 import pandas as pd
-experiments_table = pd.read_csv(os.path.join(save_dir, 'mFISH_project_expts.csv'))
-# experiments_table = experiments_table[experiments_table.project_code.isin(['LearningmFISHTask1A',
-#                                                                            'LearningmFISHDevelopment',
-#                                                                            'omFISHGad2Meso'])]
-# experiments_table = experiments_table[(experiments_table.project_code.isin(['omFISHGad2Meso']))&
-#                                       (experiments_table.session_type=='OPHYS_2_images_A_passive')]
-# experiments_table = experiments_table[(experiments_table.project_code.isin(['LearningmFISHDevelopment']))]
+experiments_table = pd.read_csv(os.path.join(save_dir, 'mFISH_project_expts.csv'), index_col=0)
+# limit to non-failed experiments
+experiments_table = experiments_table[(experiments_table.experiment_workflow_state.isin(['passed', 'qc'])) &
+                                      (experiments_table.container_workflow_state != 'failed')]
 print(len(experiments_table), 'experiments')
+print(len(experiments_table.mouse_id.unique()), 'mice')
 
 # experiments_table = loading.get_filtered_ophys_experiment_table()
 # experiments_table = experiments_table[experiments_table.project_code=='LearningmFISHTask1A']
@@ -52,20 +49,20 @@ print(len(experiments_table), 'experiments')
 # call the `sbatch` command to run the jobs.
 for mouse_id in experiments_table.mouse_id.unique():
     print('mouse_id:', mouse_id)
-    ophys_container_ids = experiments_table[experiments_table.mouse_id==mouse_id].ophys_container_id.unique()
-    print('there are', len(ophys_container_ids),'ophys_container_ids for this mouse')
-    for ophys_container_id in ophys_container_ids:
-        print('ophys_container_id:', ophys_container_id)
+    # ophys_container_ids = experiments_table[experiments_table.mouse_id==mouse_id].ophys_container_id.unique()
+    # print('there are', len(ophys_container_ids),'ophys_container_ids for this mouse')
+    # for ophys_container_id in ophys_container_ids:
+    #     print('ophys_container_id:', ophys_container_id)
         # instantiate a Slurm object
-        slurm = Slurm(mem='120g',  # '24g'
-                      cpus_per_task=1,
-                      time='20:00:00',
-                      partition='braintv',
-                      job_name='multi_session_df_'+str(mouse_id)+'_'+str(ophys_container_id),
-                      output=f'{stdout_location}/{Slurm.JOB_ARRAY_MASTER_ID}_{Slurm.JOB_ARRAY_ID}.out',
-                      )
+    slurm = Slurm(mem='120g',  # '24g'
+                  cpus_per_task=1,
+                  time='20:00:00',
+                  partition='braintv',
+                  job_name='multi_session_df_'+str(mouse_id), #+'_'+str(ophys_container_id),
+                  output=f'{stdout_location}/{Slurm.JOB_ARRAY_MASTER_ID}_{Slurm.JOB_ARRAY_ID}.out',
+                  )
 
-
-        slurm.sbatch(python_executable+' '+python_file+' --mouse_id '+str(mouse_id)+' --ophys_container_id'+' '+str(ophys_container_id))
+    # slurm.sbatch( python_executable + ' ' + python_file + ' --mouse_id ' + str(mouse_id) + ' --ophys_container_id' + ' ' + str(ophys_container_id))
+    slurm.sbatch(python_executable+' '+python_file+' --mouse_id '+str(mouse_id))
 
 
